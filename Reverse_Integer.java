cc0c53717939fe7af01a458c2e8ee828155380d7
Netal-Rathi <netalrathi2707@gmail.com>
2024-02-01 15:22:34 +0530

    Add Reverse Integer

--- Reverse_Integer.java
+++ Reverse_Integer.java
@@ -1,19 +1,19 @@
 import java.util.*;
-
 class Solution {
     public int reverse(int x) {
-        int sum = 0;
-        if (Integer.MIN_VALUE <= x && x <= Integer.MAX_VALUE) {
-            while (x != 0) {
-                int temp = x % 10;
-                sum = sum * 10 + temp;
-                x = x / 10;
-            }
-            return sum;
-        } else {
+       
+        int sum=0;
+        if(Integer.MIN_VALUE=<X && x<=Integer.MAX_VALUE){
+           while(x!=0){
+              int temp=x%10;
+               sum=sum*10+ temp;
+               x=x/10;
+           }
+           return sum;
+
+        }else{
             return 0;
         }
+        
     }
 }
-
-// Title: Reverse Integer
