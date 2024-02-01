db41839e538c22080996b3a60b778be0dad92d25
Netal-Rathi <netalrathi2707@gmail.com>
2024-02-01 15:15:44 +0530

    Add Reverse Integer

--- Reverse_Integer.java
+++ Reverse_Integer.java
@@ -3,7 +3,7 @@ class Solution {
     public int reverse(int x) {
        
         int sum=0;
-        if(Math.pow(-2,31)<=x && x<=(Math.pow(2,31))-1){
+        if(x>=Math.pow(-2,31) && x<=(Math.pow(2,31))-1){
            while(x!=0){
               int temp=x%10;
                sum=sum*10+ temp;
@@ -17,4 +17,3 @@ class Solution {
         
     }
 }
-// Title: Reverse Integer
