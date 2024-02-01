01dee297544f81ee3f266802bd5ad608ca3d0149
Netal-Rathi <netalrathi2707@gmail.com>
2024-02-01 15:36:49 +0530

    Add Palindrome Number

---
+++ Palindrome_Number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int reverse=0;
+        int real_no=x;
+        while(x!=0){
+            int temp=x%10;
+            reverse=reverse*10 + temp;
+            x=x/10;
+        }
+        return (reverse==real_no);
+         
+        
+    }
+}
