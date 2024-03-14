5f1f81d3cc67839e022d55af4767b4bd20ad1b88
Netal-Rathi <netalrathi2707@gmail.com>
2024-03-15 05:15:03 +0530

    Add Matrix Diagonal Sum

---
+++ Matrix_Diagonal_Sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int n=mat.length;
+        int sum=0;
+        int x=n%2;
+        if(n==1){
+            return mat[0][0];
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==j){
+                    sum+=mat[i][j];
+                }
+                if(i+j==n-1){
+                    sum+=mat[i][j];
+                }
+                
+            }
+        }
+        if(x!=0){
+        sum-=mat[n/2][n/2];
+        }
+        return sum;
+        
+    }
+}
