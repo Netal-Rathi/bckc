1ad9805fad3f31c30c908e54cac902d22961d2e5
Netal-Rathi <netalrathi2707@gmail.com>
2024-04-05 17:51:43 +0530

    Add Make The String Great

---
+++ Make_The_String_Great.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String makeGood(String s) {
+        if(s.isEmpty()){
+            return s;
+        }
+        boolean c=false;
+        while(!c){
+            c=true;
+        for(int i=0;i<s.length()-1;i++){
+            char current=s.charAt(i);
+            char next=s.charAt(i+1);
+            if(Math.abs((current-next))==32){
+               s = s.substring(0, i) + s.substring(i + 2);
+                c=false;
+                break;
+            }
+
+            
+        }
+
+    
+}
+return s;
+}
+}
