6e5c7dd3ad57aeac666088c190d82b6e3b1e487d
Netal-Rathi <netalrathi2707@gmail.com>
2024-04-09 06:53:46 +0530

    Add Number of Students Unable to Eat Lunch

---
+++ Number_of_Students_Unable_to_Eat_Lunch.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        ArrayList<Integer> stud = new ArrayList<>();
+        ArrayList<Integer> sand = new ArrayList<>();
+
+        
+        for (int s : students) {
+            stud.add(s);
+        }
+        for (int s : sandwiches) {
+            sand.add(s);
+        }
+
+        int count = 0;
+        while (count < stud.size()) {
+            if (stud.get(0) == sand.get(0)) { 
+                stud.remove(0);
+                sand.remove(0);
+                count = 0; 
+            } else {
+                int first = stud.remove(0);
+                stud.add(first); 
+                count++; 
+            }
+        }
+        return stud.size();
+    }
+}
