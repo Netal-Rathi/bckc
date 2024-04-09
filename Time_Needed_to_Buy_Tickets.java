fb2c5889ff2280a6d2e7c3eec53309e92c61951a
Netal-Rathi <netalrathi2707@gmail.com>
2024-04-09 06:54:12 +0530

    Add Time Needed to Buy Tickets

---
+++ Time_Needed_to_Buy_Tickets.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int time =0;
+        /*ArrayList <Integer> tic=new Arraylist<>();
+        for(int t : tickets ){
+            tic.add(t);
+        } */
+        while(tickets[k]>0){
+  for(int i=0;i<tickets.length;i++){
+    
+        if(tickets[i]>0 ){
+
+                tickets[i]=tickets[i]-1;
+                time ++;
+                if(tickets[k]==0){
+                    return time;
+                }
+            }
+    
+        
+  }
+        }
+  return time;
+        
+    }
+}
