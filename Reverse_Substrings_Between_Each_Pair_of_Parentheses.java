beaecfb4751b1025a09e51a266fcbef2588f7c77
Netal-Rathi <netalrathi2707@gmail.com>
2024-07-11 13:01:51 +0530

    Add Reverse Substrings Between Each Pair of Parentheses

---
+++ Reverse_Substrings_Between_Each_Pair_of_Parentheses.java
@@ -0,0 +1,46 @@
+import java.util.Stack;
+
+public class Solution {
+
+    public static String reverseParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        char[] arr = s.toCharArray();
+
+        System.out.println("Input: " + s);
+
+        // Traverse the string
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == '(') {
+                stack.push(i);
+                System.out.println("Push: " + i);
+            } else if (arr[i] == ')') {
+                int start = stack.pop();
+                System.out.println("Pop: " + start);
+                reverse(arr, start + 1, i - 1);
+                System.out.println("Reversed: " + new String(arr));
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (char c : arr) {
+            if (c != '(' && c != ')') {
+                sb.append(c);
+            }
+        }
+
+        String result = sb.toString();
+        System.out.println("Result: " + result);
+
+        return result;
+    }
+
+    private static void reverse(char[] arr, int start, int end) {
+        while (start < end) {
+            char temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
