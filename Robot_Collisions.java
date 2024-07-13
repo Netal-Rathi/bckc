7d4ca88581fa8497dab798cb1340882d45539575
Netal-Rathi <netalrathi2707@gmail.com>
2024-07-13 06:57:36 +0530

    Add Robot Collisions

--- Robot_Collisions.java
+++ Robot_Collisions.java
@@ -13,12 +13,11 @@ public class Solution {
             for (int i = 0; i < positions.length - 1; i++) {
                 if (directions.charAt(i) != directions.charAt(i + 1)) {
 
-                   // if(positions[i]<positions[i+1] && directions.charAt(i)!='R'  && directions.charAt(i+1)!='L'){
+                    if(positions[i]<positions[i+1] && directions.charAt(i)!='R'  && directions.charAt(i+1)!='L'){
                         continue;
                                         
                     
-                 //   } else 
-                    if (healths[i] == healths[i + 1]) {
+                    } else if (healths[i] == healths[i + 1]) {
                         healths[i] = 0;
                         healths[i + 1] = 0;
                     } else if (healths[i] > healths[i + 1]) {
@@ -52,5 +51,3 @@ public class Solution {
         return true;
     }
 }
-
-// Title: Robot Collisions
