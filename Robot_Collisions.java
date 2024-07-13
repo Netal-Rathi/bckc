057463827cfb4b56b14db3b41d94828db9b92e73
Netal-Rathi <netalrathi2707@gmail.com>
2024-07-13 07:05:56 +0530

    Add Robot Collisions

--- Robot_Collisions.java
+++ Robot_Collisions.java
@@ -13,15 +13,10 @@ public class Solution {
             for (int i = 0; i < positions.length - 1; i++) {
                 if (directions.charAt(i) != directions.charAt(i + 1)) {
                     // Original commented out logic
-                    // if(positions[i]<positions[i+1] && directions.charAt(i)!='R'  && directions.charAt(i+1)!='L'){
-                    // continue;
+                     if(positions[i]<positions[i+1] && directions.charAt(i)!='R'  && directions.charAt(i+1)!='L'){
+                     continue;
             
-                 //   }else if(position[i]>position[j] && directions.charAt(i)='R'){
-
-
-                    if(positions[i]<positions[i+1]){
-
-                     if (healths[i] == healths[i + 1]) {
+                     } else if (healths[i] == healths[i + 1]) {
                         healths[i] = 0;
                         healths[i + 1] = 0;
                     } else if (healths[i] > healths[i + 1]) {
@@ -32,24 +27,6 @@ public class Solution {
                         healths[i + 1] = healths[i + 1] - 1;
                     }
                     i++;
-                }else if((directions.charAt(i)!='L' && directions.charAt(i+1)!='R') || (directions.charAt(i)!='R' && directions.charAt(i+1)!='L')){
-
-                    if (healths[i] == healths[i + 1]) {
-                        healths[i] = 0;
-                        healths[i + 1] = 0;
-                    } else if (healths[i] > healths[i + 1]) {
-                        healths[i + 1] = 0;
-                        healths[i] = healths[i] - 1;
-                    } else {
-                        healths[i] = 0;
-                        healths[i + 1] = healths[i + 1] - 1;
-                    }
-                    i++;
-
-                }
-
-
-                
                 }
             }
 
@@ -73,5 +50,3 @@ public class Solution {
         return true;
     }
 }
-
-// Title: Robot Collisions
