e02d542b1bd83da0ab6b81ef98bc56926d94667f
Netal-Rathi <netalrathi2707@gmail.com>
2024-07-13 14:11:13 +0530

    Add Robot Collisions

--- Robot_Collisions.java
+++ Robot_Collisions.java
@@ -4,47 +4,39 @@ import java.util.List;
 
 public class Solution {
     public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String directions) {
-        int n = positions.length;
-        Integer[] indices = new Integer[n];
-        for (int i = 0; i < n; i++) {
-            indices[i] = i;
-        }
+        if (issame(directions)) {
+            return Arrays.asList(Arrays.stream(healths).boxed().toArray(Integer[]::new));
+        } else {
+            for (int i = 0; i < positions.length - 1; i++) {
+                if ((positions[i] < positions[i + 1] && directions.charAt(i) == 'R' && directions.charAt(i + 1) == 'L') ||
+                    (positions[i] > positions[i + 1] && directions.charAt(i) == 'L' && directions.charAt(i + 1) == 'R')) {
 
-        Arrays.sort(indices, (a, b) -> positions[a] - positions[b]);
-
-        List<Integer> stack = new ArrayList<>();
-        for (int i : indices) {
-            while (!stack.isEmpty() && directions.charAt(stack.get(stack.size() - 1)) == 'R' && directions.charAt(i) == 'L') {
-                int j = stack.get(stack.size() - 1);
-                if (healths[j] == healths[i]) {
-                    healths[j] = 0;
-                    healths[i] = 0;
-                    stack.remove(stack.size() - 1);
-                    break;
-                } else if (healths[j] > healths[i]) {
-                    healths[j] -= 1;
-                    healths[i] = 0;
-                    break;
-                } else {
-                    healths[i] -= 1;
-                    healths[j] = 0;
-                    stack.remove(stack.size() - 1);
+                    if (healths[i] == healths[i + 1]) {
+                        healths[i] = 0;
+                        healths[i + 1] = 0;
+                        i++;
+                    } else if (healths[i] > healths[i + 1]) {
+                        healths[i + 1] = 0;
+                        healths[i] -= 1;
+                    } else {
+                        healths[i] = 0;
+                        healths[i + 1] -= 1;
+                    }
+                   
                 }
             }
-            if (healths[i] > 0) {
-                stack.add(i);
-            }
-        }
 
-        List<Integer> result = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            if (healths[i] > 0) {
-                result.add(healths[i]);
+            List<Integer> finalans = new ArrayList<>();
+            for (int health : healths) {
+                if (health != 0) {
+                    finalans.add(health);
+                }
             }
+            return finalans;
         }
-        return result;
     }
 
+    // Static helper method moved outside
     public static boolean issame(String directions) {
         for (int i = 1; i < directions.length(); i++) {
             if (directions.charAt(0) != directions.charAt(i)) {
@@ -54,5 +46,3 @@ public class Solution {
         return true;
     }
 }
-
-// Title: Robot Collisions
