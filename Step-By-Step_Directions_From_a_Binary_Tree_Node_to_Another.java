f20cc5cb9debbb451a4c92db8d32f15033987d17
Netal-Rathi <netalrathi2707@gmail.com>
2024-07-16 16:45:19 +0530

    Add Step-By-Step Directions From a Binary Tree Node to Another

--- Step-By-Step_Directions_From_a_Binary_Tree_Node_to_Another.java
+++ Step-By-Step_Directions_From_a_Binary_Tree_Node_to_Another.java
@@ -9,57 +9,59 @@
  *     TreeNode(int val, TreeNode left, TreeNode right) {
  *         this.val = val;
  *         this.left = left;
- *         this.right = right;
+ *         this.right = right; 
  *     }
  * }
  */
 class Solution {
     public String getDirections(TreeNode root, int startValue, int destValue) {
-       
-
-       List<Character> startpath=new ArrayList<>();
-       List<Character> destpath=new ArrayList<>();
-       pathfinding(root,startValue,startpath);
-       pathfinding(root,destValue,destpath);
-
-       int i=0;
-       while(i<startpath.size() && i<destpath.size() && startpath.get(i)== destpath.get(i)){
-        i++;
-       }
-        StringBuilder result=new StringBuilder();
-       for(int  j=i;j<startpath.size();j++){
-        result.append('U');
-       }
-
-       for(int j=i;j<destpath.size();j++){
-        result.append(destpath.get(j));
-       }
-       return result.toString();
-       
+        // Find paths from root to startValue and destValue
+        List<Character> startPath = new ArrayList<>();
+        List<Character> destPath = new ArrayList<>();
+        
+        findPath(root, startValue, startPath);
+        findPath(root, destValue, destPath);
+        
+        // Find the common prefix length
+        int i = 0;
+        while (i < startPath.size() && i < destPath.size() && startPath.get(i) == destPath.get(i)) {
+            i++;
+        }
+        
+        // Move up to the common ancestor
+        StringBuilder result = new StringBuilder();
+        for (int j = i; j < startPath.size(); j++) {
+            result.append('U');
+        }
+        
+        // Move down to the destination
+        for (int j = i; j < destPath.size(); j++) {
+            result.append(destPath.get(j));
+        }
+        
+        return result.toString();
     }
-
-     public boolean pathfinding(TreeNode root,int value,List<Character> path){
-
-            if(root==null){
-                return false;
-            }
-            if(root.val==value){
-                return true;
-            }
-
-            path.add('L');
-            if(pathfinding(root.left,value,path)){
-                return true;
-            }
-            path.remove(path.size()-1);
-
-            path.add('R');
-            if(pathfinding(root.right,value,path)){
-                return true;
-            }
-
-            path.remove(path.size()-1);
+    
+    private boolean findPath(TreeNode root, int value, List<Character> path) {
+        if (root == null) {
             return false;
         }
+        if (root.val == value) {
+            return true;
+        }
+        
+        path.add('L');
+        if (findPath(root.left, value, path)) {
+            return true;
+        }
+        path.remove(path.size() - 1);
+        
+        path.add('R');
+        if (findPath(root.right, value, path)) {
+            return true;
+        }
+        path.remove(path.size() - 1);
+        
+        return false;
+    }
 }
-// Title: Step-By-Step Directions From a Binary Tree Node to Another
