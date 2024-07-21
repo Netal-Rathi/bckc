4f3aa630c6ab13cc9e50992514fc3e5b194a66c4
Netal-Rathi <netalrathi2707@gmail.com>
2024-07-21 10:39:32 +0530

    Add Climbing Stairs

--- Climbing_Stairs.java
+++ Climbing_Stairs.java
@@ -1,9 +1,4 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
-    private Map<Integer, Integer> memo = new HashMap<>();
-
     public int climbStairs(int n) {
         if (n == 0) {
             return 1;
@@ -13,16 +8,8 @@ class Solution {
             return 0;
         }
         
-        if (memo.containsKey(n)) {
-            return memo.get(n);
-        }
-        
         int c1 = climbStairs(n - 1);
         int c2 = climbStairs(n - 2);
-        
-        memo.put(n, c1 + c2);
         return c1 + c2;
     }
 }
-
-// Title: Climbing Stairs
