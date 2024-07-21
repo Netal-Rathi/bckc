19cb3bd8788427a00cd103a0e38b4594f9ed8c0a
Netal-Rathi <netalrathi2707@gmail.com>
2024-07-21 10:32:15 +0530

    Add Lexicographical Numbers

---
+++ Lexicographical_Numbers.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 1; i <= 9; i++) {
+            isLexical(n, i, ans);
+        }
+        return ans;
+    }
+
+    private void isLexical(int n, int num, List<Integer> ans) {
+        if (num > n) {
+            return;
+        }
+        ans.add(num);
+        for (int i = 0; i <= 9; i++) {
+            int next = num * 10 + i;
+            if (next > n) {
+                return;
+            }
+            isLexical(n, next, ans);
+        }
+    }
+}
