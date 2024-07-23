d04c0ab5cae968c32de794a034f4d02ee3b8eb8f
Netal-Rathi <netalrathi2707@gmail.com>
2024-07-23 06:13:34 +0530

    Add Permutations

--- Permutations.java
+++ Permutations.java
@@ -1,29 +1,26 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
-    public List<List<Integer>> permute(int[] nums){
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        findpermutations(nums,new ArrayList<>(), result);
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        findPermutation(nums, new ArrayList<>(), result);
         return result;
-
-
     }
 
-    public void findpermutations(int[] nums,List<Integer>current ,List<List<Integer>> result){
-        if(current.size()==nums.length){
-            result.add(new ArrayList<> (current));
+    private void findPermutation(int[] nums, List<Integer> current, List<List<Integer>> result) {
+        if (current.size() == nums.length) {
+            result.add(new ArrayList<>(current));
             return;
-
         }
-        for(int i=0;i<nums.length;i++){
-            if(!current.contains(nums[i])){
-                current.add(nums[i]);
 
-               findpermutations(nums,current,result);
-               current.remove(current.size()-1);
-            }
+        for (int i = 0; i < nums.length; i++) {
+            if (current.contains(nums[i])) continue; // Skip if the number is already in the current permutation
+            current.add(nums[i]);
+            findPermutation(nums, current, result);
+            current.remove(current.size() - 1); // Backtrack
         }
     }
+
     
 }
-
-// Title: Permutations
