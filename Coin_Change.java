fa88bcd7abfc4d7465f838861a1a72a4633aa0c1
Netal-Rathi <netalrathi2707@gmail.com>
2024-07-23 16:46:30 +0530

    Add Coin Change

---
+++ Coin_Change.java
@@ -0,0 +1,37 @@
+class Solution {
+    private int count;
+    private int minCount = Integer.MAX_VALUE;
+    private int idx;
+
+    public int coinChange(int[] coins, int amount) {
+       
+        count = 0;
+        idx=0;
+
+     
+        coinChangeHelper(coins, amount,idx);
+
+  
+        return minCount == Integer.MAX_VALUE ? -1 : minCount;
+    }
+
+    private void coinChangeHelper(int[] coins, int amount,int idx) {
+        if (amount == 0) {
+           
+            if (count < minCount) {
+                minCount = count;
+            }
+            return;
+        }
+
+        if (amount < 0) {
+            return;
+        }
+
+        for (int i = idx; i < coins.length; i++) {
+            count++;
+            coinChangeHelper(coins, amount - coins[i],i);
+            count--;
+        }
+    }
+}
