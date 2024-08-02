5f8b5a997cdf6f2033840fe1420594c87cddb51d
Netal-Rathi <netalrathi2707@gmail.com>
2024-08-02 16:18:25 +0530

    Add Minimum Swaps to Group All 1's Together II

--- Minimum_Swaps_to_Group_All_1's_Together_II.java
+++ Minimum_Swaps_to_Group_All_1's_Together_II.java
@@ -1,45 +1,54 @@
 class Solution {
     public int minSwaps(int[] nums) {
-        int n = nums.length;
-        int current = 0;
-        int maxOnesInWindow = 0;
-        int totalOnes = 0;
-
-        // Count the total number of 1s in the array
-        for (int num : nums) {
-            if (num == 1) {
-                totalOnes++;
+      
+        int n=nums.length;
+        int current =0;
+        int maxones=0;
+        int one=0;
+        for(int i=0;i<nums.length;i++){
+             if(nums[i]==1){
+                 one++;
             }
         }
 
-        // If there are no 1s, no swaps are needed
-        if (totalOnes == 0) {
+        if(one==0){
             return 0;
         }
 
-        // Initialize the window with the first 'totalOnes' elements
-        for (int i = 0; i < totalOnes; i++) {
-            if (nums[i] == 1) {
+        for(int i=0;i<one;i++){
+            if(nums[i]==1){
                 current++;
             }
-        }
-        maxOnesInWindow = current;
+            maxones=current;
 
-        // Sliding the window across the circular array
-        for (int i = totalOnes; i < n + totalOnes; i++) {
-            if (nums[i % n] == 1) {
+        }
+
+        for(int i=one;i<n+one;i++){
+            if(nums[i%n]==1){
                 current++;
             }
-            if (nums[(i - totalOnes) % n] == 1) {
+
+            if(nums[(i-one)%n]==1){
                 current--;
             }
-            maxOnesInWindow = Math.max(maxOnesInWindow, current);
+            maxones=Math.max(maxones,current);
+           
+                
+           
         }
+        System.out.println(one);
+         System.out.println(maxones);
 
+    
+            return one-maxones;
        
 
-        return totalOnes - maxOnesInWindow;
-    }
-}
 
-// Title: Minimum Swaps to Group All 1's Together II
+         
+
+        
+    }
+
+    
+        
+}
