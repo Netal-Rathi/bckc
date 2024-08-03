e85858a144d8689b7336284cf3e428645a8d20fc
Netal-Rathi <netalrathi2707@gmail.com>
2024-08-04 04:47:59 +0530

    Add Make Two Arrays Equal by Reversing Subarrays

--- Make_Two_Arrays_Equal_by_Reversing_Subarrays.java
+++ Make_Two_Arrays_Equal_by_Reversing_Subarrays.java
@@ -1,29 +1,16 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public boolean canBeEqual(int[] target, int[] arr) {
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        // Count frequencies of elements in target
+     Set<Integer> set = new HashSet<>();
         for (int num : target) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+            set.add(num);
         }
         
-        // Decrease frequencies based on arr
         for (int num : arr) {
-            if (!map.containsKey(num)) {
+            if (!set.contains(num)) {
                 return false;
             }
-            map.put(num, map.get(num) - 1);
-            if (map.get(num) == 0) {
-                map.remove(num);
-            }
         }
         
-        // Check if all counts are zero
-        return map.isEmpty();
+        return true;
     }
 }
-
-// Title: Make Two Arrays Equal by Reversing Subarrays
