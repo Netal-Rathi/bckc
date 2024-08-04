d8b466f2f3b50e7b6fc898afba552347d9bbd338
Netal-Rathi <netalrathi2707@gmail.com>
2024-08-05 03:20:39 +0530

    Add Kth Distinct String in an Array

---
+++ Kth_Distinct_String_in_an_Array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String kthDistinct(String[] arr, int k) {
+        HashMap<String,Integer> ans=new HashMap<>();
+        for (String s: arr){
+            ans.put(s,ans.getOrDefault(s,0)+1);
+
+        }
+        int i=0;
+        for (String s : arr){
+            if(ans.get(s)==1){
+                i++;
+
+            }
+            if(i==k){
+                return s;
+            }
+        }
+      
+
+        return "";
+
+
+        
+    }
+}
