a740fa0437509eae1881a7c9f0cd81b53e09eb0d
Netal-Rathi <netalrathi2707@gmail.com>
2024-08-07 16:27:50 +0530

    Add Integer to English Words

--- Integer_to_English_Words.java
+++ Integer_to_English_Words.java
@@ -1,44 +1,43 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-public class Solution {
+class Solution {
     public String numberToWords(int num) {
-        List<Integer> numbers = Arrays.asList(
-            1000000000, 1000000, 1000, 100, 90, 80, 70, 60, 50, 40, 30, 20,
-            19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
-        );
-
-        List<String> words = Arrays.asList(
-            "Billion", "Million", "Thousand", "Hundred", "Ninety", "Eighty",
-            "Seventy", "Sixty", "Fifty", "Forty", "Thirty", "Twenty",
-            "Nineteen", "Eighteen", "Seventeen", "Sixteen", "Fifteen", "Fourteen",
-            "Thirteen", "Twelve", "Eleven", "Ten", "Nine", "Eight", "Seven",
-            "Six", "Five", "Four", "Three", "Two", "One"
-        );
-
-        if (num == 0) return "Zero";
-
-        StringBuilder result = new StringBuilder();
-
-        for (int i = 0; i < numbers.size(); i++) {
-            if (num >= numbers.get(i)) {
-                if (num >= 100) {
-                    result.append(numberToWords(num / numbers.get(i))).append(" ").append(words.get(i)).append(" ");
-                } else {
-                    result.append(words.get(i)).append(" ");
-                }
-
-                num %= numbers.get(i);
-
-                if (num == 0) break;
-            }
-        }
-
-        return result.toString().trim();
+    if(num == 0)
+        return "Zero";
+    String[] bigString = new String[]{"Thousand","Million","Billion"};
+    String result =  numberToWordsHelper(num%1000);
+    num = num/1000;
+    if(num > 0 && num%1000>0){
+        result = numberToWordsHelper(num%1000) + "Thousand " + result;
     }
-
-  
+    num = num/1000;
+    if(num > 0 && num%1000>0){
+        result = numberToWordsHelper(num%1000) + "Million " + result;
+    }
+    num = num/1000;
+    if(num > 0){
+        result = numberToWordsHelper(num%1000) + "Billion " + result;
+    }
+    return result.trim();
 }
 
-// Title: Integer to English Words
+public String numberToWordsHelper(int num){
+    String[] digitString = new String[]{"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    String[] teenString = new String[]{"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen","Eighteen", "Nineteen"};
+    String[] tenString = new String[]{"","","Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    String result = "";
+    if(num > 99){
+        result += digitString[num/100] + " Hundred ";
+    }
+    num = num % 100;
+    if(num < 20 && num > 9){
+        result += teenString[num%10]+" ";
+    }else{
+        if(num > 19){
+            result += tenString[num/10]+" ";
+        }
+        num = num % 10;
+        if(num > 0)
+            result += digitString[num]+" ";
+    }
+    return result;
+}
+}
