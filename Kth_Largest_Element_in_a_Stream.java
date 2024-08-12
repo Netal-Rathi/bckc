015c95bd418486d70ab7bd24346bc520eb44d1ac
Netal-Rathi <netalrathi2707@gmail.com>
2024-08-12 15:46:32 +0530

    Add Kth Largest Element in a Stream

--- Kth_Largest_Element_in_a_Stream.java
+++ Kth_Largest_Element_in_a_Stream.java
@@ -1,33 +1,34 @@
+import java.util.Arrays;
+
 class KthLargest {
+    private int k;
+    private int[] nums;
 
-private int k;
-private int nums[];
     public KthLargest(int k, int[] nums) {
-        this.k=k;
-        this.nums=nums;
-            Arrays.sort(this.nums);
-           // return nums[k];
-       
-        }       
-    
-    
-    public int add(int val) {
-       int ans[]=new int[nums.length+1];
-       for(int i=0;i<nums.length;i++){
-        ans[i]=nums[i];
-       }
-       ans[nums.length]=val;
-       Arrays.sort(ans);
-       nums=ans;
-       return nums[ans.length-k];
+        this.k = k;
+        this.nums = nums;
+        Arrays.sort(this.nums);  // Sort the array
+    }
 
+    public int add(int val) {
+        // Create a new array to accommodate the new value
+        int[] newNums = new int[nums.length + 1];
         
+        // Copy existing elements to the new array
+        for (int i = 0; i < nums.length; i++) {
+            newNums[i] = nums[i];
+        }
+        
+        // Add the new value
+        newNums[nums.length] = val;
+
+        // Sort the new array
+        Arrays.sort(newNums);
+
+        // Update the nums array to the newly sorted array
+        nums = newNums;
+
+        // Return the k-th largest element
+        return nums[nums.length - k];
     }
 }
-
-/**
- * Your KthLargest object will be instantiated and called as such:
- * KthLargest obj = new KthLargest(k, nums);
- * int param_1 = obj.add(val);
- */
-// Title: Kth Largest Element in a Stream
