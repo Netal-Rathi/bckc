8dfc086604e90b550b9c29764e7acc31e34f5ebc
Netal-Rathi <netalrathi2707@gmail.com>
2024-08-17 03:31:28 +0530

    Add Maximum Number of Points with Cost

--- Maximum_Number_of_Points_with_Cost.java
+++ Maximum_Number_of_Points_with_Cost.java
@@ -1,31 +1,27 @@
 class Solution {
     public long maxPoints(int[][] points) {
-        long ans=0;
-       // long max=Long.MIN_VALUE;
-       int col1=-1;
-       
-     
-   
-        for(int i=0;i<points.length;i++){
-              long max=Long.MIN_VALUE;
-              int col2=-1;
-            for(int j=0;j<points[i].length;j++){
-                if(points[i][j]>max){
-                    max=points[i][j];
-                     col2=j;
+        long ans = 0;
+        int col1 = -1;
 
+        for (int i = 0; i < points.length; i++) {
+            long max = Long.MIN_VALUE; // Reset max for each row
+            int col2 = -1;
+
+            for (int j = 0; j < points[i].length; j++) {
+                if (points[i][j] > max) {
+                    max = points[i][j];
+                    col2 = j; // Set col2 to the current column index of the max value
                 }
+            }
 
+            ans = ans + max;
+
+            if (col1 != -1) {
+                ans = ans - Math.abs(col2 - col1); // Subtract the difference between col2 and col1
             }
-           ans=ans+max;
-            if(col1!=-1 || col1!=0){
-                ans=ans-Math.abs(col2-col1);
-                
-            }
-            col1=col2;
+
+            col1 = col2; // Update col1 to the latest column of the max value
         }
         return ans;
-        
     }
 }
-// Title: Maximum Number of Points with Cost
