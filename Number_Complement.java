ca66f12ee28a0882723444f39bcfef6fbc9af635
Netal-Rathi <netalrathi2707@gmail.com>
2024-08-22 17:23:18 +0530

    Add Number Complement

---
+++ Number_Complement.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findComplement(int num) {
+        // Get the binary length of num
+        int bitLength = Integer.toBinaryString(num).length();
+        
+        // Create a mask with all bits set to 1 of the same length as num's binary representation
+        int mask = (1 << bitLength) - 1;
+        
+        // XOR num with the mask to flip all bits
+        return num ^ mask;
+    }
+}
