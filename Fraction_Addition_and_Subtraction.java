2c4362f7ae4ee2eff355b9b16f0a1a9ca4ededaa
Netal-Rathi <netalrathi2707@gmail.com>
2024-08-23 16:53:37 +0530

    Add Fraction Addition and Subtraction

--- Fraction_Addition_and_Subtraction.java
+++ Fraction_Addition_and_Subtraction.java
@@ -1,58 +1,31 @@
-import java.math.BigInteger;
-
 class Solution {
     public String fractionAddition(String expression) {
-        BigInteger num = BigInteger.ZERO;
-        BigInteger den = BigInteger.ONE;
-        int i = 0;
-        int n = expression.length();
+        int numerator = 0, denominator = 1;
         
-        while (i < n) {
-            int sign = 1;
+        Pattern pattern = Pattern.compile("([+-]?\\d+)/(\\d+)");
+        Matcher matcher = pattern.matcher(expression);
+        
+        while (matcher.find()) {
+            int num = Integer.parseInt(matcher.group(1));
+            int den = Integer.parseInt(matcher.group(2));
             
-            // Determine the sign of the current fraction
-            if (expression.charAt(i) == '-') {
-                sign = -1;
-                i++;
-            } else if (expression.charAt(i) == '+') {
-                i++;
-            }
+            numerator = numerator * den + num * denominator;
+            denominator *= den;
             
-            // Extract the numerator
-            int j = i;
-            while (j < n && Character.isDigit(expression.charAt(j))) {
-                j++;
-            }
-            BigInteger numerator = new BigInteger(expression.substring(i, j)).multiply(BigInteger.valueOf(sign));
-            
-            // Move past the '/' character
-            i = j + 1;
-            
-            // Extract the denominator
-            j = i;
-            while (j < n && Character.isDigit(expression.charAt(j))) {
-                j++;
-            }
-            BigInteger denominator = new BigInteger(expression.substring(i, j));
-            
-            // Calculate the new denominator (LCM of current and new denominator)
-            BigInteger commonDen = den.multiply(denominator).divide(den.gcd(denominator));
-            
-            // Adjust the numerators to the new common denominator and add them
-            num = num.multiply(commonDen.divide(den)).add(numerator.multiply(commonDen.divide(denominator)));
-            den = commonDen;
-            
-            // Move to the next part of the expression
-            i = j;
+            int gcdVal = gcd(Math.abs(numerator), denominator);
+            numerator /= gcdVal;
+            denominator /= gcdVal;
         }
         
-        // Simplify the fraction
-        BigInteger gcd = num.gcd(den);
-        num = num.divide(gcd);
-        den = den.divide(gcd);
-        
-        return num.toString() + "/" + den.toString();
+        return numerator + "/" + denominator;
+    }
+    
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
 }
-
-// Title: Fraction Addition and Subtraction
