48bb4f10fff9569045348459243e5b39f2273c57
Netal-Rathi <netalrathi2707@gmail.com>
2024-10-17 12:45:23 +0530

    Add Maximum Swap

---
+++ Maximum_Swap.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maximumSwap(int num) {
+       int T=0;
+        StringBuilder s=new StringBuilder(String.valueOf(num));
+       //String s=String.valueOf(num);
+       while(T!=s.length()){
+       char large=s.charAt(T);
+       int no=T;
+       for(int i=T;i<s.length();i++){
+        if(large<=s.charAt(i)){
+            large=s.charAt(i);
+            no=i;
+        }
+
+       }
+       if(s.charAt(T)==large){
+        T++;
+          }else{
+            char temp=s.charAt(T);
+            s.setCharAt(T,s.charAt(no));
+            s.setCharAt(no,temp);
+            return Integer.parseInt(s.toString());
+          }
+       
+        
+
+    }
+    return num;
+}
+}
