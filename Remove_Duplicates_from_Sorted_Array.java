8ba2b720d684efc3cbd880bed948a5e31c6e76a3
Netal-Rathi <netalrathi2707@gmail.com>
2024-10-18 20:42:35 +0530

    Add Remove Duplicates from Sorted Array

---
+++ Remove_Duplicates_from_Sorted_Array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        
+        List <Integer> l1=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if(l1.isEmpty() || l1.get(l1.size()-1)!=nums[i]){
+               l1.add(nums[i]);
+            }
+        }
+
+        for(int i=0;i<l1.size();i++){
+              nums[i] = l1.get(i);
+        }
+
+
+        for (int i=l1.size();i<nums.length;i++){
+            nums[i]='_';
+        }
+        return l1.size();
+    }
+}
