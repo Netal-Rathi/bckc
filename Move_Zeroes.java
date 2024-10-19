24279a106f3b3baa616b92a10b501cdec62f252e
Netal-Rathi <netalrathi2707@gmail.com>
2024-10-20 05:08:10 +0530

    Add Move Zeroes

--- Move_Zeroes.java
+++ Move_Zeroes.java
@@ -2,7 +2,7 @@ class Solution {
     public void moveZeroes(int[] nums) {
     for(int i=0;i<nums.length;i++){
         for(int j=i;j<nums.length;j++){
-            if(nums[i]==0 && nums[j]!=0){
+            if(nums[i]==0 && nums[j]>0){
                 int temp=nums[i];
                 nums[i]=nums[j];
                 nums[j]=temp;
@@ -13,4 +13,3 @@ class Solution {
     }
     }
 }
-// Title: Move Zeroes
