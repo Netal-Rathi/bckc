53747ba9891cfcfb8af4cc1fc4436002652f9070
Netal-Rathi <netalrathi2707@gmail.com>
2024-10-24 17:59:11 +0530

    Add Majority Element

---
+++ Majority_Element.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+    HashMap<Integer,Integer> map=new HashMap<>();
+for(int i=0;i<nums.length;i++){
+    if(map.containsKey(nums[i])){
+        map.put(nums[i],map.get(nums[i])+1);
+    }else{
+        map.put(nums[i],1);
+    }
+}
+            int maxvalue=0;
+            int maxno=0;
+        
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            if(entry.getValue()>maxvalue){
+                maxvalue=entry.getValue();
+                maxno=entry.getKey();
+            }
+        }
+        return maxno;
+    }
+}
