7d33745fb011406daec8525c9c5215b45f10c8fb
Netal-Rathi <netalrathi2707@gmail.com>
2024-11-05 09:44:26 +0530

    Add Minimum Window Substring

--- Minimum_Window_Substring.java
+++ Minimum_Window_Substring.java
@@ -1,52 +1,35 @@
-import java.util.HashMap;
-
 class Solution {
     public String minWindow(String s, String t) {
-        if (s == null || t == null || s.length() == 0 || t.length() == 0 || s.length() < t.length()) {
-            return "";
+        if (s == null || t == null || s.length() == 0 || t.length() == 0 ||
+                s.length() < t.length()) {
+            return new String();
         }
-        
-        // HashMap to store the frequency of characters in t
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (char c : t.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
-        }
-        
+        int[] map = new int[128];
         int count = t.length();
-        int start = 0, end = 0;
-        int minLen = Integer.MAX_VALUE;
-        int startIndex = 0;
+        int start = 0, end = 0, minLen = Integer.MAX_VALUE, startIndex = 0;
+        
+        for (char c : t.toCharArray()) {
+            map[c]++;
+        }
 
-        while (end < s.length()) {
-            char endChar = s.charAt(end);
-            if (map.containsKey(endChar)) {
-                if (map.get(endChar) > 0) {
-                    count--;
-                }
-                map.put(endChar, map.get(endChar) - 1);
+        char[] chS = s.toCharArray();
+
+        while (end < chS.length) {
+            if (map[chS[end++]]-- > 0) {
+                count--;
             }
-            end++;
-
             while (count == 0) {
-                // Update the minimum window length
                 if (end - start < minLen) {
                     startIndex = start;
                     minLen = end - start;
                 }
-                
-                char startChar = s.charAt(start);
-                if (map.containsKey(startChar)) {
-                    map.put(startChar, map.get(startChar) + 1);
-                    if (map.get(startChar) > 0) {
-                        count++;
-                    }
+                if (map[chS[start++]]++ == 0) {
+                    count++;
                 }
-                start++;
             }
         }
 
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(startIndex, startIndex + minLen);
+        return minLen == Integer.MAX_VALUE ? new String() :
+                new String(chS, startIndex, minLen);
     }
 }
-
-// Title: Minimum Window Substring
