8ab8fe2b6374a32da92f9e012ddb79a84223cae9
Netal-Rathi <netalrathi2707@gmail.com>
2024-11-05 12:02:53 +0530

    Add Camelcase Matching

---
+++ Camelcase_Matching.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Boolean> camelMatch(String[] queries, String pattern) {
+        List<Boolean> ans = new ArrayList<>();
+        
+        for (String query : queries) {
+            int j = 0; // Pointer for pattern
+            boolean isMatch = true;
+            
+            for (int k = 0; k < query.length(); k++) {
+                char queryChar = query.charAt(k);
+                
+                if (j < pattern.length() && queryChar == pattern.charAt(j)) {
+                    j++;
+                } else if (Character.isUpperCase(queryChar)) {
+                    isMatch = false;
+                    break;
+                }
+                
+                if (k == query.length() - 1) {
+                    if (j != pattern.length()) {
+                        isMatch = false;
+                    }
+                }
+            }
+            
+            ans.add(isMatch);
+        }
+        
+        return ans;
+    }
+}
