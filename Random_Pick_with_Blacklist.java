9cda125a8d0141dea1a40c5c97710afbbc897d60
Netal-Rathi <netalrathi2707@gmail.com>
2024-11-05 10:05:07 +0530

    Add Random Pick with Blacklist

---
+++ Random_Pick_with_Blacklist.java
@@ -0,0 +1,31 @@
+class Solution {
+    Map<Integer, Integer> map;
+    Random rand = new Random();
+    int size;
+
+    public Solution(int n, int[] blacklist) {
+        map = new HashMap<>();
+        size = n - blacklist.length;
+        int last = n - 1;
+        for (int b: blacklist) {
+            map.put(b, -1);
+        }
+        for (int b: blacklist) {
+            if (b >= size) {
+                continue;
+            }
+            while (map.containsKey(last)) {
+                last--;
+            }
+            map.put(b, last);
+            last--;
+        }
+    }
+    public int pick() {
+        int idx = rand.nextInt(size);
+        if (map.containsKey(idx)) {
+            return map.get(idx);
+        }
+        return idx;
+    }
+}
