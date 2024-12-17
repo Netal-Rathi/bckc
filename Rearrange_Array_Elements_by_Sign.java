7409905c203ac344f883121000ef88ee4f1ed584
Netal-Rathi <netalrathi2707@gmail.com>
2024-12-18 04:17:23 +0530

    Add Rearrange Array Elements by Sign

--- Rearrange_Array_Elements_by_Sign.java
+++ Rearrange_Array_Elements_by_Sign.java
@@ -1,21 +1,50 @@
 class Solution {
-    public int[] rearrangeArray(int[] nums) {
-        int n = nums.length;
-        int[] result = new int[n]; // Result array to store the rearranged elements
-        int posIndex = 0, negIndex = 1; // Start indexes for positive and negative numbers
+    private int next_negative(int[] nums, int j) {
+        for (int i = j + 1; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                return i;
+            }
+        }
+        return -1; // Return -1 if no negative number is found
+    }
 
-        for (int num : nums) {
-            if (num > 0) {
-                result[posIndex] = num; // Place positive numbers at even indices
-                posIndex += 2;
-            } else {
-                result[negIndex] = num; // Place negative numbers at odd indices
-                negIndex += 2;
+    private int next_positive(int[] nums, int j) {
+        for (int i = j + 1; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                return i;
+            }
+        }
+        return -1; // Return -1 if no positive number is found
+    }
+
+    public int[] rearrangeArray(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i += 2) {
+            while (!(nums[i] > 0 && nums[i + 1] < 0)) {
+                if (nums[i] > 0 && nums[i + 1] > 0) {
+                    int p = next_negative(nums, i + 1);
+                    if (p == -1) break; // No further negatives
+                    // Instead of shifting elements one by one, swap directly
+                    int temp = nums[p];
+                    nums[p] = nums[i + 1];
+                    nums[i + 1] = temp;
+
+                } else if (nums[i] < 0 && nums[i + 1] < 0) {
+                    int p = next_positive(nums, i + 1);
+                    if (p == -1) break; // No further positives
+                    // Instead of shifting elements one by one, swap directly
+                    int temp = nums[p];
+                    nums[p] = nums[i + 1];
+                    nums[i + 1] = temp;
+
+                } else {
+                    // If mismatched, swap directly
+                    int temp = nums[i + 1];
+                    nums[i + 1] = nums[i];
+                    nums[i] = temp;
+                }
             }
         }
 
-        return result;
+        return nums;
     }
 }
-
-// Title: Rearrange Array Elements by Sign
