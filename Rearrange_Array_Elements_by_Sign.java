e130d9836b8df96ecfd2bc23e5a3a8ee53bfdf70
Netal-Rathi <netalrathi2707@gmail.com>
2024-12-18 04:18:33 +0530

    Add Rearrange Array Elements by Sign

--- Rearrange_Array_Elements_by_Sign.java
+++ Rearrange_Array_Elements_by_Sign.java
@@ -1,20 +1,19 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-       int s=nums.length;
-       int result[]=new int[s];
-       int p=0;
-       int n=1;
-       for(int i=0;i<s;i++){
-        if(nums[i]>0){
-            result[p]=nums[i];
-            p+=2;
-        }else{
-            result[n]=nums[i];
-            n+=2;
+        int n = nums.length;
+        int[] result = new int[n]; // Result array to store the rearranged elements
+        int posIndex = 0, negIndex = 1; // Start indexes for positive and negative numbers
+
+        for (int num : nums) {
+            if (num > 0) {
+                result[posIndex] = num; // Place positive numbers at even indices
+                posIndex += 2;
+            } else {
+                result[negIndex] = num; // Place negative numbers at odd indices
+                negIndex += 2;
+            }
         }
-       }
-       return result;
+
+        return result;
     }
 }
-
-// Title: Rearrange Array Elements by Sign
