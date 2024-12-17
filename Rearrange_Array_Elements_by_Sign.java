a9fed8909d89e9fe6270e7c3b6eb64caa92911ca
Netal-Rathi <netalrathi2707@gmail.com>
2024-12-18 04:12:33 +0530

    Add Rearrange Array Elements by Sign

--- Rearrange_Array_Elements_by_Sign.java
+++ Rearrange_Array_Elements_by_Sign.java
@@ -1,52 +1,60 @@
 class Solution {
-    private int next_negative(int[] nums, int j) {
-        for (int i = j + 1; i < nums.length; i++) {
-            if (nums[i] < 0) {
-                return i;
-            }
-        }
-        return -1; // Return -1 if no negative number is found
-    }
-
-    private int next_positive(int[] nums, int j) {
-        for (int i = j + 1; i < nums.length; i++) {
-            if (nums[i] > 0) {
-                return i;
-            }
-        }
-        return -1; // Return -1 if no positive number is found
-    }
-
-    public int[] rearrangeArray(int[] nums) {
-        for (int i = 0; i < nums.length - 1; i += 2) {
-            while (!(nums[i] > 0 && nums[i + 1] < 0)) {
-                if (nums[i] > 0 && nums[i + 1] > 0) {
-                    int p = next_negative(nums, i + 1);
-                    if (p == -1) break; // No further negatives
-                    // Instead of shifting elements one by one, swap directly
-                    int temp = nums[p];
-                    nums[p] = nums[i + 1];
-                    nums[i + 1] = temp;
-
-                } else if (nums[i] < 0 && nums[i + 1] < 0) {
-                    int p = next_positive(nums, i + 1);
-                    if (p == -1) break; // No further positives
-                    // Instead of shifting elements one by one, swap directly
-                    int temp = nums[p];
-                    nums[p] = nums[i + 1];
-                    nums[i + 1] = temp;
-
-                } else {
-                    // If mismatched, swap directly
-                    int temp = nums[i + 1];
-                    nums[i + 1] = nums[i];
-                    nums[i] = temp;
+      private int next_negative (int[] nums,int j){
+            for(int i=j+1;i<nums.length;i++){
+                if(nums[i]<0){
+                    return i;
                 }
             }
+            return -1;
         }
 
+         private int next_positive (int[] nums,int j){
+            for(int i=j+1;i<nums.length;i++){
+                if(nums[i]>0){
+                    return i;
+                }
+            }
+            return -1;
+        }
+    public int[] rearrangeArray(int[] nums) {
+        
+      
+
+        for(int i=0;i<nums.length-1;i+=2){
+            while(!(nums[i]>0 && nums[i+1]<0)){
+                
+            if(nums[i]>0 && nums[i+1]>0){
+             int p=next_negative(nums,i);
+             int temp=nums[p];
+             for(int k=p;k>i+1;k--){
+                nums[k]=nums[k-1];
+
+               }
+               nums[i+1]=temp;
+                
+            }else if(nums[i]<0 && nums[i+1]<0){
+                int p=next_positive(nums,i);
+               int temp=nums[p];
+               
+               for(int k=p;k>i+1;k--){
+                nums[k]=nums[k-1];
+
+               }
+               nums[i+1]=temp;
+
+
+
+            }else{
+                int temp=nums[i+1];
+                nums[i+1]=nums[i];
+                nums[i]=temp;
+            }
+        }
+
+
+
+        }
         return nums;
+
     }
 }
-
-// Title: Rearrange Array Elements by Sign
