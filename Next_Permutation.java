17dddc06e5aca67b56111fab48fc3cbf97f127e2
Netal-Rathi <netalrathi2707@gmail.com>
2024-12-20 17:35:28 +0530

    Add Next Permutation

---
+++ Next_Permutation.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int count = 0;
+
+        
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i + 1] > nums[i]) {
+                count++;
+            }
+        }
+
+
+        if (count == 0) {
+            Arrays.sort(nums);
+            return;
+        }
+int i;
+        for(i=nums.length-1;i>0;i--){
+            if(nums[i]>nums[i-1]){
+                for(int j=nums.length-1;j>=i;j--){
+                    if(nums[j]>nums[i-1]){
+                    int temp=nums[j];
+                    nums[j]=nums[i-1];
+                    nums[i-1]=temp;
+                    //break;
+                    }
+                   // break;
+                }
+                break;
+            }
+        }
+
+        
+        Arrays.sort(nums, i, nums.length);
+    }
+} 
