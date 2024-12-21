81199a072fda3734f44e1064fb177c9ef5d72ba1
Netal-Rathi <netalrathi2707@gmail.com>
2024-12-21 11:46:23 +0530

    Add Set Matrix Zeroes

--- Set_Matrix_Zeroes.java
+++ Set_Matrix_Zeroes.java
@@ -1,38 +1,36 @@
+import java.util.HashSet;
+
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int m=matrix.length;
-        int n=matrix[0].length;
-        HashSet<Integer> row_h=new HashSet<>();
-        HashSet<Integer> clm_h=new HashSet<>();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if (matrix[i][j]==0 ){
-                     row_h.add(i);
-                     clm_h.add(j);
-                    
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        // Use sets to store the rows and columns that need to be zeroed
+        HashSet<Integer> zeroRows = new HashSet<>();
+        HashSet<Integer> zeroCols = new HashSet<>();
+
+        // First pass: Identify all rows and columns to be zeroed
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    zeroRows.add(i);
+                    zeroCols.add(j);
                 }
             }
         }
 
-                    for(int row:row_h){
-              for(int l=0;l<n;l++){
-                matrix[row][l]=0;
-              }
-                }
+        // Second pass: Set rows to zero
+        for (int row : zeroRows) {
+            for (int j = 0; j < n; j++) {
+                matrix[row][j] = 0;
+            }
+        }
 
-
-                for(int col:clm_h){
-              for(int k=0;k<m;k++){
-                matrix[k][col]=0;
-              }
-                }
-
-             
-
-             //   System.out.print(matrix);
-            
-        
-     
+        // Third pass: Set columns to zero
+        for (int col : zeroCols) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][col] = 0;
+            }
+        }
     }
 }
-// Title: Set Matrix Zeroes
