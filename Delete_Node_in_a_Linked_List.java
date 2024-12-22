2a1fa4b0db4c3a9d44095dda3b931e0cabceb342
Netal-Rathi <netalrathi2707@gmail.com>
2024-12-22 18:52:40 +0530

    Add Delete Node in a Linked List

--- Delete_Node_in_a_Linked_List.java
+++ Delete_Node_in_a_Linked_List.java
@@ -8,21 +8,15 @@
  */
 class Solution {
     public void deleteNode(ListNode node) {
-       if(node==null ){
-        return ;
-       }
-
-       if (node.next==null){
-        return ;
-
-       }
-
-       node.val=node.next.val;
-       node.next=node.next.next;
-
-       
-         
-
+        // Ensure the node is not null and is not the last node
+        if (node == null || node.next == null) {
+            return;
+        }
+        
+        // Copy the value from the next node to the current node
+        node.val = node.next.val;
+        
+        // Bypass the next node
+        node.next = node.next.next;
     }
 }
-// Title: Delete Node in a Linked List
