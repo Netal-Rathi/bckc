5515828d95abf8c35729d594df9193ab2fb8bcb7
Netal-Rathi <netalrathi2707@gmail.com>
2024-12-31 13:43:57 +0530

    Add Longest Common Prefix

--- Longest_Common_Prefix.java
+++ Longest_Common_Prefix.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=0;i<strs[0].length();i++){
             char current=strs[0].charAt(i);
             for(int j=1;j<strs.length;j++){
-            if(i >= strs[j].length() || current!=strs[j].charAt(i)){
+            if(current!=strs[j].charAt(i)){
                 return ans;
             }
         }
@@ -15,4 +15,3 @@ class Solution {
     return ans;
 }
 }
-// Title: Longest Common Prefix
