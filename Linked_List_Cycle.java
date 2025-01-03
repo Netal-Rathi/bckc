f830f1e479d4499abdd707599336172c4bfb3367
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-04 03:14:23 +0530

    Add Linked List Cycle

--- Linked_List_Cycle.java
+++ Linked_List_Cycle.java
@@ -11,36 +11,23 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        // hash map 
-    //     HashMap<ListNode,Integer> map=new HashMap<>();
-    //     if(head==null|| head.next==null){
-    //         return false;
-    //     }
-    //     while(head!=null){
-    //       //  map.put(head,map.getorDefault(head,0)+1)
-    //         if(map.containsKey(head)){
-    //             return true;
-    //         }else{
-    //             map.put(head,1);
-    //             head=head.next;
-
-    //         }
-           
-    //     }
-    //   return false;
-    ListNode slow=head;
-    ListNode fast=head;
-
-    while(slow!=null && fast!=null && fast.next!=null){
-        slow=slow.next;
-        fast=fast.next.next;
-        if(slow==fast){
-            return true;
+        HashMap<ListNode,Integer> map=new HashMap<>();
+        if(head==null|| head.next==null){
+            return false;
         }
+        while(head!=null){
+          //  map.put(head,map.getorDefault(head,0)+1)
+            if(map.containsKey(head)){
+                return true;
+            }else{
+                map.put(head,1);
+                head=head.next;
 
-    }
-        return false;
+            }
+           
+        }
+      return false;
+        
         
     }
 }
-// Title: Linked List Cycle
