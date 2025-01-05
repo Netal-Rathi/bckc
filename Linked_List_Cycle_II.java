35fe7cd1e70e6fee3c82ae3260cb9951d626b8bd
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-05 09:30:42 +0530

    Add Linked List Cycle II

---
+++ Linked_List_Cycle_II.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+
+     ListNode slow=head;
+    ListNode fast=head;
+
+    while(slow!=null && fast!=null && fast.next!=null){
+        slow=slow.next;
+        fast=fast.next.next;
+        
+        if(slow==fast){
+          slow=head;
+          while(slow!=fast){
+            slow=slow.next;
+            fast=fast.next;
+          }
+          return slow;
+
+        }
+
+    }        
+    return null;
+
+    // hashset 
+    // HashMap<ListNode,Integer> map=new HashMap<>();
+    // while(head!=null){
+    //     if(map.containsKey(head)){
+    //         return head;
+    //     }else{
+    //          map.put(head,1);
+    //          head=head.next;
+    //     }
+    // }
+    // return null;
+    
+
+
+    }
+}
