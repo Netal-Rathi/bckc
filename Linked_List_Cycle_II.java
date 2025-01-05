af3084a7f7212ab0406d1eeca8f78b17ec8efe1a
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-05 09:23:00 +0530

    Add Linked List Cycle II

--- Linked_List_Cycle_II.java
+++ Linked_List_Cycle_II.java
@@ -12,40 +12,34 @@
 public class Solution {
     public ListNode detectCycle(ListNode head) {
 
-     ListNode slow=head;
-    ListNode fast=head;
+    //  ListNode slow=head;
+    // ListNode fast=head;
 
-    while(slow!=null && fast!=null && fast.next!=null){
-        slow=slow.next;
-        fast=fast.next.next;
-        
-        if(slow==fast){
-          slow=head;
-          while(slow!=fast){
-            slow=slow.next;
-            fast=fast.next;
-          }
-          return slow;
-
-        }
-
-    }        
-    return null;
-
-    // hashset 
-    // HashMap<ListNode,Integer> map=new HashMap<>();
-    // while(head!=null){
-    //     if(map.containsKey(head)){
-    //         return head;
-    //     }else{
-    //          map.put(head,1);
-    //          head=head.next;
+    // while(slow!=null && fast!=null && fast.next!=null){
+    //     slow=slow.next;
+    //     fast=fast.next.next;
+    //     if(slow==head){
+    //         return slow;
     //     }
-    // }
+    //     if(slow==fast){
+    //        slow=slow.next;
+    //         return slow;
+    //     }
+
+    // }        
     // return null;
+    HashMap<ListNode,Integer> map=new HashMap<>();
+    while(head!=null){
+        if(map.containsKey(head)){
+            return head;
+        }else{
+             map.put(head,1);
+             head=head.next;
+        }
+    }
+    return null;
     
 
 
     }
 }
-// Title: Linked List Cycle II
