85a098e6c1b68c893510560d9f99374118ec5ded
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-06 18:32:32 +0530

    Add Intersection of Two Linked Lists

---
+++ Intersection_of_Two_Linked_Lists.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        // HASH-map uses extra space O(n) to store values 
+        // HashMap<ListNode,Integer> map =new HashMap<>();
+        // while(headA!=null){
+        //     if(map.containsKey(headA)){
+        //         return headA;
+        //     }else{
+        //         map.put(headA,1);
+        //         headA=headA.next;
+        //     }
+
+        // }        
+
+        // while(headB!=null){
+
+        // if(map.containsKey(headB)){
+        //         return headB;
+        //     }else{
+        //         map.put(headB,1);
+        //         headB=headB.next;
+        //     }
+        // }
+        // return null;
+        ListNode pA=headA,pB=headB;
+
+
+        while(pA!=pB){
+            if(pA!=null){
+                pA=pA.next;
+            }else{
+              pA= headB;
+            }  
+
+               if(pB!=null){
+                pB=pB.next;
+            }else{
+                pB=headA;
+            }
+        }
+        return pA;
+    }
+}
