d0095ae6a3413235630f8c075b7ac612afca8773
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-06 18:08:20 +0530

    Add Intersection of Two Linked Lists

--- Intersection_of_Two_Linked_Lists.java
+++ Intersection_of_Two_Linked_Lists.java
@@ -1,18 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        // Initialize two pointers for the two lists.
-        ListNode pointerA = headA;
-        ListNode pointerB = headB;
+        HashMap<ListNode,Integer> map =new HashMap<>();
+        while(headA!=null){
+            if(map.containsKey(headA)){
+                return headA;
+            }else{
+                map.put(headA,1);
+                headA=headA.next;
+            }
 
-        // Traverse both lists. When one pointer reaches the end, redirect it to the head of the other list.
-        while (pointerA != pointerB) {
-            pointerA = (pointerA == null) ? headB : pointerA.next;
-            pointerB = (pointerB == null) ? headA : pointerB.next;
+        }        
+
+        while(headB!=null){
+
+        if(map.containsKey(headB)){
+                return headB;
+            }else{
+                map.put(headB,1);
+                headB=headB.next;
+            }
         }
-
-        // The intersection node is either the intersection point or null if there's no intersection.
-        return pointerA;
+        return null;
     }
 }
-
-// Title: Intersection of Two Linked Lists
