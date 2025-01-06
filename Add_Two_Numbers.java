11026848e58b3614da12a616f7cfbf95b3596b6d
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-07 02:51:40 +0530

    Add Add Two Numbers

--- Add_Two_Numbers.java
+++ Add_Two_Numbers.java
@@ -1,42 +1,42 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if(l1==null || l2==null){
-            return null;
-        }
-        
-        ListNode l3=new ListNode(0);
-        ListNode temp=l3;
-        int carry=0;
-        while(l1!=null || l2!=null || carry!=0){
-            int sum=carry;
-            if(l1!=null){
-                sum+=l1.val;
-                l1=l1.next;
+        ListNode l3 = new ListNode(0);  // Dummy node
+        ListNode temp = l3;  // Pointer to build the result
+        int carry = 0;
+
+        // Traverse both lists until all digits and carry are processed
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = carry;  // Start with the carry value
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;  // Move to the next node in l1
             }
 
-            if(l2!=null){
-                sum+=l2.val;
-                l2=l2.next;
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;  // Move to the next node in l2
             }
-         carry=sum/10;
-             temp = temp.next = new ListNode(sum % 10);
-            carry=sum/10;
-           // temp=temp.next;
+
+            carry = sum / 10;  // Calculate carry for the next addition
+            temp = temp.next = new ListNode(sum % 10);  // Create new node for the current digit and move temp
         }
-       return (l3.next);
+
+        return l3.next;  // Reverse the result list before returning
     }
 
-   
-}
+    // Function to reverse the linked list
+    // public ListNode reverse(ListNode head) {
+    //     ListNode prev = null, current = head, next = null;
 
-// Title: Add Two Numbers
+    //     // Traverse the list and reverse the links
+    //     while (current != null) {
+    //         next = current.next;  
+    //         current.next = prev;  // Reverse the link
+    //         prev = current;  // Move prev to current
+    //         current = next;  // Move current to next
+    //     }
+
+    //     return prev;  // Return the new head of the reversed list
+    // }
+}
