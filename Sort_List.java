8795b73703f66f72fcc471b22322ac9594de556e
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-06 15:12:46 +0530

    Add Sort List

--- Sort_List.java
+++ Sort_List.java
@@ -11,41 +11,28 @@
 class Solution {
     public ListNode sortList(ListNode head) {
         if (head == null || head.next == null) {
-            return head; // Already sorted if the list is empty or has only one element
+            return head; // If the list is empty or has only one node, it's already sorted
         }
 
-        ListNode temp = head;
+        boolean swapped;
+        do {
+            swapped = false;
+            ListNode current = head;
+            ListNode prev = null;
 
-        // Continue sorting until no swaps are needed in a complete pass
-        while (temp != null) {
-            ListNode current = temp;
-            boolean isSorted = true; // Assume that the list is sorted in this pass
-
-            // Traverse the list starting from temp
             while (current != null && current.next != null) {
                 if (current.val > current.next.val) {
-                    // Swap the values between current node and next node
-                    int tempVal = current.val;
+                    // Swap values
+                    int temp = current.val;
                     current.val = current.next.val;
-                    current.next.val = tempVal;
-
-                    // Since we made a swap, set isSorted to false
-                    isSorted = false;
+                    current.next.val = temp;
+                    swapped = true;
                 }
-                current = current.next; // Move to the next node
+                prev = current;
+                current = current.next;
             }
-
-            // If no swaps were made, the list is sorted, so break the loop
-            if (isSorted) {
-                break;
-            }
-            
-            // Move to the next node for another pass
-            temp = temp.next;
-        }
+        } while (swapped);
 
         return head;
     }
 }
-
-// Title: Sort List
