2491e4314adc14f8a074328004ca035710ceb604
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-07 03:38:20 +0530

    Add Rotate List

---
+++ Rotate_List.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        ListNode current=head,temp=head,prev=head;//current =alwasy first node 
+        if ( head==null ||head.next==null){
+            return head;
+        }
+            int size=1;
+        while(temp.next!=null){
+            temp=temp.next;
+            size++;
+        }
+        k=k%size;
+        if(k%size==0 ){
+            return head;
+        }
+        
+        for(int i=0;i<k;i++){
+            temp=head;
+            prev=null;
+             while(temp.next!=null){
+            prev=temp;//second last node
+            temp=temp.next;//last node
+            //size++:
+        }
+        prev.next=null;
+        temp.next=head;
+        head=temp;
+           
+            
+        }
+        return head;
+    }
+}
