bf4a02d07f997c02bd9a03b47088065ae89cc1cd
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-11 03:35:28 +0530

    Add Rotate String

---
+++ Rotate_String.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        int ans[] = position(s, goal.charAt(0));
+       // int i = 0;
+        for (int j = 0; j < ans.length; j++) {
+            int start = ans[j];
+            boolean isvalid=true;
+            for (int k = 0; k < goal.length(); k++) {
+
+                int idx = (start + k) % s.length();
+                if (goal.charAt(k) != s.charAt(idx)) {
+                    // continue;
+                    isvalid = false;
+                    break;
+                }
+
+            }
+            if (isvalid) {
+                return true;
+                // break;
+            } 
+        }
+        return false;
+
+    }
+
+    public int[] position(String s, Character cg) {
+        int arr[] = new int[s.length()];
+        int j = 0;
+        for (int i = 0; i < s.length(); i++) {
+            Character c = s.charAt(i);
+            if (c.equals(cg)) {
+                arr[j] = i;
+                j++;
+            }
+        }
+        return arr;
+        //return Arrays.copyOf(arr, j);
+    }
+}
