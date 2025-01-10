b9112f7facc60b3840a9fe8c2c730912e8f4fce8
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-10 12:03:19 +0530

    Add Word Subsets

--- Word_Subsets.java
+++ Word_Subsets.java
@@ -1,9 +1,8 @@
 class Solution {
     public List<String> wordSubsets(String[] words1, String[] words2) {
         List<String> ans=new ArrayList<>();
-      //  StringBuilder s1=new StringBuilder(String.join("",words1));
-     // StringBuilder temp=new StringBuilder();
-        String Hello="";
+        StringBuilder s1=new StringBuilder(String.join("",words1));
+        String temp,Hello="";
         int count=0,checkcount=0;
 
 
@@ -11,16 +10,13 @@ class Solution {
             count=0;
             for(String check: words2){
                 checkcount=0;
-             StringBuilder temp=new StringBuilder(word);
-         //  temp=word;
+                temp=word;
                 
                 for(int i=0;i<check.length();i++){
                  //   StringBuilder temp=new StringBuilder(check);
                 
-                if(temp.toString().contains(Character.toString(check.charAt(i)))){
-                   // temp.replace(Character.toString(check.charAt(i)),"");
-              temp.deleteCharAt(temp.indexOf(Character.toString(check.charAt(i))));
-
+                if(temp.contains(Character.toString(check.charAt(i)))){
+                    temp.replace(Character.toString(check.charAt(i)),"");
                     checkcount++;
                 }else{
                     break;
@@ -40,4 +36,3 @@ class Solution {
 
     }
 }
-// Title: Word Subsets
