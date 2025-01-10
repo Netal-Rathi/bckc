c40ad7fb53552ddfe1bcbb9fa357d3fa51c83f6e
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-10 12:49:10 +0530

    Add Word Subsets

--- Word_Subsets.java
+++ Word_Subsets.java
@@ -1,43 +1,38 @@
 class Solution {
     public List<String> wordSubsets(String[] words1, String[] words2) {
-        List<String> ans=new ArrayList<>();
-      //  StringBuilder s1=new StringBuilder(String.join("",words1));
-     // StringBuilder temp=new StringBuilder();
-        String Hello="";
-        int count=0,checkcount=0;
+        List<String> ans = new ArrayList<>();
+        // StringBuilder s1=new StringBuilder(String.join("",words1));
+        // StringBuilder temp=new StringBuilder();
+        String Hello = "";
+        int count = 0, checkcount = 0;
+        String result = String.join("",words2);
 
+        for (String word : words1) {
+            count = 0;
 
-        for(String word : words1){
-            count=0;
-            for(String check: words2){
-                checkcount=0;
-             StringBuilder temp=new StringBuilder(word);
-         //  temp=word;
-                
-                for(int i=0;i<check.length();i++){
-                 //   StringBuilder temp=new StringBuilder(check);
-                
-                if(temp.toString().contains(Character.toString(check.charAt(i)))){
-                   // temp.replace(Character.toString(check.charAt(i)),"");
-              temp.deleteCharAt(temp.indexOf(Character.toString(check.charAt(i))));
+            // checkcount=0;
+            StringBuilder temp = new StringBuilder(word);
+            // temp=word;
 
-                    checkcount++;
-                }else{
+            for (int i = 0; i < result.length(); i++) {
+                // StringBuilder temp=new StringBuilder(check);
+
+                if (temp.toString().contains(Character.toString(result.charAt(i)))) {
+                    // temp.replace(Character.toString(check.charAt(i)),"");
+                    temp.deleteCharAt(temp.indexOf(Character.toString(result.charAt(i))));
+                    count++;
+                    // checkcount++;
+                } else {
                     break;
                 }
-                }
-                if(checkcount==check.length()){
-                count++;
-                }
-                 
             }
-            if(count==words2.length){
+
+            if (count == result.length()) {
                 ans.add(word);
-                   }
-           
+            }
+
         }
         return ans;
 
     }
 }
-// Title: Word Subsets
