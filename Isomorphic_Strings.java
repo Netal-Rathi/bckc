9d14f73b5a294cf8909f4c2a5a9179d8d3594ef5
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-11 00:56:58 +0530

    Add Isomorphic Strings

---
+++ Isomorphic_Strings.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap <Character,Character> map =new HashMap<>();
+        StringBuilder ans=new StringBuilder();
+
+        if(s.length()!=t.length()){
+            return false;
+        }
+        if(s==t){
+            return true;
+        }
+        for(int i=0;i<t.length();i++){
+            if(map.containsKey(s.charAt(i))){
+                ans.append(map.get(s.charAt(i)));
+            //   s.replaceAll(s.charAt(i),map.get(s.charAt(i)));
+            //   charAt(i)=map.get(s.charAt(i));
+                
+            }else if(!map.containsValue(t.charAt(i))){
+                map.put(s.charAt(i),t.charAt(i));
+             ans.append(map.get(s.charAt(i)));
+                
+            }else{
+                return false;
+            }
+        }
+        System.out.println(ans.toString());
+        if(ans.toString().equals(t)){
+            return true;
+        }else{
+            return false;
+        }
+
+        
+    }
+}
