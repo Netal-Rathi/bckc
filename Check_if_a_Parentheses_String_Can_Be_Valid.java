bdda2add4c5769c86fa6e2242146c89793229cde
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-12 10:29:20 +0530

    Add Check if a Parentheses String Can Be Valid

--- Check_if_a_Parentheses_String_Can_Be_Valid.java
+++ Check_if_a_Parentheses_String_Can_Be_Valid.java
@@ -1,41 +1,41 @@
 import java.util.Stack;
-
 class Solution {
     public boolean canBeValid(String s, String locked) {
-        // Check if the string length is odd
-        if (s.length() % 2 != 0) {
+        if(s.length()%2==1){
             return false;
         }
+        Stack<Integer> stack1=new Stack<>();
+        Stack<Integer> open=new Stack<>();
 
-        Stack<Character> stack = new Stack<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char current = s.charAt(i);
-            char lock = locked.charAt(i);
-
-            if (current == ')') {
-                if (!stack.isEmpty() && stack.peek() == '(') {
-                    // Balance a locked '(' with ')'
-                    stack.pop();
-                } else if (lock == '0') {
-                    // Treat unlocked ')' as '(' to allow flexibility
-                    stack.push('(');
-                } else {
-                    return false; // No way to balance
+        for(int i=0;i<s.length();i++){
+            if(locked.charAt(i)=='0'){
+                stack1.push(i);
+            }else if(s.charAt(i)=='('){
+                open.push(i);
+            }else if(s.charAt(i)==')' ){
+                if(!open.isEmpty()){
+                open.pop();
+                }else if(!stack1.isEmpty()){
+                    stack1.pop();
+                }else{
+                    return false;
                 }
-            } else if (current == '(') {
-                if (lock == '0' && !stack.isEmpty() && stack.peek() == '(') {
-                    // Use unlocked '(' to balance another '('
-                    stack.pop();
-                } else {
-                    stack.push(current);
-                }
-            }
-        }
-
-        // If the stack is empty, the string is balanced
-        return stack.isEmpty();
     }
+ 
 }
 
-// Title: Check if a Parentheses String Can Be Valid
+while(!open.isEmpty() && !stack1.isEmpty() && open.peek()<stack1.peek()){
+    open.pop();
+    stack1.pop();
+}
+
+if(!open.isEmpty()){
+    return false;
+
+}
+if(stack1.size()%2!=0){
+    return false;
+}
+    return true;
+}
+}
