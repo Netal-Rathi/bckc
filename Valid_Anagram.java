58ee42e798e76c344a5fbeb0c5bc0c592c70c370
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-12 11:11:49 +0530

    Add Valid Anagram

--- Valid_Anagram.java
+++ Valid_Anagram.java
@@ -9,21 +9,23 @@ class Solution {
         HashMap <Character,Integer> map1=new HashMap();
         HashMap <Character,Integer> map2=new HashMap();
         for(int i=0;i<s.length();i++){
-            
+            if(map1.containsKey(s.charAt(i))){
                 map1.put(s.charAt(i),map1.getOrDefault(s.charAt(i),0)+1);
                 
+            }else {
+                map1.put(s.charAt(i),1);
+            }
+              if(map2.containsKey(t.charAt(i))){
+                map2.put(t.charAt(i),map2.getOrDefault(t.charAt(i),0)+1);
+            }else {
+                map2.put(t.charAt(i),1);
+            }
+
+
         }
-         for(int i=0;i<s.length();i++){
-           
-                map1.put(t.charAt(i),map1.getOrDefault(t.charAt(i),0)-1);
-         }
-            
-
-
-        
 
            for(Character c : map1.keySet()){
-            if(map1.get(c)!=0){
+            if(!map1.get(c).equals(map2.get(c))){
                 return false;
             }
 
@@ -31,4 +33,3 @@ class Solution {
         return true;
     }
 }
-// Title: Valid Anagram
