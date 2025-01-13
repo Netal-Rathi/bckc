7b9d5b655f8d89fd17146e3d9d42785d4c96ca80
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-14 05:29:48 +0530

    Add Roman to Integer

--- Roman_to_Integer.java
+++ Roman_to_Integer.java
@@ -1,33 +1,54 @@
 class Solution {
     public int romanToInt(String s) {
-        int num=0,prev=0,ans=0;
-        for(int i=s.length()-1;i>=0;i--){
-            switch(s.charAt(i)){
-                case 'M' : num=1000; break;
-                case 'D' : num=500; break;
-                case 'C' : num=100; break;
-                case 'L' : num=50; break;
-                case 'X' : num=10; break;
-                case 'V' : num=5; break;
-                case 'I' : num=1; break;
-               
-            }
-
-            if(num<prev){
-                ans-=num;
+      //  int I=1,V=5,X=10,L=50,C=100,D=500,M=1000;
+   //   HashMap <Character,Inteh>
+   if(s.length()==1){
+    return isvalid(s.charAt(0));
+   }
+        int count=0, m=0;
+        for(int i=0;i<s.length()-1;i++){
+            int one=isvalid(s.charAt(i));
+            int two=isvalid(s.charAt(i+1));
+            if(one>=two){
+                count+=one;
+                System.out.println(count);
+                
             }else{
-                ans+=num;
+                count+=two-one;
+                 System.out.println(count);
+                 i++;
             }
-            //num=prev;
-            prev=num;
+            m=i;
+
         }
-        return ans;
-     
+        if(m==s.length()-2){
+        count+=isvalid(s.charAt(s.length()-1));
+        }
+        return count;
+       
+        
+    }
+
+    public int isvalid(Character c){
+        int count=0;
+        switch(c){
+                case 'M' : count=1000;
+                break;
+                case 'D': count=500;
+                break;
+                    case 'C': count=100;
+                break;
+                case 'L' : count=50;
+                break;
+                    case 'X' : count=10;
+                break;
+                case 'V' : count=5;
+                break;
+                    case 'I' : count=1;
+                break;
+               
+
+            }
+            return count;
     }
 }
-
-
-
-
-
-// Title: Roman to Integer
