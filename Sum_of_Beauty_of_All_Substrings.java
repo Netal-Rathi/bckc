be3d0be65ea9a20e581e6605073f7396a7237411
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-15 13:00:20 +0530

    Add Sum of Beauty of All Substrings

---
+++ Sum_of_Beauty_of_All_Substrings.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+
+    public int beautySum(String s) {
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int arr[]=new int[26];
+            for (int j = i; j < s.length(); j++) {
+                arr[s.charAt(j)-'a']++;
+                 sum += calc(arr);
+
+                
+            }
+           
+        }
+        return sum;
+    }
+
+    public int calc(int arr[]) {
+        int maxv=0,minv=Integer.MAX_VALUE;
+       for(int elem : arr){
+        if(elem>0){
+        maxv=Math.max(maxv,elem);
+        minv=Math.min(minv,elem);
+        }
+
+       }
+       return maxv-minv;
+    }
+}
