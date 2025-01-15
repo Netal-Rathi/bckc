afc16d3736c805a59302fc6117fa47bb668dd6f0
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-15 16:47:34 +0530

    Add Minimize XOR

--- Minimize_XOR.java
+++ Minimize_XOR.java
@@ -1,42 +1,10 @@
 class Solution {
     public int minimizeXor(int num1, int num2) {
-        int count1 = Integer.bitCount(num1); // Count set bits in num1
-        int count2 = Integer.bitCount(num2); // Count set bits in num2
-
-        if (count1 == count2) {
+        if(Integer.bitCount(num1)==Integer.bitCount(num2)){
             return num1;
         }
-
-        // Convert num1 to binary and use StringBuilder for manipulation
-        StringBuilder sb = new StringBuilder(Integer.toBinaryString(num1));
+        int num3=num1+(num1*2);
+        return num3;
         
-        // If num1 has more set bits than num2, reduce the set bits in num1
-        if (count1 > count2) {
-            int bitsToRemove = count1 - count2;
-            for (int i = sb.length() - 1; i >= 0 && bitsToRemove > 0; i--) {
-                if (sb.charAt(i) == '1') {
-                    sb.setCharAt(i, '0');
-                    bitsToRemove--;
-                }
-            }
-        } else { // If num1 has fewer set bits than num2, add more set bits to num1
-            int bitsToAdd = count2 - count1;
-            for (int i = sb.length() - 1; i >= 0 && bitsToAdd > 0; i--) {
-                if (sb.charAt(i) == '0') {
-                    sb.setCharAt(i, '1');
-                    bitsToAdd--;
-                }
-            }
-            // If more bits need to be added, prepend '1's to the binary string
-            while (bitsToAdd > 0) {
-                sb.insert(0, '1');
-                bitsToAdd--;
-            }
-        }
-
-        // Convert the binary string back to an integer
-        return Integer.parseInt(sb.toString(), 2);
     }
 }
-
-// Title: Minimize XOR
