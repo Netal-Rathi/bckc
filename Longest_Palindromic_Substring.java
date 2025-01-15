9e71b2c7ab22bf110811eddd7d41e24e4e7c0b12
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-15 12:34:20 +0530

    Add Longest Palindromic Substring

---
+++ Longest_Palindromic_Substring.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        if(s==null || s.length()==0){
+            return "";
+        }
+    
+   
+     int start=0, end=0;
+
+     for(int i=0;i<s.length();i++){
+     int odd=indexes(s,i,i);
+     int even=indexes(s,i,i+1);
+     int max_len = Math.max(odd,even);
+        if(max_len > end - start ){
+               start = i - (max_len - 1) / 2;
+                end = i + max_len / 2;
+        }
+
+     }
+  
+     return s.substring(start, end + 1);
+   
+    }
+
+    public int indexes(String s,int l,int r){
+          while(l>=0 && r<s.length() && s.charAt(l)==s.charAt(r)){
+            l--;
+            r++;
+
+        }
+      return r - l - 1;
+    }
+
+
+}
