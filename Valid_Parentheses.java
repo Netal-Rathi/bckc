6d32362cd10e4664daba97213546215c4d31188d
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-15 19:08:09 +0530

    Add Valid Parentheses

---
+++ Valid_Parentheses.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()==1 || s.length()==0){
+            return false;
+        }
+        Stack <Character> stack=new Stack<>();
+        for(Character ch : s.toCharArray()){
+            if(ch.equals('(') || ch.equals('{') || ch.equals('[')){
+                stack.push(ch);
+            }else if(ch.equals(')') && !stack.isEmpty() && stack.peek().equals('(') ){
+                stack.pop();
+            }else if(ch.equals(']') && !stack.isEmpty() && stack.peek().equals('[') ){
+                stack.pop();
+            }else if(ch.equals('}') && !stack.isEmpty() && stack.peek().equals('{') ){
+                stack.pop();
+            }else{
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
