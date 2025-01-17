0655e4f0849f14601eff0b2df18c65a07290f082
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-17 15:17:27 +0530

    Add Pow(x, n)

---
+++ Pow(x,_n).java
@@ -0,0 +1,26 @@
+class Solution {
+
+    public double solve(double x, long n) {
+
+        if (n == 0) {
+            return 1.0;
+        }
+
+        if (n < 0) {
+            // return 1 / myPow(x , -n);
+            return solve(1 / x, -n);
+        }
+
+        if (n % 2 == 0) {
+            return solve(x * x, n / 2);
+        } else {
+            return x * solve(x * x, (n -1)/ 2);
+        }
+
+    }
+
+    public double myPow(double x, int n) {
+        return solve(x, (long) n);
+
+    }
+}
