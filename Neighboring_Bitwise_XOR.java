157bdff5c409540e897e2b6d697c5ebf4eaff771
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-17 09:34:45 +0530

    Add Neighboring Bitwise XOR

--- Neighboring_Bitwise_XOR.java
+++ Neighboring_Bitwise_XOR.java
@@ -1,11 +1,35 @@
-public class Solution {
-
+class Solution {
     public boolean doesValidArrayExist(int[] derived) {
-        int sum = 0;
-        for (int num : derived) {
-            sum += num;
+        int n=derived.length;
+        int ans[]=new int[n];
+        ans[0]=0;
+         System.out.print(ans[0]);
+        for(int i=0;i<n-1;i++){
+             if(derived[i]==0 ){
+                ans[i+1]=ans[i];
+                 System.out.print(ans[i]);
+            }else{
+                ans[i+1]=(1-ans[i]);
+                 System.out.print(ans[i]);
+            }
         }
-        return sum % 2 == 0;
+        // if(derived[n-1]==0){
+        //     ans[n-1]=ans[0];
+        // }else{
+        //     ans[n-1]=1-ans[0];
+        // }
+      System.out.print(ans[n-1]);
+
+         for(int i=0;i<n-1;i++){
+       
+            if(derived[i]!= (ans[i] ^ ans[i+1])){
+                    return false;
+            }
+        }
+        if(derived[n-1] != (ans[0] ^ ans[n-1])){
+            return false;
+        }
+        return true;
+        
     }
 }
-// Title: Neighboring Bitwise XOR
