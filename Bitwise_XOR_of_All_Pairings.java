1c53acf64992ecb87091eb374e2190d3b7a92e40
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-17 05:33:16 +0530

    Add Bitwise XOR of All Pairings

--- Bitwise_XOR_of_All_Pairings.java
+++ Bitwise_XOR_of_All_Pairings.java
@@ -2,16 +2,15 @@ class Solution {
 
     public int xorAllNums(int[] nums1, int[] nums2) {
          int result=0;
-       // int ans[]=new int[nums1.length*nums2.length];
+        int ans[]=new int[nums1.length*nums2.length];
         for(int i=0;i<nums1.length;i++){
             for(int j=0;j<nums2.length;j++){
-                //ans[i]=nums1[i]^nums2[j];
-               result=result^(nums1[i]^nums2[j]);
+                ans[i]=nums1[i]^nums2[j];
+               result=result^ans[i];
                 System.out.print(result);
 
             }
         }
-        
         System.out.println();
         System.out.print(0^4);
 
@@ -25,4 +24,3 @@ class Solution {
         return result;
     }
 }
-// Title: Bitwise XOR of All Pairings
