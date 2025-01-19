43617a68c5d690bca55ae38f07f75868aa740f09
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-20 04:37:36 +0530

    Add Count Good Numbers

--- Count_Good_Numbers.java
+++ Count_Good_Numbers.java
@@ -2,35 +2,31 @@ class Solution {
     long mod = 1000000007;
 
     public int countGoodNumbers(long n) {
-         long ans=1;
+        // long ans=1;
         if (n % 2 == 0) {
             // 5 to the power n% 2 calculates && 4 to the power
-return (int)( (helper(5,n/2)  * helper(4,n/2)) % mod);
+            return (int) (((helper(5, n / 2) % mod) * (helper(4, n / 2) % mod)) % mod);
 
         } else {
             // 4
-            return (int) ((helper(5, n / 2 + 1)  * helper(4, n / 2) ) % mod);
+            return (int) (((helper(5, n / 2 + 1) % mod) * (helper(4, n / 2) % mod)) % mod);
 
         }
 
     }
 
     public long helper(long n1, long m) {
-        long ans=1;
-        n1 %=  mod;
-      
-        while(m>0){
-      if(m%2==0){
-        n1=(n1*n1)% mod;
-        m=m/2;
+        m %= mod;
 
-      }else{
-        ans=(ans*n1) % mod;
-        m-=1;
-      }
+        if (m == 0) {
+            return 1;
         }
-        return ans ;
+
+        if (m % 2 == 0) {
+            return (helper((n1 * n1) % mod, m / 2) );
+        } else {
+            return (n1 * helper((n1 * n1)% mod, m / 2) );
+        }
 
     }
 }
-// Title: Count Good Numbers
