a148316c9b1a235495eaea1d63147a35e9fae2e1
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-19 14:50:54 +0530

    Add Count Good Numbers

--- Count_Good_Numbers.java
+++ Count_Good_Numbers.java
@@ -1,35 +1,37 @@
 class Solution {
     public int countGoodNumbers(long n) {
-        if(n==0){
+        long mod = 1000000007;
+
+        if (n == 0) {
             return 1;
         }
-        long mod=1000000007;
-        
-    if(n%2==0){
-        // 5 to the power n% 2 calculates  && 4 to the power
-       return (int) ((helper(5,n/2) * helper(4,n/2)) % mod );
-       
-    }else{
-        //4
-      return (int) (( helper(5,n/2 +1)* helper(4,n/2)) % mod );
-     
-    }
-        
+
+        if (n % 2 == 0) {
+            // Multiply results of helper and take modulo
+            long ans = (helper(5, n / 2) % mod) * (helper(4, n / 2) % mod);
+            return (int) (ans % mod);
+        } else {
+            // Multiply results of helper for odd case and take modulo
+            long ans = (helper(5, n / 2 + 1) % mod) * (helper(4, n / 2) % mod);
+            return (int) (ans % mod);
+        }
     }
 
-    public long helper(long n1,long m){
-                long mod=1000000007;
-        if(m==0){
+    public int helper(long n1, long m) {
+        long mod = 1000000007;
+
+        if (m == 0) {
             return 1;
         }
-        
-        if(m %2==0){
-            return (helper(n1*n1,m/2) % mod);
-        }else{
-            return  ((n1* helper(n1*n1,m/2)) % mod );
-        }
-      
 
+        if (m < 0) {
+            return (int) (helper(1 / n1, -m) % mod);
+        }
+
+        if (m % 2 == 0) {
+            return (int) (helper(n1 * n1, m / 2) % mod);
+        } else {
+            return (int) ((n1 * helper(n1 * n1, m / 2)) % mod);
+        }
     }
 }
-// Title: Count Good Numbers
