3f92537a91a361b2f7960c234679758086a222f9
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-20 15:32:39 +0530

    Add Find All Duplicates in an Array

---
+++ Find_All_Duplicates_in_an_Array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i : nums) {
+            if (map.containsKey(i)) {
+                list.add(i);
+            } else {
+                map.put(i, 1);
+            }
+
+        }
+
+        return list;
+
+    }
+}
