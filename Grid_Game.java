10e6799f8ac5e81f1eacbf343a1e8ba905b8c6dc
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-23 07:42:46 +0530

    Add Grid Game

--- Grid_Game.java
+++ Grid_Game.java
@@ -1,49 +1,64 @@
 class Solution {
-    public int[][] prefix_sum(int[][] grid) {
-        int rows = grid.length;
-        int cols = grid[0].length;
-        int[][] prefix = new int[rows][cols];
-        
-        // Compute prefix sum for the first row
-        prefix[0][0] = grid[0][0];
-        for (int i = 1; i < cols; i++) {
-            prefix[0][i] = prefix[0][i - 1] + grid[0][i];
+    public int[][] prefix_sum(int[][] grid){
+    //    int n=grid.length;
+        int n=grid[0].length;
+        int k=0;
+        int prefix [][]=new int[2][n];
+        prefix[0][0]=grid[0][0];
+        for(int i=1;i<n;i++){
+            if(k==0){
+                prefix[k][i]=prefix[k][i-1]+grid[k][i];
+
+            }
         }
-        
-        // Compute prefix sum for the second row
-        prefix[1][0] = grid[1][0];
-        for (int i = 1; i < cols; i++) {
-            prefix[1][i] = prefix[1][i - 1] + grid[1][i];
+        k++;
+        prefix[1][0]=grid[0][0]+grid[1][0];
+        for(int i=1;i<n;i++){
+            prefix[k][i]=Math.max(prefix[k][n-1] + grid[k][i] , prefix[0][i] + grid[k][i] );
+
         }
-        
         return prefix;
+      
     }
+
 
     public long gridGame(int[][] grid) {
-        int n = grid[0].length;
+        int n=grid[0].length;
 
-        // Compute prefix sums
-        int[][] prefix = prefix_sum(grid);
-
-        // Initialize variables
-        long sum = Long.MAX_VALUE;
-
-        // Simulate the two-player game
-        for (int i = 0; i < n; i++) {
-            // Robot 1 chooses column `i`, leaving two paths:
-            // 1. Sum of the top row from column `i+1` to the end.
-            long topRow = i + 1 < n ? prefix[0][n - 1] - prefix[0][i] : 0;
-
-            // 2. Sum of the bottom row from column `0` to `i-1`.
-            long bottomRow = i > 0 ? prefix[1][i - 1] : 0;
-
-            // Robot 2 will minimize Robot 1's maximum possible sum.
-            long maxSum = Math.max(topRow, bottomRow);
-            sum = Math.min(sum, maxSum);
+  int prefix[][]= prefix_sum(grid);
+  grid[1][n-1]=0;
+  grid[0][0]=0;
+       int l=n-2;
+                while(l>=0){
+          if(prefix[0][l+1]>prefix[1][l]){
+                for(int i=l+1;i>0;i--){
+                    grid[0][i]=0;
+                    l--;
+                }
+        }else{
+            grid[1][l]=0;
+            l--;
         }
+                }
+System.out.println(Arrays.deepToString(grid));
+   
+        long sum=0;
+        
 
+        for(int i =n-1;i>0;i--){
+            if(grid[0][i]>grid[1][i-1]){
+                for(int j=i;j>=0;j--){
+                sum+=grid[0][j];
+                }
+                break;
+                
+               
+            }else{
+                sum+=grid[1][i-1];
+            }
+        }
+       // sum+=grid[1][n-1];
         return sum;
+        
     }
 }
-
-// Title: Grid Game
