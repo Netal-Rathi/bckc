262692d38e53a146ac18954d59b357e36d85d112
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-23 08:18:19 +0530

    Add Grid Game

---
+++ Grid_Game.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    public long gridGame(int[][] grid) {
+        long toprow=0;
+        int n=grid[0].length;
+    for(int i=0;i<n;i++){
+        toprow+=grid[0][i];
+
+    }
+    long bottomrow=0;
+    long minsum=Long.MAX_VALUE;
+
+    for(int i=0;i<n;i++){
+        toprow=toprow-grid[0][i];
+        minsum=Math.min (minsum,
+        Math.max(toprow,bottomrow));
+        bottomrow+=grid[1][i];
+    }
+    return minsum;
+    }
+}
