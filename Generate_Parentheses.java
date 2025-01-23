093fd900975fa8c815c64a792142212faafdc858
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-23 13:03:25 +0530

    Add Generate Parentheses

---
+++ Generate_Parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        int closed=0,open=0;
+        List<String> ans=new ArrayList<>();
+        generate(n,closed,open,"",ans);
+        return ans;
+    }
+    // n==> length
+    //m=> character array ki length
+    // k=closed brackets how much
+    public void generate (int n,int closed,int open,String current,List<String> ans){
+        if(current.length()==2*n){
+            ans.add(current);
+        }
+        if(open<n){
+            generate(n,closed,open+1,current+"(",ans);
+        }
+        if(closed<open){
+             generate(n,closed+1,open,current + ")",ans);
+        }
+        
+    }
+
+ 
+}
