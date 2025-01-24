b5f99896c8e1a6440a4f18826f2f09024c5ad9ec
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-24 13:00:47 +0530

    Add Subsets

---
+++ Subsets.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> arr = new ArrayList<>();
+        int i = 0;
+        helper(nums, arr, i, ans);
+        return ans;
+    }
+
+    public void helper(int[] nums, List<Integer> arr, int i, List<List<Integer>> ans) {
+        if (i == nums.length) {
+            ans.add(new ArrayList<>(arr));
+            return;
+        }
+
+        helper(nums, arr, i + 1, ans);
+        arr.add(nums[i]);
+        helper(nums, arr, i + 1, ans);
+        arr.remove(arr.size() - 1);
+
+    }
+}
