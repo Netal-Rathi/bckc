56110ef4cb10f0fed211ca03a0d11806bba99c5f
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-24 16:28:56 +0530

    Add Find Eventual Safe States

---
+++ Find_Eventual_Safe_States.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        List<Integer> list=new ArrayList<>();
+       // List<Integer> ans=new ArrayList<>();
+               int n = graph.length;
+       boolean []ans=new boolean[n];
+       Arrays.fill(ans,true);
+
+        for(int i=0;i<n;i++){
+            if(graph[i].length==0){
+                list.add(i);
+                ans[i]=false;
+            }
+
+            
+        }
+
+        for(int i=0;i<n;i++){
+            if(!(Arrays.stream(graph[i]).allMatch(list::contains)) ){
+                ans[i]=false;
+            }
+        }
+        System.out.println(list);
+
+        for(int i=0;i<n;i++){
+            if(ans[i]){
+                list.add(i);
+            }
+        }
+         //ans.addAll(list);
+         list.sort(Comparator.naturalOrder());
+         return list;
+
+
+    }
+}
