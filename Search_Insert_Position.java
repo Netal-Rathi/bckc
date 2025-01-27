e4d5ff2a1836b56f8567fe5f0189b5ca5a694a7c
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-27 17:56:01 +0530

    Add Search Insert Position

--- Search_Insert_Position.java
+++ Search_Insert_Position.java
@@ -1,22 +1,30 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target) {
-                return mid; // Target found
-            } else if (nums[mid] < target) {
-                left = mid + 1; // Search in the right half
-            } else {
-                right = mid - 1; // Search in the left half
+        int n=nums.length/2;
+        System.out.println(n);
+        System.out.println(nums[n]);
+        if(nums[n]<=target){
+            for(int i=n;i<nums.length;i++){
+                if(target==nums[i]){
+                    return i;
+                }else if(nums[i]>target){
+                    return i;
+                }else{
+                    continue;
+                }
+            }
+        }else{
+            for(int i=0;i<n;i++){
+                if(target==nums[i]){
+                    return i;
+                }else if(nums[i]> target){
+                    return i;
+                }else{
+                    continue;
+                }
             }
         }
-
-        // If target is not found, `left` will be the correct insert position
-        return left;
+       return target>nums[nums.length-1] ? nums.length:  0;
+    //    return nums.length;
     }
 }
-
-// Title: Search Insert Position
