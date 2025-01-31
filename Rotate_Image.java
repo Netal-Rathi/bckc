ef23cd47c71a6062aa4585e02f7d7a19af0219d0
Netal-Rathi <netalrathi2707@gmail.com>
2025-01-31 14:15:52 +0530

    Add Rotate Image

---
+++ Rotate_Image.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n=matrix.length;
+       // int temp=0;
+        for(int i=0 ;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+               int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+                
+            }
+        }
+        System.out.println(Arrays.deepToString(matrix));
+
+        for(int k=0;k<n;k++){
+            int i=0,j=n-1;
+            while(i<j ){
+                int var=matrix[k][i];
+               matrix[k][i]= matrix[k][j];
+               matrix[k][j]=var;
+               i++;
+               j--;
+            }
+           
+        }
+        
+    }
+}
