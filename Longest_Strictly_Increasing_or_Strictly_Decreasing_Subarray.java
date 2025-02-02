8183e26c80cf8a810cb6f09c0f87ba8d9026f555
Netal-Rathi <netalrathi2707@gmail.com>
2025-02-03 02:56:15 +0530

    Add Longest Strictly Increasing or Strictly Decreasing Subarray

--- Longest_Strictly_Increasing_or_Strictly_Decreasing_Subarray.java
+++ Longest_Strictly_Increasing_or_Strictly_Decreasing_Subarray.java
@@ -1,30 +1,17 @@
 class Solution {
     public int longestMonotonicSubarray(int[] nums) {
-        return helper(nums, 0, 1, 1, 1, 2); // Start with neutral status
+        return helper(nums, 0, 1, 1, 1);
     }
 
-    public int helper(int[] nums, int i, int n, int count, int ans, int status) {
+    public int helper(int[] nums, int i, int n, int count, int ans) {
         if (n == nums.length) {
             return Math.max(ans, count);
         }
 
-        if (nums[n - 1] < nums[n] && (status == 1 || status == 2)) {
-            if (status == 2) {
-                status = 1;
-            }
-            return helper(nums, i, n + 1, count + 1, Math.max(count + 1, ans), status);
-
-        } else if (nums[n - 1] > nums[n] && (status == 0 || status == 2)) {
-            if (status == 2) {
-                status = 0;
-            }
-            return helper(nums, i, n + 1, count + 1, Math.max(count + 1, ans), status);
-
+        if (nums[n] > nums[n - 1] || nums[n] < nums[n - 1]) {
+            return helper(nums, i, n + 1, count + 1, Math.max(ans, count + 1));
         } else {
-            status = 2; // Reset to neutral
-            return helper(nums, n, n + 1, 1, Math.max(count, ans), status);
+            return helper(nums, n, n + 1, 1, Math.max(ans, count));
         }
     }
 }
-
-// Title: Longest Strictly Increasing or Strictly Decreasing Subarray
