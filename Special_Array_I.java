376c76ea2ec7ec28102c428061883b1e6ebe2cec
Netal-Rathi <netalrathi2707@gmail.com>
2025-02-02 17:59:45 +0530

    Add Special Array I

---
+++ Special_Array_I.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        int n = nums.length;
+        if (n == 1) {
+            return true;
+        }
+        
+        int parity = nums[0] % 2;  
+        
+        
+        for (int i = 0; i < n; i++) {
+            if (nums[i] % 2 != (i % 2 == 0 ? parity : 1 - parity)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
