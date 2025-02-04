f53bc10221b2d96c08cc629d56eda81f389d4235
Netal-Rathi <netalrathi2707@gmail.com>
2025-02-04 15:58:39 +0530

    Add Maximum Ascending Subarray Sum

--- Maximum_Ascending_Subarray_Sum.java
+++ Maximum_Ascending_Subarray_Sum.java
@@ -1,20 +1,19 @@
 class Solution {
     public int maxAscendingSum(int[] nums) {
-        return helper(nums,0,0,0,0);
-        
+        return helper(nums, 0, 0, 0, 0);
     }
 
-    public int helper(int[] nums,int n,int sum,int i,int ans){
-        if(n==nums.length){
-            return Math.max(sum,ans);
+    public int helper(int[] nums, int n, int sum, int i, int ans) {
+        if (n == nums.length) {
+            return Math.max(sum, ans);  // Return max of sum and ans
         }
 
-        if(n==0 || nums[n]>nums[n-1] ){
-       return  helper(nums,n+1,sum+nums[n],i,Math.max(sum,ans));
-        }else{
-          return helper(nums,n+1,nums[n],n,ans);
+        if (n == 0 || nums[n] > nums[n - 1]) {
+            sum += nums[n];
+            ans = Math.max(sum, ans);
+            return helper(nums, n + 1, sum, i, ans);
+        } else {
+            return helper(nums, n + 1, nums[n], n, ans);  // Reset sum when sequence breaks
         }
-    //    return ans;
     }
 }
-// Title: Maximum Ascending Subarray Sum
