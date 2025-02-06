5dc2f3d7c1b480c9bb00214c239b0d4c443590f6
Netal-Rathi <netalrathi2707@gmail.com>
2025-02-07 01:31:40 +0530

    Add Majority Element II

--- Majority_Element_II.java
+++ Majority_Element_II.java
@@ -1,19 +1,19 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         List<Integer> ans=new ArrayList<>();
-        // if(nums.length<=2){
-        //     for(int num:nums){
-        //         ans.add(num);
-        //     }
-        //     return ans;
+        if(nums.length<=2){
+            for(int num:nums){
+                ans.add(num);
+            }
+            return ans;
 
-        // }
+        }
         HashMap <Integer,Integer> map=new HashMap<>();
         for(int num : nums){
             map.put(num , map.getOrDefault(num,0)+1);
         }
         for(int key : map.keySet()){
-            if( map.get(key)>nums.length/3 || (nums.length<=2)){
+            if( map.get(key)>nums.length/3){
                 ans.add(key);
             }
         }
