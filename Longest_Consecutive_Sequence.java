833b4ac4b1bfce0361b97cb6fbc17c37b4595861
Netal-Rathi <netalrathi2707@gmail.com>
2025-02-06 06:38:54 +0530

    Add Longest Consecutive Sequence

--- Longest_Consecutive_Sequence.java
+++ Longest_Consecutive_Sequence.java
@@ -2,21 +2,19 @@ class Solution {
     public int longestConsecutive(int[] nums) {
         Arrays.sort(nums);
         System.out.println(Arrays.toString(nums));
-        return helper(nums,0,0,0);
+        return helper(nums,0,1,1,0);
     }
-    public int helper(int [] nums ,int n,int count,int ans){
-            if(n==nums.length ){
+    public int helper(int [] nums ,int i,int n,int count,int ans){
+            if(n==nums.length || i== nums.length){
                 return Math.max(count,ans);
             }
-            
 
-            if( n==0 || nums[n]==nums[n-1]+1 ){
+            if(nums[n]==nums[n-1]+1){
                 count++;
-               return helper(nums,n+1,count,ans);
+               return helper(nums,i,n+1,count,ans);
             }
-           return helper(nums,n+1,1,Math.max(count,ans));
+           return helper(nums,n,n+1,1,Math.max(count,ans));
 
     //       return ans ;
     }
 }
-// Title: Longest Consecutive Sequence
