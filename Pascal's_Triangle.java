9f44f197a037cac2b6c81f2e73e39394d71a46bd
Netal-Rathi <netalrathi2707@gmail.com>
2025-02-06 11:29:17 +0530

    Add Pascal's Triangle

--- Pascal's_Triangle.java
+++ Pascal's_Triangle.java
@@ -1,35 +1,42 @@
 import java.util.*;
+
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-
         List<List<Integer>> ans = new ArrayList<>();
-
         if (numRows == 0) {
             return ans;
         }
+        
+        // First row is always [1]
         ans.add(new ArrayList<>(Arrays.asList(1)));
         if (numRows == 1) {
             return ans;
         }
-        return helper(numRows,  ans ,1);
-
+        
+        return helper(numRows, 1, ans);
     }
 
-    public List<List<Integer>> helper(int numRows, List<List<Integer>> ans , int n) {
+    public List<List<Integer>> helper(int numRows, int n, List<List<Integer>> ans) {
         if (n == numRows) {
             return ans;
         }
-
-        List<Integer> previous = ans.get(n - 1);
-        List<Integer> current = new ArrayList<>();
-
-        current.add(1);
-        for (int i = 1; i < previous.size(); i++) {
-            current.add(previous.get(i) + previous.get(i - 1));
+        
+        List<Integer> prevRow = ans.get(n - 1);
+        List<Integer> currRow = new ArrayList<>();
+        
+        // First element is always 1
+        currRow.add(1);
+        
+        // Generate middle elements
+        for (int i = 1; i < prevRow.size(); i++) {
+            currRow.add(prevRow.get(i - 1) + prevRow.get(i));
         }
-        current.add(1);
-        ans.add(current);
-        return helper(numRows,  ans, n+1);
+        
+        // Last element is always 1
+        currRow.add(1);
+        
+        ans.add(currRow);
+        
+        return helper(numRows, n + 1, ans);
     }
 }
-// Title: Pascal's Triangle
