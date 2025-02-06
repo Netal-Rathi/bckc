33bdfc1224de09a5e44a7d55b0718fb6a06b44ba
Netal-Rathi <netalrathi2707@gmail.com>
2025-02-06 09:33:17 +0530

    Add Longest Consecutive Sequence

--- Longest_Consecutive_Sequence.java
+++ Longest_Consecutive_Sequence.java
@@ -1,60 +1,29 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        // time complexity : nlogn
-    //     if(nums.length==0){
-    //         return 0;
-    //     }
-    //     Arrays.sort(nums);
-    //     System.out.println(Arrays.toString(nums));
-    //     return helper(nums,1,1,0);
-    // }
-    // public int helper(int [] nums ,int n,int count,int ans){
-    //         if(n==nums.length ){
-    //             return Math.max(count,ans);
-    //         }
+        if(nums.length==0){
+            return 0;
+        }
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+        return helper(nums,1,1,0);
+    }
+    public int helper(int [] nums ,int n,int count,int ans){
+            if(n==nums.length ){
+                return Math.max(count,ans);
+            }
 
-    //         if(nums[n]==nums[n-1]){
-    //             return helper(nums,n+1,count,ans);
+            if(nums[n]==nums[n-1]){
+                return helper(nums,n+1,count,ans);
 
-    //         }
+            }
             
 
-    //         if( n==0 || nums[n]==nums[n-1]+1 ){
-    //          //   count++;
-    //            return helper(nums,n+1,count+1,ans);
-    //         }
-    //        return helper(nums,n+1,1,Math.max(count,ans));
+            if( n==0 || nums[n]==nums[n-1]+1 ){
+             //   count++;
+               return helper(nums,n+1,count+1,ans);
+            }
+           return helper(nums,n+1,1,Math.max(count,ans));
 
-    // //       return ans ;
-
-
-    // time comlexity=o(n)
-
-    if(nums.length==0){
-        return 0;
-    }
-        HashSet<Integer> map=new HashSet<>();
-        for(int num: nums){
-            map.add(num);
-        }
-        int ans=0;
-        int current=0;
-        int count=1;
-
-        for (int num : map){
-                if(!map.contains(num-1)){
-                    current=num;
-                     count=1;
-                }
-
-                while(map.contains(current+1)){
-                    current++;
-                    count++;
-                }
-                ans=Math.max(count,ans);
-        }
-        return ans;
-    
+    //       return ans ;
     }
 }
-// Title: Longest Consecutive Sequence
