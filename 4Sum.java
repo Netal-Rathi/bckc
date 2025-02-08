04f901ed6de439c3c681e96aba18fc0243b50cf2
Netal-Rathi <netalrathi2707@gmail.com>
2025-02-08 11:22:09 +0530

    Add 4Sum

--- 4Sum.java
+++ 4Sum.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> ans = new ArrayList<>();
@@ -6,41 +8,35 @@ class Solution {
         }
         Arrays.sort(nums);
         int n = nums.length;
-        // i=0(n-3) j=0+1(n-2) kj+1 l=n-1
-       long sum = 0;
 
         for (int i = 0; i < n - 3; i++) {
-         if (i > 0 && nums[i] == nums[i - 1]){
-            continue;
-         }
-            for (int j = i + 1; j < n - 2; j++) {
-                if (j > i+1 && nums[j] == nums[j - 1]  ) {
-                    continue;
-                }
-                int k = j + 1, l = n - 1;
-                while (k<l) {
-                    sum = (long)nums[i] + nums[j] + nums[k] + nums[l];
-                    if (sum == target) {
-         ans.add(new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[k], nums[l])));
+            // Skip duplicates for 'i'
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-                        while (k < l && nums[k] == nums[k + 1]) {
-                            k++;
-                        }
-                        while (k < l && nums[l] == nums[l - 1]) {
-                            l--;
-                        }
+            for (int j = i + 1; j < n - 2; j++) {
+                // Skip duplicates for 'j'
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                int k = j + 1, l = n - 1;
+                while (k < l) {
+                    long sum = (long) nums[i] + nums[j] + nums[k] + nums[l]; // Avoid integer overflow
+                    if (sum == target) {
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+
+                        // Skip duplicate elements after finding a valid combination
+                        while (k < l && nums[k] == nums[k + 1]) k++;
+                        while (k < l && nums[l] == nums[l - 1]) l--;
+
                         k++;
                         l--;
-                    }else if(sum<target){
+                    } else if (sum < target) {
                         k++;
-                    }else{
+                    } else {
                         l--;
                     }
                 }
             }
         }
         return ans;
-
     }
 }
-// Title: 4Sum
