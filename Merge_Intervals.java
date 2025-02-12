09fb4420b00f20f0d057b6182bd06b34a887f32a
Netal-Rathi <netalrathi2707@gmail.com>
2025-02-13 01:42:44 +0530

    Add Merge Intervals

---
+++ Merge_Intervals.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+    Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int m=intervals[0].length;
+        int n=intervals.length;
+        int arr[][]=new int [n][m];
+     //   ArrayList<int[]> arr=new ArrayList<>()
+        int j=0;
+        arr[0][0]=intervals[0][0];
+        arr[0][1]=intervals[0][1];
+
+        for(int i=1;i<n;i++){
+ if (arr[j][1] >= intervals[i][0]) { 
+                // Merge intervals
+                arr[j][0] = Math.min(arr[j][0], intervals[i][0]);
+                arr[j][1] = Math.max(arr[j][1], intervals[i][1]);
+                
+            }else{
+                j++;
+                  arr[j][0]=intervals[i][0];
+                arr[j][1]=intervals[i][1];
+              //  j++;
+                
+            }
+        }
+        int [][] result=new int[j+1][2];
+        for(int i=0;i<j+1;i++){
+            result[i][0]=arr[i][0];
+            result[i][1]=arr[i][1];
+        }
+        return result;
+    }
+    
+}
+
+// if(arr[j][0]<=intervals[i][0] && arr[j][1]<=intervals[i[1] &&
+// arr[j][1]=>intervals[i][0]){
