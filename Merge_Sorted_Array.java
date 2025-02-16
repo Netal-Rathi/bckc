1530c008ea4a8443e5a38e3bc9883996e3a6ec18
Netal-Rathi <netalrathi2707@gmail.com>
2025-02-16 13:46:29 +0530

    Add Merge Sorted Array

--- Merge_Sorted_Array.java
+++ Merge_Sorted_Array.java
@@ -1,37 +1,26 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        if (n==0){
-            System.out.println(nums1);
+        int i = m - 1;      // Pointer for nums1
+        int j = n - 1;      // Pointer for nums2
+        int k = m + n - 1;  // Pointer for final position in nums1
+        
+        // Merge from the back to avoid shifting
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
         }
 
-            if (m==0){
-                for(int i=0;i<n;i++){
-                    nums1[i]=nums2[i];
-
-                }
-                System.out.println(nums2);
-            }
-        // int x=Math.max(m,n)
-        // int j=0;
-        // for(int i=0;i<x;i++){
-        //     if(nums1[i]<nums2[i] && nums1[i]!=0){
-        //         continue;
-        //         j++;
-        //     }else if (nums1[i]==nums2[i] == nums1[i]!=0){
-        //        for(int k=i+1;k<=m+n;k++){
-                
-        //        }
-        //         nums1[++j]=nums2[i];
-
-        //     }
-
-        // }
-        int j=0;
-         for(int i=m ; i<m+n;i++){
-            nums1[i]= nums2[j];
-            j++;
-         }
-         Arrays.sort(nums1);
-         System.out.println(nums1);
+        // Copy remaining elements of nums2 (if any)
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
     }
 }
