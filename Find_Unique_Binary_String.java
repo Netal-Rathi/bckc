f53c12fa2e67faead2a42bb1e58857c9c62afc35
Netal-Rathi <netalrathi2707@gmail.com>
2025-02-21 05:07:22 +0530

    Add Find Unique Binary String

--- Find_Unique_Binary_String.java
+++ Find_Unique_Binary_String.java
@@ -1,26 +1,12 @@
-import java.util.Arrays;
-
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
-        return helper(nums, "", 0);
-    }
-    
-    private String helper(String[] nums, String ans, int n) {
-        // Base case: If the length of 'ans' is equal to nums.length
-        if (n == nums.length) {
-            if (!Arrays.asList(nums).contains(ans)) {
-                return ans;
-            }
-            return null;
-        }
-
-        // Explore both possibilities: '0' and '1' at the current position
-        String result = helper(nums, ans + '0', n + 1);
-        if (result != null) return result; // Return if a valid string is found
+        StringBuilder ans = new StringBuilder();
         
-        result = helper(nums, ans + '1', n + 1);
-        return result;
+        for (int i = 0; i < nums.length; i++) {
+            // Flip the i-th character of the i-th string
+            ans.append(nums[i].charAt(i) == '0' ? '1' : '0');
+        }
+        
+        return ans.toString();
     }
 }
-
-// Title: Find Unique Binary String
