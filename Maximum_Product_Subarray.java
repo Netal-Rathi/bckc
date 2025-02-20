a613e41330107472569a61273eb228ab4c891cbe
Netal-Rathi <netalrathi2707@gmail.com>
2025-02-20 19:28:48 +0530

    Add Maximum Product Subarray

--- Maximum_Product_Subarray.java
+++ Maximum_Product_Subarray.java
@@ -1,18 +1,15 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int res=1,maxprod=Integer.MIN_VALUE,minprod=Integer.MAX_VALUE;
-        //int ans=Intgere.MAX_VALUE;
+        int res=1,final_res=Integer.MIN_VALUE;
 
         for(int i=0;i<nums.length;i++){
                     res=res*nums[i];
-                  
-                    minprod=Math.min(res,minprod);
-                      maxprod=Math.max(res,Math.max(minprod,maxprod));
-                   
-                    
-                    
+                    final_res=Math.max(final_res,res);
+                    if(res==0){
+                        res=1;
+                    }
                     
             }
-      return maxprod ;
+      return final_res;
     }
 }
