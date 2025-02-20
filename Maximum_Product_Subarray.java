b21916ebb61b384ce563b949d736d8229e744ccf
Netal-Rathi <netalrathi2707@gmail.com>
2025-02-20 19:28:12 +0530

    Add Maximum Product Subarray

--- Maximum_Product_Subarray.java
+++ Maximum_Product_Subarray.java
@@ -5,9 +5,6 @@ class Solution {
         for(int i=0;i<nums.length;i++){
                     res=res*nums[i];
                     final_res=Math.max(final_res,res);
-                    if(res==0){
-                        res=1;
-                    }
                     
             }
       return final_res;
