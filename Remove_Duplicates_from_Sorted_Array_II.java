c23842fc2446899bf2109552c4566dea4a87802d
Netal-Rathi <netalrathi2707@gmail.com>
2025-02-21 04:20:26 +0530

    Add Remove Duplicates from Sorted Array II

--- Remove_Duplicates_from_Sorted_Array_II.java
+++ Remove_Duplicates_from_Sorted_Array_II.java
@@ -1,24 +1,21 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int ans=0;
-        TreeMap <Integer,Integer> map=new TreeMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        if(nums.length<=2){
+            return nums.length;
         }
-        int i=0;
-
-          for(int key: map.keySet()){
-           if(map.get(key)==1){
-           nums[i++]=key;
-          // i++;
-           ans++;
-           }else{
-            nums[i++]=key;
-            nums[i++]=key;
-            ans+=2;
-           }
-          }
-            return ans;
+     int j=1,count=1;
+     for(int i=1;i<nums.length;i++){
+        if(nums[i]==nums[i-1]){
+            count++;
+        }else{
+            count=1;
+        }
+        if(count<=2){
+            nums[j]=nums[i];
+            j++;
+        }
+     }
+       return j;
        
     }
 }
