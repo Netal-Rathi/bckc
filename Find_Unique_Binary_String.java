c4f46a02338184d117f1227a007b105656f0ea54
Netal-Rathi <netalrathi2707@gmail.com>
2025-02-21 18:58:20 +0530

    Add Find Unique Binary String

--- Find_Unique_Binary_String.java
+++ Find_Unique_Binary_String.java
@@ -1,12 +1,39 @@
+// class Solution {
+//     public String findDifferentBinaryString(String[] nums) {
+//         return  helper(nums,"" ,0);
+//     }public String helper(String[] nums ,String ans ,int n){
+//         if(n==nums.length){
+//              if(!Arrays.asList(nums).contains(ans)){
+//                 return ans;
+//               // break;
+//              }
+//              return null;
+//         }
+
+     
+
+//            String result= helper(nums,ans + '0' ,n+1);
+//            if(result!=null)
+//             return result;
+           
+
+//              result= helper(nums,ans + '1' ,n+1);
+//               return result;
+             
+//     }
+
+
+// }
+
+
+
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
-        StringBuilder ans = new StringBuilder();
-        
-        for (int i = 0; i < nums.length; i++) {
-            // Flip the i-th character of the i-th string
-            ans.append(nums[i].charAt(i) == '0' ? '1' : '0');
-        }
-        
-        return ans.toString();
+    StringBuilder ans=new StringBuilder();
+    for(int i=0;i<nums.length;i++){
+        ans.append(nums[i].charAt(i)== '0' ? '1' : '0');
+
+    }
+    return ans.toString();
     }
 }
