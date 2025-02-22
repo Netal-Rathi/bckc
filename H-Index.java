e993ed6e1751e020221d67ae7ae5cf4395b29797
Netal-Rathi <netalrathi2707@gmail.com>
2025-02-22 08:47:59 +0530

    Add H-Index

--- H-Index.java
+++ H-Index.java
@@ -1,16 +1,22 @@
 class Solution {
     public int hIndex(int[] citations) {
-
-        int max_h = 0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        // if(citations.length==1 && citations[0]==0){
+        //     return 0;
+        // }
+        int max_h=0;
         Arrays.sort(citations);
-        for (int i = 0; i < citations.length; i++) {
+        for(int i = 0;i<citations.length;i++ ){
+            if(citations[i]!=0)
+            map.put(citations.length-i , citations[i]);
+        }
+        System.out.println(map);
 
-            if (citations.length - i <= citations[i]) {
-                max_h = Math.max(max_h, citations.length - i);
+        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+            if(entry.getKey() <= (entry.getValue())){
+                max_h=Math.max(max_h,entry.getKey());
             }
         }
-
-        return max_h;
+       return max_h;
     }
 }
-// Title: H-Index
