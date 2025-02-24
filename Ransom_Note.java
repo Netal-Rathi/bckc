0288eab73bbccd85dd03d3f4da107384e8f3582c
Netal-Rathi <netalrathi2707@gmail.com>
2025-02-25 01:34:17 +0530

    Add Ransom Note

--- Ransom_Note.java
+++ Ransom_Note.java
@@ -7,20 +7,24 @@ class Solution {
             return false;
         }
 
-        int [] count =new int[26];
-        for(char c:magazine.toCharArray()){
-            count[c-'a']++;
+        HashMap<Character, Integer> rn = new HashMap<>();
+        HashMap<Character, Integer> mag = new HashMap<>();
 
+        for (int i = 0; i < ransomNote.length(); i++) {
+            rn.put(ransomNote.charAt(i), rn.getOrDefault(ransomNote.charAt(i), 0) + 1);
         }
 
-        for(char c : ransomNote.toCharArray()){
-            if(count[c-'a']==0){
+        for (int i = 0; i < magazine.length(); i++) {
+            mag.put(magazine.charAt(i), mag.getOrDefault(magazine.charAt(i), 0) + 1);
+        }
+
+        // Compare character counts
+        for (Map.Entry<Character, Integer> entry : rn.entrySet()) {
+            if (!mag.containsKey(entry.getKey()) || entry.getValue() > mag.get(entry.getKey())) {
                 return false;
-            }else{
-                count[c-'a']--;
             }
         }
 
-      return true;
+        return true;
     }
 }
