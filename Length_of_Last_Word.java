e98c55fcc3c63bbbaf004db3e1f2f8372b4420fd
Netal-Rathi <netalrathi2707@gmail.com>
2025-02-24 16:11:21 +0530

    Add Length of Last Word

--- Length_of_Last_Word.java
+++ Length_of_Last_Word.java
@@ -1,11 +1,12 @@
 class Solution {
     public int lengthOfLastWord(String s) {
+       
         int i=s.length()-1;
         while(s.charAt(i)==' '){
             i--;
         }
         int ans=0;
-        while(s.charAt(i)!=' '){
+        while(  i>=0 && s.charAt(i)!=' ' ){
             ans++;
             i--;
         }
