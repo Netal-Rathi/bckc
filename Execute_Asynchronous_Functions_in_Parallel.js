dec5122514cede40039ba05fa8e616885be8b86e
Netal-Rathi <netalrathi2707@gmail.com>
2025-02-25 19:51:26 +0530

    Add title comment to Execute Asynchronous Functions in Parallel

--- Execute_Asynchronous_Functions_in_Parallel.js
+++ Execute_Asynchronous_Functions_in_Parallel.js
@@ -23,3 +23,4 @@ var promiseAll = async function(functions) {
  * const promise = promiseAll([() => new Promise(res => res(42))])
  * promise.then(console.log); // [42]
  */
+// Title: Execute Asynchronous Functions in Parallel
