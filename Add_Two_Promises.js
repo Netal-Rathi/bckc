cd6930b912b308d404ad758854c7fbaf4932fb6d
Netal-Rathi <netalrathi2707@gmail.com>
2025-02-26 03:43:24 +0530

    Add Add Two Promises

---
+++ Add_Two_Promises.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    let one = await promise1.then();
+    let two = await promise2.then();
+    let data = one + two;
+
+    let ans = new Promise((resolve, reject) => {
+        resolve(data);
+    })
+
+    return ans;
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
