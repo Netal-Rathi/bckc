66fd407bbd8ddde61bec71f701a8b79e0b9876fe
Netal-Rathi <netalrathi2707@gmail.com>
2025-02-26 03:56:24 +0530

    Add Sort By

---
+++ Sort_By.js
@@ -0,0 +1,15 @@
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function(arr, fn) {
+    return arr.sort((a,b)=> fn(a)-fn(b));
+};
+
+// (a, b) => a-b; sort no. in ascending order
+
+// It should return a number where:
+// -> A negative value indicates thtat a should come before b.
+// -> A Positive value indicates thtat a should come after b.
+// -> Zero or Null indicates that a and b are considered equal.
