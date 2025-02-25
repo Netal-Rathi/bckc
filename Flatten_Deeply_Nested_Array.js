d7a07f63d80cb03c675f3879463fee00931f3c99
Netal-Rathi <netalrathi2707@gmail.com>
2025-02-26 03:59:49 +0530

    Add Flatten Deeply Nested Array

---
+++ Flatten_Deeply_Nested_Array.js
@@ -0,0 +1,29 @@
+/**
+ * @param {Array} arr
+ * @param {number} depth
+ * @return {Array}
+ */
+
+var flat = function (arr, n) {
+    if(n === 0){
+        console.log(arr);
+        return arr.slice()
+    }  // it create shallow copy of array
+    // Q. Why we are using slice method ...
+    // Ans. This is because in case we have to modify original array then it create some conflict.
+
+    let flatarr = [];
+
+    for(let i=0; i<arr.length; i++){
+        if(Array.isArray(arr[i])){ // this check that at index i there is array or not
+            const nested = flat(arr[i], n-1); //if n=0 then it hit base case and return as it is...
+            flatarr.push(...nested);
+        }else{
+            flatarr.push(arr[i]);
+        }
+    }
+    // [1,2,3,[4,5,6],[7,8,[9,10,11],12],[13,14,15]]
+
+    return flatarr;
+
+};
