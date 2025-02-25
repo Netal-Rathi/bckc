6e9d79b5f331c44838c90058d9eddc04e80e7cc7
Netal-Rathi <netalrathi2707@gmail.com>
2025-02-26 03:41:39 +0530

    Add Return Length of Arguments Passed

---
+++ Return_Length_of_Arguments_Passed.js
@@ -0,0 +1,11 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    let count = 0;
+    for(let i=args.length -1; i>=0; i--){
+        count++;
+    }
+    return count;
+};
