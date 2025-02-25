77aa481513a3eb33302be7aba685b4ee7de025cc
Netal-Rathi <netalrathi2707@gmail.com>
2025-02-25 23:58:08 +0530

    Add Ransom Note

---
+++ Ransom_Note.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) {
+            return false;
+        }
+
+        int [] count =new int[26];
+        for(char c:magazine.toCharArray()){
+            count[c-'a']++;
+
+        }
+
+        for(char c : ransomNote.toCharArray()){
+            if(count[c-'a']==0){
+                return false;
+            }else{
+                count[c-'a']--;
+            }
+        }
+
+      return true;
+    }
+}
