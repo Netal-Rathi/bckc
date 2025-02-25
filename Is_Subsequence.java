8689be697f0e639111a3bc835c3654c9194c289d
Netal-Rathi <netalrathi2707@gmail.com>
2025-02-26 03:32:50 +0530

    Add Is Subsequence

--- Is_Subsequence.java
+++ Is_Subsequence.java
@@ -1,11 +1,18 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
+        if(s.length()==0){
+            return true;
+
+        }
         if(s.length()> t.length()){
             return false;
         }
         int j=0;
         for(int i=0;i<t.length();i++){
-            if(s.charAt(j)==t.charAt(i)){
+            if(j==s.length()){
+                break;
+            }
+            if(t.charAt(i)==s.charAt(j)){
                 j++;
             }
         }
