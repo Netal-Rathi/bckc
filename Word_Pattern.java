e78b38f23fdfa2c87efc527a26ecdd041cf2af02
Netal-Rathi <netalrathi2707@gmail.com>
2025-02-26 11:59:59 +0530

    Add Word Pattern

---
+++ Word_Pattern.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String words[]=s.split(" ");
+        if(pattern.length()!=words.length){
+              System.out.println(" 1st false");
+            return false;
+
+        }
+        
+        HashMap<Character,String> map=new HashMap<>();
+        for(int i=0;i<pattern.length();i++){
+            if(map.containsKey(pattern.charAt(i)) && !(map.get(pattern.charAt(i)).equals(words[i]))){
+                System.out.println(" 2nd false");
+                return false;
+                
+            }
+
+            if(!map.containsKey(pattern.charAt(i)) &&  !map.containsValue(words[i])){
+                map.put(pattern.charAt(i),words[i]);
+            }else{
+             if(!map.containsKey(pattern.charAt(i)) &&  map.containsValue(words[i])){
+                return false;
+            }
+            }
+
+        }
+        return true;
+        
+    }
+}
