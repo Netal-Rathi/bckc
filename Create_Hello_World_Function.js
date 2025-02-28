b71626924f3c0edd2a6ec6a246dd4d7630915f26
Netal-Rathi <netalrathi2707@gmail.com>
2025-03-01 01:35:21 +0530

    Add Create Hello World Function

--- Create_Hello_World_Function.js
+++ Create_Hello_World_Function.js
@@ -1,5 +1,14 @@
-var createHelloWorld = function() { 
-    return function() {
-        return "Hello World";
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        
     }
-}
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
