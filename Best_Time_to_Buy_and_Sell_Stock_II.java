98afcc74d4644b5487b3d0ee9e825b9cd2af2686
Netal-Rathi <netalrathi2707@gmail.com>
2025-03-01 13:36:04 +0530

    Add Best Time to Buy and Sell Stock II

---
+++ Best_Time_to_Buy_and_Sell_Stock_II.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+       int maxsell=prices[0], buy= prices[0] , profit=0;
+       for(int i=1;i<prices.length;i++){
+        if(prices[i]>=maxsell){
+            maxsell=prices[i];
+        }else{
+            profit+= maxsell- buy;
+            maxsell=prices[i];
+            buy=prices[i];
+        }
+
+       }
+       profit+= maxsell- buy;
+       return profit ;
+
+    }
+}
