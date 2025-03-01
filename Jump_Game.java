457b7dfde9951219eaedc1647cc9e1a89c35a851
Netal-Rathi <netalrathi2707@gmail.com>
2025-03-01 05:42:59 +0530

    Add Jump Game

--- Jump_Game.java
+++ Jump_Game.java
@@ -1,17 +1,18 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int maxjump=0;
-        for(int i=0;i<nums.length;i++){
-            if(i>maxjump){
-                return false;
-            }
-            maxjump=Math.max(maxjump,i+nums[i]);
-            if(maxjump>=nums.length-1){
+        if(nums.length==1){
                 return true;
             }
+        for(int i=0;i<nums.length;i++){
+            
+            if(i+nums[i]<nums.length){
+                i=i+nums[i];
+            }else{
+                
+                return false;
+            }
         }
-        return false;
+        return true ;
         
     }
 }
-// Title: Jump Game
