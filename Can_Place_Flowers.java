3f3522b5d69c622084135318a24e70a4c2736b22
Netal-Rathi <netalrathi2707@gmail.com>
2025-03-03 05:06:03 +0530

    Add Can Place Flowers

--- Can_Place_Flowers.java
+++ Can_Place_Flowers.java
@@ -1,14 +1,28 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-      
-        for(int i=1;i<flowerbed.length-1;i++){
-            if(flowerbed[i-1]==0 && flowerbed[i+1]==0){
-                flowerbed[i]=1;
+        if(n==0){
+            return true;
+        }
+        if (flowerbed.length==1 ) {
+             if(flowerbed[0]==0 &&  n==1){
+            return true;
+             }else{
+                return false;
+             }
+        }
+
+        for (int i = 0; i < flowerbed.length; i++) {
+
+            if ( (i == 0 && flowerbed[i] == 0 && flowerbed[i + 1] == 0) || (i == flowerbed.length - 1 && flowerbed[i] == 0 && flowerbed[i - 1] == 0)
+                    || (i > 0 && flowerbed[i] == 0 && flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0))
+                     {
+                flowerbed[i] = 1;
                 n--;
             }
 
         }
-        return n==0 ? true : false ;
-        
+        System.out.println(n);
+        return n <= 0 ? true : false;
+
     }
 }
