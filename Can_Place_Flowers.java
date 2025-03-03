0ddf1f0fc3d3a15ceccffbf57258a8ebabc9ddff
Netal-Rathi <netalrathi2707@gmail.com>
2025-03-03 12:51:20 +0530

    Add Can Place Flowers

--- Can_Place_Flowers.java
+++ Can_Place_Flowers.java
@@ -5,13 +5,13 @@ class Solution {
         }
       
         for(int i=1;i<flowerbed.length-1;i++){
-            if(flowerbed[i]==0 && flowerbed[i-1]==0 && flowerbed[i+1]==0 ){
+            if(flowerbed[i-1]==0 && flowerbed[i+1]==0){
                 flowerbed[i]=1;
                 n--;
             }
 
         }
-        return n<=0 ? true : false ;
+        return n==0 ? true : false ;
         
     }
 }
