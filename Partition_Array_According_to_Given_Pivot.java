2b8e17da91c63882ca8f079404f1701895ba0506
Netal-Rathi <netalrathi2707@gmail.com>
2025-03-03 11:06:22 +0530

    Add Partition Array According to Given Pivot

---
+++ Partition_Array_According_to_Given_Pivot.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+     //   int count=0;
+        int[] ans=new int[nums.length];
+        int i=0,k=0,j=nums.length-1,l=nums.length-1;
+        while(i<nums.length && j>=0){
+            if(nums[i]<pivot){
+                ans[k]=nums[i];
+                k++;
+                
+            }
+
+        if(nums[j]>pivot){
+            ans[l]=nums[j];
+            l--;
+        }
+        i++;
+        j--;
+        }
+        for(int m=k;m<=l;m++){
+            ans[m]=pivot;
+        }
+                    
+
+                   return ans;
+            
+        }
+
+    
+}
