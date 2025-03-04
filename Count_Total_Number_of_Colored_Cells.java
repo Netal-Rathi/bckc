9bfa6c67df0d34397e4f56d60c046a002d8ecd9b
Netal-Rathi <netalrathi2707@gmail.com>
2025-03-05 03:51:28 +0530

    Add Count Total Number of Colored Cells

--- Count_Total_Number_of_Colored_Cells.java
+++ Count_Total_Number_of_Colored_Cells.java
@@ -1,9 +1,11 @@
 class Solution {
     public long coloredCells(int n) {
-        
-       return 1 + (long) (n) * (n-1) * 2 ;
+        if(n==0){
+            return 0;
+        }
 
+       return 1 + (long) (n) * (n-1) * 2 ;
+       
         
     }
 }
-// Title: Count Total Number of Colored Cells
