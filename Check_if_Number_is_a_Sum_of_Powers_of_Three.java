ad0ec9c3b20a2fd9539626d1a86525a25db3a9c2
Netal-Rathi <netalrathi2707@gmail.com>
2025-03-04 10:11:41 +0530

    Add Check if Number is a Sum of Powers of Three

---
+++ Check_if_Number_is_a_Sum_of_Powers_of_Three.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean checkPowersOfThree(int n) {
+       ArrayList<Integer> list=new ArrayList<>();
+        while(n>0){
+            int x=0;
+            while(Math.pow(3,x)<=n){
+                x++;
+
+            }
+            x--;
+            if(list.contains(x)){
+                return false;
+            }
+            int y=(int)Math.pow(3,x);
+            list.add(x);
+            n=n-y;
+        }
+        return n==0 ? true :false;
+    }
+}
