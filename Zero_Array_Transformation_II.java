c01e5d7f1d8ea795c5218b16ad10c5db681d99a3
Netal-Rathi <netalrathi2707@gmail.com>
2025-03-13 02:50:48 +0530

    Add Zero Array Transformation II

--- Zero_Array_Transformation_II.java
+++ Zero_Array_Transformation_II.java
@@ -1,10 +1,6 @@
 class Solution {
     public int minZeroArray(int[] nums, int[][] queries) {
-        if(iszero(nums)){
-            return 0;
-        }
         int count=0;
-        int [] temp=java.util.Arrays.copyOf(nums,queries.length);
         for(int [] arr : queries){
             for(int i=arr[0];i<=arr[1];i++){
                 if(arr[2] >= nums[i]){
@@ -13,9 +9,7 @@ class Solution {
                     nums[i]=nums[i]-arr[2];
                 }
             }
-            if(!Arrays.equals(temp,nums)){
             count++;
-            }
             if(iszero(nums)){
                 return count;
             }
@@ -31,4 +25,3 @@ class Solution {
         return true;
     }
 }
-// Title: Zero Array Transformation II
