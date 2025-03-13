69d129846851829e08057eaa07848fc1275aa8c5
Netal-Rathi <netalrathi2707@gmail.com>
2025-03-13 21:18:42 +0530

    Add Zero Array Transformation II

--- Zero_Array_Transformation_II.java
+++ Zero_Array_Transformation_II.java
@@ -1,25 +1,29 @@
 class Solution {
 
     public int minZeroArray(int[] nums, int[][] queries) {
-            int [] ans=new int[nums.length+1];
-            int k=0,sum=0;
-            for(int i=0;i<nums.length;i++){
-                while(sum+ans[i]<nums[i]){
-                    k++;
-                    if(k>queries.length){
-                        return -1;
-                    }
-                        int left=queries[k-1][0]  , right=queries[k-1][1]  , val=queries[k-1][2]  ;
-                            if(right>=i){
-                                ans[Math.max(left,i)]+=val;
-                                ans[right+1]-=val;
-                            }
-                    
+        int n = nums.length, sum = 0, k = 0;
+        int[] differenceArray = new int[n + 1];
 
-                        }
-                        sum+=ans[i];
+        for (int index = 0; index < n; index++) {
+
+            while (sum + differenceArray[index] < nums[index]) {
+                k++;
+
+               
+                if (k > queries.length) {
+                    return -1;
+                }
+                int left = queries[k - 1][0], right = queries[k - 1][1], val =
+                    queries[k - 1][2];
+
+                if (right >= index) {
+                    differenceArray[Math.max(left, index)] += val;
+                    differenceArray[right + 1] -= val;
+                }
             }
-            return k;
+      
+            sum += differenceArray[index];
+        }
+        return k;
     }
 }
-// Title: Zero Array Transformation II
