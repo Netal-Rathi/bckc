c3be5b4c48045bfada3d1cc1c1ac70c3c0b6fde4
Netal-Rathi <netalrathi2707@gmail.com>
2025-03-17 02:27:27 +0530

    Add Divide Array Into Equal Pairs

--- Divide_Array_Into_Equal_Pairs.java
+++ Divide_Array_Into_Equal_Pairs.java
@@ -1,19 +1,18 @@
 class Solution {
-
     public boolean divideArray(int[] nums) {
-
-        Set<Integer> unpaired = new HashSet<>();
-
-     
-        for (int num : nums) {
-            if (unpaired.contains(num)) {
-                unpaired.remove(num);
-            } else {
-                unpaired.add(num);
-            }
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int count=nums.length/2;
+        for(int i : nums){
+            map.put(i,map.getOrDefault(i,0)+1);
         }
+        for(Map.Entry <Integer,Integer> entry:map.entrySet()){
+            if(entry.getValue()%2!=0){
+                return false;
 
-        return unpaired.isEmpty();
+            }
+
+        }
+        return true ;
+        
     }
 }
-// Title: Divide Array Into Equal Pairs
