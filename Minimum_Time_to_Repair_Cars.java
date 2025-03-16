a3082285832f9938c508256a40bbd567fd86aa90
Netal-Rathi <netalrathi2707@gmail.com>
2025-03-16 19:03:26 +0530

    Add Minimum Time to Repair Cars

---
+++ Minimum_Time_to_Repair_Cars.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public long repairCars(int[] ranks, int cars) {
+     //   Arrays.sort(ranks);
+    long left=1;
+    long right=(long) ranks[0]*(long) cars* (long) cars;
+    long ans=right;
+
+    while(left<=right){
+      long mid=left+(right-left ) /2;
+      if(isall(ranks,cars,mid)){
+        ans=mid;
+        right=mid-1;
+      }else{
+        left=mid+1;
+      }
+
+    }
+    return ans;
+    }
+
+    public boolean isall(int[] ranks , int cars , long mid){
+long current=0;
+        for(int rank : ranks){
+            current+=(long)Math.sqrt(mid/rank);
+            if(current>=cars){
+                return true;
+            }
+           
+        }
+         return current >= cars;
+    }
+     
+}
