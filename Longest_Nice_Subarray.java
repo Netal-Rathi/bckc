6a4e33348b8e90933f4b2488715aba958fc78276
Netal-Rathi <netalrathi2707@gmail.com>
2025-03-18 09:45:56 +0530

    Add Longest Nice Subarray

---
+++ Longest_Nice_Subarray.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int longestNiceSubarray(int[] nums) {
+        int maxsum = Integer.MIN_VALUE; 
+        int count = 0;
+        int checker = 0; 
+        
+        for (int left = 0, right = 0; right < nums.length; right++) {
+            while ((checker & nums[right]) != 0) { 
+              
+                checker ^= nums[left]; 
+                left++;
+            }
+            
+            
+            checker |= nums[right];
+            count = right - left + 1; 
+            maxsum = Math.max(maxsum, count);
+        }
+        
+        return maxsum > 1 ? maxsum : 1;
+    }
+}
