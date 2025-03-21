7a949813b59ca934c4f4825e39ede06b84fd5096
Netal-Rathi <netalrathi2707@gmail.com>
2025-03-21 18:18:12 +0530

    Add Find All Possible Recipes from Given Supplies

--- Find_All_Possible_Recipes_from_Given_Supplies.java
+++ Find_All_Possible_Recipes_from_Given_Supplies.java
@@ -1,27 +1,25 @@
-import java.util.*;
-
 class Solution {
     public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
-        List<String> answers = new ArrayList<>();
-        List<String> supplyList = new ArrayList<>(Arrays.asList(supplies)); 
-
-        for (int i = 0; i < ingredients.size(); i++) {
-            boolean isValid = true; 
-            
-            for (String s : ingredients.get(i)) {
-                if (!supplyList.contains(s)) {  
-                    isValid = false;
-                    break;
+     List<String> supplyList = new ArrayList<>(Arrays.asList(supplies));
+List<String> answers=new ArrayList<>();
+boolean isvalid=true;
+int i=0;
+        for(List<String> ing: ingredients){
+            for(String s: ing){
+                if(!supplyList.contains(s)){
+                       
+                        isvalid=false;
+                         break;
                 }
             }
-            
-            if (isValid) {
-                supplyList.add(recipes[i]);  
-                answers.add(recipes[i]);  
+            if(isvalid){
+                supplyList.add(recipes[i]);
+                answers.add(recipes[i]);
             }
+            i++;
+
         }
         return answers;
+        
     }
 }
-
-// Title: Find All Possible Recipes from Given Supplies
