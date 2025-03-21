5911fca4b37efaf83e3842ed28cd13950a9dc99b
Netal-Rathi <netalrathi2707@gmail.com>
2025-03-21 18:58:30 +0530

    Add Find All Possible Recipes from Given Supplies

--- Find_All_Possible_Recipes_from_Given_Supplies.java
+++ Find_All_Possible_Recipes_from_Given_Supplies.java
@@ -1,54 +1,26 @@
 class Solution {
+    public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
+     List<String> supplyList = new ArrayList<>(Arrays.asList(supplies));
+List<String> answers=new ArrayList<>();
 
-    public List<String> findAllRecipes(
-        String[] recipes,
-        List<List<String>> ingredients,
-        String[] supplies
-    ) {
-        // Track available ingredients and recipes
-        Set<String> available = new HashSet<>();
-        for (String supply : supplies) {
-            available.add(supply);
-        }
 
-        // Queue to process recipe indices
-        Queue<Integer> recipeQueue = new LinkedList<>();
-        for (int idx = 0; idx < recipes.length; ++idx) {
-            recipeQueue.offer(idx);
-        }
-
-        List<String> createdRecipes = new ArrayList<>();
-        int lastSize = -1;
-
-        // Continue while we keep finding new recipes
-        while (available.size() > lastSize) {
-            lastSize = available.size();
-            int queueSize = recipeQueue.size();
-
-            // Process all recipes in current queue
-            while (queueSize-- > 0) {
-                int recipeIdx = recipeQueue.poll();
-                boolean canCreate = true;
-
-                // Check if all ingredients are available
-                for (String ingredient : ingredients.get(recipeIdx)) {
-                    if (!available.contains(ingredient)) {
-                        canCreate = false;
-                        break;
-                    }
-                }
-
-                if (!canCreate) {
-                    recipeQueue.offer(recipeIdx);
-                } else {
-                    // Recipe can be created - add to available items
-                    available.add(recipes[recipeIdx]);
-                    createdRecipes.add(recipes[recipeIdx]);
+   for (int i=0;i<recipes.length;i++){
+    boolean isvalid=true;
+            for(String s: ingredients.get(i)){
+                if(!supplyList.contains(s)){
+                       
+                        isvalid=false;
+                         break;
                 }
             }
-        }
+            if(isvalid){
+                supplyList.add(recipes[i]);
+                answers.add(recipes[i]);
+            }
+           // i++;
 
-        return createdRecipes;
+        }
+        return answers;
+        
     }
 }
-// Title: Find All Possible Recipes from Given Supplies
