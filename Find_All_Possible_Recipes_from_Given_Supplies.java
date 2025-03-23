ea5f05f5111c0d10ae4d21f37ed81760ed509385
Netal-Rathi <netalrathi2707@gmail.com>
2025-03-23 05:31:32 +0530

    Add Find All Possible Recipes from Given Supplies

--- Find_All_Possible_Recipes_from_Given_Supplies.java
+++ Find_All_Possible_Recipes_from_Given_Supplies.java
@@ -1,59 +1,50 @@
-class Solution {
+import java.util.*;
 
+class Solution {
     public List<String> findAllRecipes(
             String[] recipes,
             List<List<String>> ingredients,
             String[] supplies) {
-                HashSet<String> set=new HashSet<>(Arrays.asList(supplies));
-                // for(String s : supplies){
-                //     set.add(s);
-                // }
-            HashMap<String,Integer> ind=new HashMap<>();
-            HashMap<String,List<String>> adj=new HashMap<>();
-            Queue<String> q=new LinkedList<>();
-            List<String> ans=new ArrayList<>();
+                
+        HashSet<String> set = new HashSet<>(Arrays.asList(supplies)); 
+        HashMap<String, Integer> ind = new HashMap<>(); 
+        HashMap<String, List<String>> adj = new HashMap<>(); 
+        Queue<String> q = new LinkedList<>(); 
+        List<String> ans = new ArrayList<>();
 
-                for(int i=0;i<recipes.length;i++){
-                       ind.put(recipes[i], 0); 
-                   
-                    for(String s : ingredients.get(i)){
-                      
-                        if(!set.contains(s)){
-
-
-                            adj.putIfAbsent(s,new ArrayList<>());
-                            adj.get(s).add(recipes[i]);
-                            ind.put(recipes[i],ind.getOrDefault(recipes[i],0)+1);
-
-                        }
-
-                    }
+        
+        for (int i = 0; i < recipes.length; i++) {
+          ind.put(recipes[i], 0);
+            for (String ingredient : ingredients.get(i)) {
+                if (!set.contains(ingredient)) { 
+                    adj.putIfAbsent(ingredient, new ArrayList<>());
+                    adj.get(ingredient).add(recipes[i]); 
+                    ind.put(recipes[i], ind.getOrDefault(recipes[i], 0) + 1); 
                 }
+            }
+        }
 
-                for(Map.Entry<String,Integer> entry:ind.entrySet()){
-                    if(entry.getValue()==0){
-                    q.offer(entry.getKey());
-                    }
+        
+        for (String recipe : recipes) {
+            if (ind.get(recipe) == 0) {
+                q.offer(recipe);
+            }
+        }
+
+       
+        while (!q.isEmpty()) {
+            String node = q.poll();
+            ans.add(node);
+
+            for (String n : adj.getOrDefault(node, new ArrayList<>())) {
+                ind.put(n, ind.get(n) - 1);
+                if (ind.get(n) == 0) {
+                    q.offer(n);
                 }
+            }
+        }
 
-                while(!q.isEmpty()){
-                    String node=q.poll();
-                    ans.add(node);
-
-                        for( String n : adj.getOrDefault(node,new ArrayList<>())){
-                            ind.put(n,ind.get(n)-1);
-
-                            if(ind.get(n)==0){
-                                q.offer((n));
-                            }
-
-                        }
-
-
-                }
-              
-                return ans;
-
+        return ans;
     }
+
 }
-// Title: Find All Possible Recipes from Given Supplies
