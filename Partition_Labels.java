fac39d9f4fd147dab1caf8293d357e8ceb710517
Netal-Rathi <netalrathi2707@gmail.com>
2025-03-30 01:16:24 +0530

    Add Partition Labels

--- Partition_Labels.java
+++ Partition_Labels.java
@@ -1,33 +1,33 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
-        List<Integer> ans=new ArrayList<>();
-        HashMap<Character,List<Integer>> map=new HashMap<>();
-        for(int i=0;i<s.length();i++){
-           Character ind=s.charAt(i);
-           map.putIfAbsent(ind,new ArrayList<>());
-           map.get(ind).add(i);
-        }
-        int i=0;
-        while(i!=s.length()){
-            HashSet <Character> set=new HashSet<>();
-            int start =map.get(s.charAt(i)).get(0);
-            int end =map.get(s.charAt(i)).get(map.get(s.charAt(i)).size()-1);
-            for(int j=start;j<=end;j++){
-                    Character ind=s.charAt(j);
-                set.add(ind);
+        List<Integer> ans = new ArrayList<>();
+        HashMap<Character, List<Integer>> map = new HashMap<>();
 
-                if(map.get(ind).get(map.get(ind).size()-1)>end){
-                    end=map.get(ind).get(map.get(ind).size()-1);
+        // Populate the map with character indices
+        for (int i = 0; i < s.length(); i++) {
+            map.putIfAbsent(s.charAt(i), new ArrayList<>());  // Ensure the key exists
+            map.get(s.charAt(i)).add(i);  // Add index to the list
+        }
+
+        int i = 0;
+        while (i < s.length()) {  // Loop condition corrected
+            HashSet<Character> set = new HashSet<>();
+            int start = map.get(s.charAt(i)).get(0);
+            int end = map.get(s.charAt(i)).get(map.get(s.charAt(i)).size() - 1);
+
+            for (int j = start; j <= end; j++) {
+                set.add(s.charAt(j));
+
+                // Fixing incorrect parentheses and index out-of-bounds
+                int lastIndex = map.get(s.charAt(j)).get(map.get(s.charAt(j)).size() - 1);
+                if (lastIndex > end) {
+                    end = lastIndex;
                 }
             }
-            ans.add(end-start+1);
-           
-            i=end+1;
-           
 
-            
+            ans.add(end - start + 1);
+            i = end + 1;  // Move i to the next partition
         }
         return ans;
     }
 }
-// Title: Partition Labels
