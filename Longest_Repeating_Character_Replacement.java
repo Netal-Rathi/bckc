44c46eb77f87017d1d1231021653177deed6f61b
Netal-Rathi <netalrathi2707@gmail.com>
2025-04-06 11:04:48 +0530

    Add Longest Repeating Character Replacement

---
+++ Longest_Repeating_Character_Replacement.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] count = new int[26]; // Equivalent to vector<int> cou(26, 0)
+        int n = s.length();
+        int ans = 0;
+       
+        int left = 0, right = 0;
+        int maxFreq = 0;
+        
+        while (right < n) {
+            char ch = s.charAt(right);
+            count[ch - 'A']++;
+            right++;
+            maxFreq = Math.max(count[ch - 'A'], maxFreq);
+
+            if ((right - left - maxFreq) > k) {
+                count[s.charAt(left) - 'A']--;
+                left++;
+            }
+            ans = Math.max(ans, right - left);
+        }
+        return ans;
+    }
+}
