8c87a6b6555856afd18df89c5f6f0e4b82aaf222
Netal-Rathi <netalrathi2707@gmail.com>
2025-04-06 11:42:28 +0530

    Add Maximum Points You Can Obtain from Cards

--- Maximum_Points_You_Can_Obtain_from_Cards.java
+++ Maximum_Points_You_Can_Obtain_from_Cards.java
@@ -1,26 +1,23 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
-        int n = cardPoints.length;
-        int totalSum = 0;
-        
-       
-        for (int i = 0; i < k; i++) {
-            totalSum += cardPoints[i];
-        }
-        
-        int maxSum = totalSum;
-        int left = k - 1; 
-        int right = n - 1; 
-        
-       
-        for (int i = 0; i < k; i++) {
-            totalSum = totalSum - cardPoints[left] + cardPoints[right];
-            maxSum = Math.max(maxSum, totalSum);
-            left--;
-            right--;
-        }
-        
-        return maxSum;
+        int n=cardPoints.length;
+    int maxsum=0,leftsum=0,left=k-1,rightsum=0,right=n-1;
+    for(int i=0;i<k;i++){
+       leftsum+=cardPoints[i];
+    }
+    maxsum=leftsum+rightsum;
+    int temp=0;
+
+    while(temp<k){
+        rightsum+=cardPoints[right];
+        leftsum-=cardPoints[left];
+        left--;
+        temp++;
+        right--;
+        maxsum=Math.max(maxsum,leftsum+rightsum);
+    }
+    
+
+return maxsum;
     }
 }
-// Title: Maximum Points You Can Obtain from Cards
