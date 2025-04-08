7ea2f045ede1251bd40a8e8fda6edfca065454a9
Netal-Rathi <netalrathi2707@gmail.com>
2025-04-09 01:45:35 +0530

    Add Count Number of Nice Subarrays

--- Count_Number_of_Nice_Subarrays.java
+++ Count_Number_of_Nice_Subarrays.java
@@ -1,24 +1,25 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        int start=0,prefixsubarray=0,oddcount=0,result=0;
-
-        for(int end=0;end<nums.length;end++){
-            if(nums[end]%2!=0){
-                oddcount++;
-              prefixsubarray=0;
+        int start = 0, oddCount = 0, result = 0;
+        int prefixSubarrays = 0; 
+        
+        for (int end = 0; end < nums.length; end++) {
+            if (nums[end] % 2 != 0) {
+                oddCount++;
+                prefixSubarrays = 0; 
             }
-
-            while(oddcount==k){
-                if(nums[start]%2!=0){
-                    oddcount--;
+            
+            while (oddCount == k) {
+                if (nums[start] % 2 != 0) {
+                    oddCount--;
                 }
-
-                prefixsubarray++;
+                prefixSubarrays++; 
                 start++;
             }
-            result+=prefixsubarray;
+            
+            result += prefixSubarrays;
         }
+        
         return result;
     }
 }
-// Title: Count Number of Nice Subarrays
