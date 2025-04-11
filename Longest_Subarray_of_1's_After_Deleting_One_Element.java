78cd22e176f6718fb872bbeae1b44f6ee8b1d20c
Netal-Rathi <netalrathi2707@gmail.com>
2025-04-11 17:44:00 +0530

    Add Longest Subarray of 1's After Deleting One Element

---
+++ Longest_Subarray_of_1's_After_Deleting_One_Element.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestSubarray(int[] nums) {
+        int left=0,max_count=0,count=0;
+        int x=0;
+        for(int i=0;i<nums.length;i++){
+                if(nums[i]==0){
+                    x++;
+                //    count+=1;
+                }
+                count++;
+
+                while(x>1){
+                    if(nums[left]==0){
+                        x--;
+                    }
+                
+                    count--;
+                    left++;
+                }
+                max_count=Math.max(count,max_count);
+
+        }
+        return max_count-1;
+    }
+}
