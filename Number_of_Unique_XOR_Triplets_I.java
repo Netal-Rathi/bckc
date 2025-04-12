3da0596dc31d3a9a955487a6eb058a5af1ed2663
Netal-Rathi <netalrathi2707@gmail.com>
2025-04-12 15:03:03 +0530

    Add Number of Unique XOR Triplets I

--- Number_of_Unique_XOR_Triplets_I.java
+++ Number_of_Unique_XOR_Triplets_I.java
@@ -1,33 +1,43 @@
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
-        Set<Integer> ans = new HashSet<>();
-        calculate(ans, nums, 0, 0, 0);
+     //   int i=0,j=0,k=0;
+        Set<Integer> ans=new HashSet<>();
+        calculate(ans,nums,0,0,0);
         return ans.size();
+        
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=i;j<nums.length;j++){
+        //         for(int k=j;k<nums.length;k++){
+        //             ans.add(nums[i]^nums[j]^nums[k]);
+        //         }
+        //     }
+        // }
+        // return ans.size();
+        
     }
     
-    public void calculate(Set<Integer> ans, int[] nums, int i, int j, int k) {
-      
-        if (i >= nums.length || j >= nums.length || k >= nums.length) {
+    public void calculate(Set<Integer> ans ,int nums[],int i,int j ,int k ){
+        if(i>=nums.length-1 || j>=nums.length-1 || k>=nums.length-1){
             return;
         }
+                ans.add(nums[i] ^ nums[j] ^ nums[k]);
+        
+        if(k<nums.length-1){
+
+        //first element fixedj+1
         
         
-        ans.add(nums[i] ^ nums[j] ^ nums[k]);
+        calculate(ans,nums,i,j,k+1);
+        }else if (j<nums.length-1) {
         
-      
-        if (k < nums.length - 1) {
-           
-            calculate(ans, nums, i, j, k + 1);
-        } else if (j < nums.length - 1) {
-           
-            calculate(ans, nums, i, j + 1, j + 1); 
-        } else {
+        //secon;
+        calculate(ans,nums,i,j+1,j+1);
             
-            calculate(ans, nums, i + 1, i + 1, i + 1); 
+        }else{
+            calculate(ans,nums,i+1,i+1,i+1);
         }
+        
+        
+        
     }
 }
-// Title: Number of Unique XOR Triplets I
