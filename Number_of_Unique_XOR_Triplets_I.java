9b058d8e105cf90a51a7b08c955d541af8676f2a
Netal-Rathi <netalrathi2707@gmail.com>
2025-04-12 15:17:06 +0530

    Add Number of Unique XOR Triplets I

--- Number_of_Unique_XOR_Triplets_I.java
+++ Number_of_Unique_XOR_Triplets_I.java
@@ -1,18 +1,29 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
-        int n = nums.length;
-
-        if (n < 3) return n;
-
-        // Find the Most Significant Bit
-        int cnt = 0;
-        int temp = n;
-        while (temp > 0) {
-            temp >>= 1;
-            cnt++;
+        Set<Integer> uniqueXors = new HashSet<>();
+        HashMap<Integer, Integer> pairXors = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                int xor = nums[i] ^ nums[j];
+                pairXors.put(i * nums.length + j, xor);
+            }
         }
-
-        return (int)Math.pow(2, cnt);
+        
+        for (int k = 0; k < nums.length; k++) {
+            for (var entry : pairXors.entrySet()) {
+                int ij = entry.getKey();
+                int j = ij % nums.length;
+                if (j <= k) {
+                    uniqueXors.add(entry.getValue() ^ nums[k]);
+                }
+            }
+        }
+        
+        return uniqueXors.size();
     }
 }
-// Title: Number of Unique XOR Triplets I
