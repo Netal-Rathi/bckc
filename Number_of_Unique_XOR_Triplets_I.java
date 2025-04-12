8d14879798faa1f3fcbef436e93f8feddc6a5fb9
Netal-Rathi <netalrathi2707@gmail.com>
2025-04-12 18:51:45 +0530

    Add Number of Unique XOR Triplets I

---
+++ Number_of_Unique_XOR_Triplets_I.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+      if(nums.length==1){
+        return 1;
+      }
+      if(nums.length==2){
+        return 2;
+      }
+      //int n=nums.length;
+   
+      String b=Integer.toBinaryString(nums.length);
+     int n=b.length();
+      
+      return (int)Math.pow(2,n);
+    }
+}
