8b8bf587456bf1147acbbebe2860be81a4b67a79
Netal-Rathi <netalrathi2707@gmail.com>
2025-04-12 15:12:00 +0530

    Add Number of Unique XOR Triplets I

--- Number_of_Unique_XOR_Triplets_I.java
+++ Number_of_Unique_XOR_Triplets_I.java
@@ -4,35 +4,32 @@ import java.util.Set;
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
         Set<Integer> ans = new HashSet<>();
-        optimizedCalculate(ans, nums, 0, 0, 0);
+        calculate(ans, nums, 0, 0, 0);
         return ans.size();
     }
     
-    private void optimizedCalculate(Set<Integer> ans, int[] nums, int i, int j, int k) {
-
-        if (i >= nums.length) {
+    public void calculate(Set<Integer> ans, int[] nums, int i, int j, int k) {
+      
+        if (i >= nums.length || j >= nums.length || k >= nums.length ||  i > j || j > k) {
             return;
         }
         
         
-        if (j >= nums.length) {
-            optimizedCalculate(ans, nums, i + 1, i + 1, i + 1);
-            return;
+        ans.add(nums[i] ^ nums[j] ^ nums[k]);
+        
+      
+ if (k  < nums.length && j <= k + 1) {
+          calculate(ans, nums, i, j, k + 1);
         }
         
-       
-        if (k >= nums.length) {
-            optimizedCalculate(ans, nums, i, j + 1, j + 1);
-            return;
+        if (j < nums.length && i <= j + 1 && j + 1 <= k) {
+            
+            calculate(ans, nums, i, j + 1, k);
         }
         
-   
-        if (i <= j && j <= k) {
-            ans.add(nums[i] ^ nums[j] ^ nums[k]);
+        if (i < nums.length && i + 1 <= j && j <= k) {
+            
+            calculate(ans, nums, i + 1, j, k);
         }
-        
-     
-        optimizedCalculate(ans, nums, i, j, k + 1);
     }
 }
-// Title: Number of Unique XOR Triplets I
