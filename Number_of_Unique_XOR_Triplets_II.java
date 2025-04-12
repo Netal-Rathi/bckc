bf1fb64ebce3310d8edfc20c122dc5c4b1e1190d
Netal-Rathi <netalrathi2707@gmail.com>
2025-04-12 19:10:10 +0530

    Add Number of Unique XOR Triplets II

--- Number_of_Unique_XOR_Triplets_II.java
+++ Number_of_Unique_XOR_Triplets_II.java
@@ -1,5 +1,4 @@
-class Solution {
-      public int uniqueXorTriplets(int[] nums) {
+    public int uniqueXorTriplets(int[] nums) {
         // 0 used here because duplicates xor to 0.
         Set<Integer> pairs = new HashSet<>(List.of(0)); 
         for (int i = 0, n = nums.length; i < n; ++i) {
@@ -16,5 +15,3 @@ class Solution {
         }
         return triplets.cardinality();
     }
-}
-// Title: Number of Unique XOR Triplets II
