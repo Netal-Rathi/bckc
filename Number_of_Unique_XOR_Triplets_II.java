f8363883800cdf9dfb19e97788e87302fb98b72f
Netal-Rathi <netalrathi2707@gmail.com>
2025-04-12 18:57:59 +0530

    Add Number of Unique XOR Triplets II

--- Number_of_Unique_XOR_Triplets_II.java
+++ Number_of_Unique_XOR_Triplets_II.java
@@ -1,21 +1,16 @@
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
-        Set<Integer> set=new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                set.add(nums[i]^nums[j]);
-            }
-        }
-
-        Set<Integer> ans=new HashSet<>();
-       // int k=0;
-        for(Integer i :set){
-            for(int k:nums){
-            ans.add(k^i);
-           // k++;
-            }
-        }
-        return ans.size();
+         if(nums.length==1){
+        return 1;
+      }
+      if(nums.length==2){
+        return 2;
+      }
+        Arrays.sort(nums);
+        String a=Integer.toBinaryString(nums[0]);
+         String b=Integer.toBinaryString(nums.length-1);
+         int ans=(int)(Math.pow(2,b.length()))- (int)(Math.pow(2,a.length()));
+         return Math.abs(ans);
+        
     }
 }
-// Title: Number of Unique XOR Triplets II
