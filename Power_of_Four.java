772c6846926ac2480f8e8b65cb493aff94f9fa71
Netal-Rathi <netalrathi2707@gmail.com>
2025-04-15 18:19:40 +0530

    Add Power of Four

--- Power_of_Four.java
+++ Power_of_Four.java
@@ -1,21 +1,16 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        return (n!=2 && n>0 && isPowerOfTwo(n) && isevenplace(n));
+        return (n!=2 && powerof2(n) && isevenplace(n));
     }
-      public boolean isPowerOfTwo(int n) {
-       if(n>0 && (( n & (n-1))==0)){
-        return true;
-       }
-        return false;
+    public boolean powerof2(int n){
+        return (n>0 && ((n & (n-1))==0));
     }
 
     public boolean isevenplace(int n){
         String s=Integer.toBinaryString(n);
         s=new StringBuilder(s).reverse().toString();
-        System.out.println(s);
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='1'&& i%2!=0){
-                 System.out.println(i);
+            if(s.charAt(i)==1 && i%2!=0){
                 return false;
 
             }
@@ -23,4 +18,3 @@ class Solution {
         return true;
     }
 }
-// Title: Power of Four
