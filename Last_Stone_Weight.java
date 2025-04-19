82bffdf7a7139f40f25943709bc538727da6966a
Netal-Rathi <netalrathi2707@gmail.com>
2025-04-19 15:00:25 +0530

    Add Last Stone Weight

---
+++ Last_Stone_Weight.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+     List<Integer> list=new ArrayList<>();
+     for(int i : stones){
+        list.add(i);
+     }
+     int x=0;
+     int y=0;
+     while(list.size()>1){
+        Collections.sort(list,Collections.reverseOrder());
+        y=list.get(0);
+        x=list.get(1);
+         list.remove(1);
+         list.remove(0);
+       
+        if(x!=y){
+                list.add(y-x);
+        }
+
+     }
+       return list.isEmpty()? 0:list.get(0)  ;
+    }
+    
+        
+        
+}
