50f513a54c236968bdf319abd73d04f4943e1137
Netal-Rathi <netalrathi2707@gmail.com>
2025-04-26 15:34:34 +0530

    Add Count Cells in Overlapping Horizontal and Vertical Substrings

---
+++ Count_Cells_in_Overlapping_Horizontal_and_Vertical_Substrings.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countCells(char[][] grid, String pattern) {
+        int m=grid.length;
+        int n=grid[0].length;
+        if (pattern.length()==1 && m*n==1){
+            return 1;
+        }
+        Set<Set<String>> set1=new HashSet<>();
+        Set<Set<String>> set2=new HashSet<>();
+        
+        return 1;
+    }
+}
