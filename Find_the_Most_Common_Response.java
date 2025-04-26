890702dd2432a81beac8fcce480f1625f74e9381
Netal-Rathi <netalrathi2707@gmail.com>
2025-04-26 14:56:24 +0530

    Add Find the Most Common Response

--- Find_the_Most_Common_Response.java
+++ Find_the_Most_Common_Response.java
@@ -1,31 +1,45 @@
-import java.util.*;
-
 class Solution {
-    HashMap<String, Integer> map = new HashMap<>();
-
+    HashMap<String ,Integer> map=new HashMap<>();
+    
     public String findCommonResponse(List<List<String>> responses) {
-        for (List<String> response : responses) {
-            Set<String> uniqueWords = new HashSet<>();
-            for (String word : response) {
-                if (uniqueWords.add(word)) { // add() returns false if already exists
-                    map.put(word, map.getOrDefault(word, 0) + 1);
-                }
-            }
+        int count=0;
+        String fa="";
+   //     HashMap<String,Integer> mainmap=new HashMap<>();
+       for(List<String> s: responses){
+           finder(s);
+       }
+          for(Map.Entry<String,Integer> entry : map.entrySet()){
+          // count=Math.max(count,entry.getKey());
+              if(count<entry.getValue()){
+                  count=entry.getValue();
+                  fa=entry.getKey();
+              }else{
+                  if(count==entry.getValue()){
+                      if(fa.compareTo(entry.getKey())>0){
+                          fa=entry.getKey();
+                      }
+                      
+                  }
+              }
         }
-
-        String mostCommon = "";
-        int maxCount = 0;
-
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            if (entry.getValue() > maxCount || 
-               (entry.getValue() == maxCount && entry.getKey().compareTo(mostCommon) < 0)) {
-                maxCount = entry.getValue();
-                mostCommon = entry.getKey();
+        return fa; 
+        
+        
+    }
+    
+    public List<String> finder(List<String> li){
+        List<String> ans=new ArrayList<>();
+        for(String l: li){
+           // map.put(l,map.getOrDefault(l,0)+1);
+            if(!ans.contains(l)){
+                ans.add(l);
+                map.put(l,map.getOrDefault(l,0)+1);
+                
             }
+            
         }
-
-        return mostCommon;
+       
+        return ans;
+        
     }
 }
-
-// Title: Find the Most Common Response
