fe825a06379fb1e8a2b620aacb02af9e936b74ae
Netal-Rathi <netalrathi2707@gmail.com>
2025-04-30 06:56:14 +0530

    Add Summary Ranges

--- Summary_Ranges.java
+++ Summary_Ranges.java
@@ -1,29 +1,29 @@
 class Solution {
     public List<String> summaryRanges(int[] nums) {
-        List<String> list=new ArrayList<>();
+        List<String> list = new ArrayList<>();
+        if (nums == null || nums.length == 0) return list;
 
-        int start=nums[0];
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]!=nums[i-1]+1){
-                    if(start!=nums[i-1]){
-                        list.add(start + "->" + nums[i-1]);
-                       // start=nums[i];
-                        
-                    }else{
-                        list.add(String.valueOf(start));
-                       // start=nums[i];
-                    }
-                    start=nums[i];
+        int start = nums[0]; // Starting number of the current range
+
+        for (int i = 1; i < nums.length; i++) {
+            // If the sequence breaks
+            if (nums[i] != nums[i - 1] + 1) {
+                if (start == nums[i - 1]) {
+                    list.add(String.valueOf(start));
+                } else {
+                    list.add(start + "->" + nums[i - 1]);
+                }
+                start = nums[i]; // Reset start to the new number
             }
+        }
 
-        }
-        if(start==nums[nums.length-1]){
+        // Handle the last range after loop
+        if (start == nums[nums.length - 1]) {
             list.add(String.valueOf(start));
-        }else{
-            list.add(start + "->" + nums[nums.length-1]);
+        } else {
+            list.add(start + "->" + nums[nums.length - 1]);
         }
-  
+
         return list;
     }
 }
-// Title: Summary Ranges
