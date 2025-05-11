0cbf8272afc741fca2a3a55fa5a7618a90b288ea
Netal-Rathi <netalrathi2707@gmail.com>
2025-05-12 04:56:35 +0530

    Add Finding 3-Digit Even Numbers

--- Finding_3-Digit_Even_Numbers.java
+++ Finding_3-Digit_Even_Numbers.java
@@ -1,26 +1,38 @@
 class Solution {
     public int[] findEvenNumbers(int[] digits) {
-      Set<Integer> set=new HashSet<>();
-int n=digits.length;
-      for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            for(int k=0;k<n;k++){
-                if(i==j || i==k || j==k) 
-                continue;
-               
-                if(digits[k]%2!=0)
-                continue;
-                if(digits[i]==0)
-                continue;
-                set.add(digits[i]*100+digits[j]*10+digits[k]);
-            
-                
+        Arrays.sort(digits);
+        List<Integer> list=new ArrayList<>();
+
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i] % 2 == 0) {
+                for (int j = 0; j < digits.length - 1; j++) {
+
+                    for (int k = j + 1; k < digits.length; k++) {
+                        if (i != j && i!=k) {
+                            if (digits[j] != 0) {
+                                int num1=digits[j] * 100 + digits[k] * 10 + digits[i];
+                                if(!list.contains(num1)){
+                                list.add(num1);
+                                }
+                            }
+                            if (digits[k] != 0) {
+                                int num2=digits[k] * 100 + digits[j] * 10 + digits[i];
+
+                                if(!list.contains(num2)){
+                                list.add(num2);
+                                }
+                            }
+                        }
+
+                    }
+                }
             }
         }
-      }
-      return set.stream().sorted().mapToInt(i->i).toArray();
+        Collections.sort(list);
+        int ans[]=list.stream().mapToInt(i->i).toArray();
+        return ans;
+        
         
 
     }
 }
-// Title: Finding 3-Digit Even Numbers
