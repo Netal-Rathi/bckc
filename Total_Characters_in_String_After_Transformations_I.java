fd78501597c6ed78716bc907a7fca4f861e46583
Netal-Rathi <netalrathi2707@gmail.com>
2025-05-13 12:44:30 +0530

    Add Total Characters in String After Transformations I

---
+++ Total_Characters_in_String_After_Transformations_I.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int lengthAfterTransformations(String s, int t) {
+        // 97 -122 
+                int mod=1000000007;
+                int ans=0;
+                int[] count=new int [26];
+                for(char ch:s.toCharArray()){
+                    count[ch-'a']++;
+                }
+
+                for(int i=0;i<t;i++){
+                    int [] arr=new int [26];
+                    arr[0]=count[25]%mod;
+                    arr[1]=(count[25] + count [0])%mod;
+                    for(int j=2;j<26;j++){
+                        arr[j]=count[j-1]%mod;
+                    }
+                    count=arr;
+                }
+
+                for(int i=0;i<26;i++){
+                    ans=(ans+count[i])%mod;
+                }
+                return ans ;
+
+    }
+    
+}
