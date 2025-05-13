cea7b057770f410e2327b11e9e78c69e1853c8cf
Netal-Rathi <netalrathi2707@gmail.com>
2025-05-14 04:10:24 +0530

    Add Next Greater Element I

--- Next_Greater_Element_I.java
+++ Next_Greater_Element_I.java
@@ -1,22 +1,35 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-       HashMap<Integer,Integer> map=new HashMap<>();
-       Stack<Integer> stack=new Stack<>();
-       for(int num : nums2){
-        while(!stack.isEmpty() && num>stack.peek()){
-            map.put(stack.pop(),num);
+        int ans[]=new int[nums1.length];
+        
+        int k=0;
 
+        for(int num : nums1){
+            int i=0;
+            boolean check=false;
+            while(num!=nums2[i]){
+                i++;
+            }
+            if(i==nums2.length-1){
+               ans[k]=-1;
+                k++;
+            }else{
+            for(int j=i+1;j<nums2.length;j++){
+
+                if(nums2[i]<nums2[j]){
+                        ans[k]=nums2[j];
+                        k++;
+                        check=true;
+                        break;
+                }
+            }
+            System.out.println(!check);
+            if(!check){
+            ans[k]=-1;
+            k++;
+            }
+            }
         }
-        stack.push(num);
-       }
-       while(!stack.isEmpty()){
-        map.put(stack.pop(),-1);
-       }
-       int arr[]=new int [nums1.length];
-       for(int i=0;i<nums1.length;i++){
-        arr[i]=map.get(nums1[i]);
-       }
-       return arr;
+        return ans;
     }
 }
-// Title: Next Greater Element I
