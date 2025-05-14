d2f8ad18f090f737518b0d28ca5ceddcff02126b
Netal-Rathi <netalrathi2707@gmail.com>
2025-05-14 06:22:32 +0530

    Add Next Greater Element II

--- Next_Greater_Element_II.java
+++ Next_Greater_Element_II.java
@@ -1,14 +1,4 @@
 class Solution {
-   public int popfrombottom(Stack<Integer> s1){
-    int top = s1.pop();
-    if (s1.isEmpty()) {
-        return top;  // ✅ base case: bottom element
-    }
-    int bottom = popfrombottom(s1);
-    s1.push(top);   // ✅ restore the stack after recursion
-  return bottom;
-}
-
     public int[] nextGreaterElements(int[] nums) {
               HashMap<Integer,Integer> map=new HashMap<>();
        Stack<Integer> stack=new Stack<>();
@@ -19,34 +9,23 @@ class Solution {
         }
         stack.push(num);
        }
-while(!stack.isEmpty()){
-    int bottom=popfrombottom(stack);
-    while( !stack.isEmpty() && bottom>stack.peek()){
-        map.put(stack.pop(),bottom);
-    }
-    map.put(bottom,-1);
-}
-    //    while(!stack.isEmpty()){
-    //     int top=stack.pop();
-    //     if(stack.isEmpty()){
-    //         map.put(top,-1);
 
-    //     }else{
-    //         map.put(top,stack.peek());
-    //     }
-    //    }
-     
+       while(!stack.isEmpty()){
+        int top=stack.pop();
+        if(stack.isEmpty()){
+            map.put(top,-1);
+
+        }else{
+            map.put(top,stack.peek());
+        }
+       }
   int arr[]=new int [nums.length];
        for(int i=0;i<nums.length;i++){
         arr[i]=map.get(nums[i]);
        }
        return arr;
-
-   
        }
 
 
 
     }
-
-// Title: Next Greater Element II
