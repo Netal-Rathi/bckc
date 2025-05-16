092ff63b031ed3ce2ad25ba40bfbd38c8cb8d220
Netal-Rathi <netalrathi2707@gmail.com>
2025-05-16 16:41:47 +0530

    Add Reverse Bits

---
+++ Reverse_Bits.java
@@ -0,0 +1,7 @@
+public class Solution {
+    public int reverseBits(int n) {
+        String binary = String.format("%32s", Integer.toBinaryString(n)).replace(' ', '0');
+        String reversed = new StringBuilder(binary).reverse().toString();
+        return (int) Long.parseLong(reversed, 2);  // Cast to int as return type
+    }
+}
