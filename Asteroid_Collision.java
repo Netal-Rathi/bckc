f5504a68fdf241ed0d9443a6ea964f2016844229
Netal-Rathi <netalrathi2707@gmail.com>
2025-05-26 17:34:27 +0530

    Add Asteroid Collision

---
+++ Asteroid_Collision.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack=new Stack<>();
+        int n=asteroids.length;
+
+        for (int a : asteroids ){
+            boolean alive =true;
+          while(!stack.isEmpty() && a<0 && stack.peek()>0){
+            if(Math.abs(a)>stack.peek()){
+                stack.pop();
+                continue;
+            }else if (Math.abs(a)==stack.peek()){
+                stack.pop();
+
+            }
+             alive=false;
+             break;
+            
+          }
+            if(alive){
+            stack.push(a);
+        }
+     
+        }
+       int[] result = new int[stack.size()];
+        for (int i = stack.size() - 1; i >= 0; --i) {
+            result[i] = stack.pop();
+        }
+        
+        return result;
+    }
+    
+}
