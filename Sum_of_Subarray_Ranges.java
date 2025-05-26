1cbffbe0e58a3b895184b303439ffc54582f1e22
Netal-Rathi <netalrathi2707@gmail.com>
2025-05-27 03:55:52 +0530

    Add Sum of Subarray Ranges

--- Sum_of_Subarray_Ranges.java
+++ Sum_of_Subarray_Ranges.java
@@ -1,87 +1,87 @@
-import java.util.Arrays;
-import java.util.Stack;
-
 class Solution {
     public long subArrayRanges(int[] nums) {
-        long sumMax = maximum(nums);
-        long sumMin = minimum(nums);
-        return sumMax - sumMin;
+       long sum1=minimum(nums);
+       long sum2=maximum(nums);
+       return sum2-sum1;
+
+        
     }
+        public long minimum(int [] nums){
+             int n=nums.length;
+             int[] left =new int [n];
+             int [] right =new int [n];
+             Arrays.fill(left,-1);
+             Arrays.fill(right,n);
 
-    public long minimum(int[] nums) {
-        int n = nums.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
-        Arrays.fill(left, -1);
-        Arrays.fill(right, n);
+             Stack<Integer> stack=new Stack<>();
+             for(int i=0;i<n;i++){
+                while(!stack.isEmpty() && nums[i]<nums[stack.peek()]){
+                    stack.pop();
+                }
+                if(!stack.isEmpty()){
+                        left[i]=stack.peek();
+                }
+                stack.push(i);
+             }
+             stack.clear();
 
-        Stack<Integer> stack = new Stack<>();
-        // Left pass: find previous smaller element
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && nums[i] <= nums[stack.peek()]) {
-                stack.pop();
-            }
-            if (!stack.isEmpty()) {
-                left[i] = stack.peek();
-            }
-            stack.push(i);
-        }
-        stack.clear();
-
-        // Right pass: find next smaller element
-        for (int i = n - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && nums[i] < nums[stack.peek()]) {
-                stack.pop();
-            }
-            if (!stack.isEmpty()) {
-                right[i] = stack.peek();
-            }
-            stack.push(i);
-        }
-
-        long sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += ((long)(i - left[i]) * (right[i] - i)) * nums[i];
-        }
-        return sum;
-    }
-
-    public long maximum(int[] nums) {
-        int n = nums.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
-        Arrays.fill(left, -1);
-        Arrays.fill(right, n);
-
-        Stack<Integer> stack = new Stack<>();
-        // Left pass: find previous greater element
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && nums[i] >= nums[stack.peek()]) {
-                stack.pop();
-            }
-            if (!stack.isEmpty()) {
-                left[i] = stack.peek();
-            }
-            stack.push(i);
-        }
-        stack.clear();
-
-        // Right pass: find next greater element
-        for (int i = n - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && nums[i] > nums[stack.peek()]) {
-                stack.pop();
-            }
-            if (!stack.isEmpty()) {
-                right[i] = stack.peek();
-            }
-            stack.push(i);
+              for(int i=n-1;i>=0;i--){
+                while(!stack.isEmpty() && nums[i]<=nums[stack.peek()]){
+                    stack.pop();
+                }
+                if(!stack.isEmpty()){
+                        right[i]=stack.peek();
+                }
+               stack.push(i);
+             }
+                    long sum=0;
+             for(int i=0;i<n;i++){
+                sum+=((i-left[i])*(right[i]-i)*nums[i]);
+             }
+             System.out.println(Arrays.toString(left));
+              System.out.println(Arrays.toString(right));
+             System.out.println(sum);
+             return sum;
         }
 
-        long sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += ((long)(i - left[i]) * (right[i] - i)) * nums[i];
+
+
+              public long maximum(int [] nums){
+             int n=nums.length;
+             int[] left =new int [n];
+             int [] right =new int [n];
+             Arrays.fill(left,-1);
+             Arrays.fill(right,n);
+
+             Stack<Integer> stack=new Stack<>();
+             for(int i=0;i<n;i++){
+                while(!stack.isEmpty() && nums[i]>nums[stack.peek()]){
+                    stack.pop();
+                }
+                if(!stack.isEmpty()){
+                        left[i]=stack.peek();
+                }
+                stack.push(i);
+             }
+             stack.clear();
+
+              for(int i=n-1;i>=0;i--){
+                while(!stack.isEmpty() && nums[i]>=nums[stack.peek()]){
+                    stack.pop();
+                }
+                if(!stack.isEmpty()){
+                        right[i]=stack.peek();
+                }
+               stack.push(i);
+             }
+                    long sum=0;
+             for(int i=0;i<n;i++){
+                sum+=((i-left[i])*(right[i]-i))*nums[i];
+             }
+            
+                 System.out.println(Arrays.toString(left));
+              System.out.println(Arrays.toString(right));
+               System.out.println(sum);
+             return sum;
         }
-        return sum;
-    }
 }
-// Title: Sum of Subarray Ranges
