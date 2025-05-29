bc448aca122fa35dab3edba135364f21cdd14014
Netal-Rathi <netalrathi2707@gmail.com>
2025-05-29 08:37:10 +0530

    Add LRU Cache

--- LRU_Cache.java
+++ LRU_Cache.java
@@ -1,46 +1,39 @@
+import java.util.*;
+
 class LRUCache {
     private final int capacity;
-  private final Deque <Integer> q;
-  private final Map<Integer,Integer> map;
+    private final Deque<Integer> q; // stores keys in order: least recently used -> most
+    private final Map<Integer, Integer> map; // key -> value
 
     public LRUCache(int capacity) {
-        this.capacity=capacity;
-        q=new ArrayDeque<>();
-        map=new HashMap<>();
-        
-    }
-    
-    public int get(int key) {
-    if(!q.contains(key)){
-        return -1;
+        this.capacity = capacity;
+        this.q = new ArrayDeque<>();
+        this.map = new HashMap<>();
     }
 
+    public int get(int key) {
+        if (!map.containsKey(key)) {
+            return -1;
+        }
+
+        // move key to the end (most recently used)
         q.remove(key);
         q.addLast(key);
         return map.get(key);
-        
     }
-    
+
     public void put(int key, int value) {
-      if(q.contains(key)){
-        q.remove(key);
-      }else{
-        if(q.size()==capacity){
-           // q.removeFirst();
-           int lru=q.pollFirst();
-           map.remove(lru);
+        if (map.containsKey(key)) {
+            // Update value and refresh usage order
+            q.remove(key);
+        } else if (q.size() == capacity) {
+            // Remove least recently used key
+            int lru = q.pollFirst();
+            map.remove(lru);
         }
 
-      }
-      q.addLast(key);
-      map.put(key,value);
+        // Insert key as most recently used
+        q.addLast(key);
+        map.put(key, value);
     }
 }
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
-// Title: LRU Cache
