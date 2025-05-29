55ef9ee5c697b4c26db7dc83bdba9afba5b7414d
Netal-Rathi <netalrathi2707@gmail.com>
2025-05-29 09:46:23 +0530

    Add LRU Cache

--- LRU_Cache.java
+++ LRU_Cache.java
@@ -1,42 +1,41 @@
 class LRUCache {
- int capacity;
- Deque <Integer> q;
- Map<Integer,Integer> map;
+    private final int capacity;
+  private final Deque <Integer> q;
+  private final Map<Integer,Integer> map;
 
     public LRUCache(int capacity) {
         this.capacity=capacity;
-        this.q=new ArrayDeque();
-        this.map=new HashMap<>();
-
+        q=new ArrayDeque<>();
+        map=new HashMap<>();
+        
     }
     
     public int get(int key) {
+    if(!q.contains(key)){
+        return -1;
+    }
 
-        if(!q.contains(key)){
-            return -1;
-        }
         q.remove(key);
         q.addLast(key);
         return map.get(key);
-  
         
     }
     
     public void put(int key, int value) {
-        if(q.contains(key)){
-            q.remove(key);
-
-        }else if(q.size()==capacity){
-            map.remove(q.pollFirst());
+      if(q.contains(key)){
+        q.remove(key);
+      }else{
+        if(q.size()==capacity){
+           // q.removeFirst();
+           int lru=q.pollFirst();
+           map.remove(lru);
         }
-        q.addLast(key);
-        map.put(key,value);
-     
 
       }
-    
+      q.addLast(key);
+      map.put(key,value);
     }
-
+}
 
 /**
  * Your LRUCache object will be instantiated and called as such:
@@ -44,4 +43,3 @@ class LRUCache {
  * int param_1 = obj.get(key);
  * obj.put(key,value);
  */
-// Title: LRU Cache
