6ab3eb9afa8aa5475ede72fdf124d14b4b4ca67b
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-02 03:58:10 +0530

    Add Permutation in String

--- Permutation_in_String.java
+++ Permutation_in_String.java
@@ -5,8 +5,8 @@ class Solution {
         }
 
 
-     s1=   sort(s1);
-        for(int i=0;i<=s2.length()-s1.length();i++){
+        sort(s1);
+        for(int i=0;i<s2.length()-s1.length();i++){
             if(s1.equals(sort(s2.substring(i,i+s1.length())))){
                 return true;
             }
@@ -36,5 +36,3 @@ class Solution {
     //     return false; // If no permutation matches
     // }
 }
-
-// Title: Permutation in String
