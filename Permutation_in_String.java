17fb4f858d1202c555dc56ea4096ea7188cbd3d6
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-02 03:32:39 +0530

    Add Permutation in String

--- Permutation_in_String.java
+++ Permutation_in_String.java
@@ -1,23 +1,24 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        return isValid(s1, "", s2);
+        return isvalid(s1,"",s2);
+        
     }
 
-    public boolean isValid(String s, String permutation, String check) {
-        if (s.length() == 0) {
-            return check.contains(permutation);
-        }
+    public boolean isvalid(String s,String permutation,String check){
+        
+            if(s.length()==0  ){
+                return check.contains(permutation);
 
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            String sub = s.substring(0, i) + s.substring(i + 1);
-            if (isValid(sub, permutation + c, check)) {
-                return true;  // As soon as we find a match, return true
             }
-        }
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+           
+          String sub=s.substring(0,i)+s.substring(i+1);
+          if(  isvalid(sub,permutation+c,check)){
+            return true ;
+          }
 
-        return false; // If no permutation matches
+        }
+        return false;
     }
 }
-
-// Title: Permutation in String
