451995a368aca5699f9f39f11ef99b12800b48cb
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-02 04:30:30 +0530

    Add Permutation in String

--- Permutation_in_String.java
+++ Permutation_in_String.java
@@ -1,45 +1,49 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int m = s1.length();
-        int n = s2.length();
-        if (m > n) {
+        if(s1.length()>s2.length()){
             return false;
         }
 
-        char[] s1count = new char[26];
-        char[] s2count = new char[26];
+                char[] s1count=new char[26];
+                char[] s2count =new char[26];
 
-        for (int i = 0; i < m; i++) {
-            s1count[s1.charAt(i) - 'a']++;
-            s2count[s2.charAt(i) - 'a']++;
-        }
+                for(int i=0;i<s1.length();i++){
+                    s1count[s1.charAt(i)-'a']++;
+                     s2count[s2.charAt(i)-'a']++;
+                }
 
-        int start = 0;
+                // for(int i=0;i<s1.length();i++){
+                //     s2count[s2.charAt(i)-'a']++;
+                // }
+                    
+                    int start=0;
+                
+                for(int i=s1.length();i<s2.length();i++){
+                    if(Arrays.equals(s1count ,s2count)){
+                    return true ;
+                }
 
-        for (int i = m; i < n; i++) {
-            if (Arrays.equals(s1count, s2count)) {
-                return true;
-            }
+                    s2count[s2.charAt(start)-'a']--;
+                    start++;
+                    s2count[s2.charAt(i)-'a']++;
 
-            s2count[s2.charAt(start) - 'a']--;
-            start++;
-            s2count[s2.charAt(i) - 'a']++;
 
-        }
-        return Arrays.equals(s1count, s2count);
+                } 
+        return Arrays.equals(s1count ,s2count);
 
         // through sorrting 
-        //     if(m>n){
-        //         return false;
-        //     }
+    //     if(s1.length()>s2.length()){
+    //         return false;
+    //     }
 
-        //  s1=   sort(s1);
-        //     for(int i=0;i<=n-m;i++){
-        //         if(s1.equals(sort(s2.substring(i,i+m)))){
-        //             return true;
-        //         }
-        //     }
-        //     return false;
+
+    //  s1=   sort(s1);
+    //     for(int i=0;i<=s2.length()-s1.length();i++){
+    //         if(s1.equals(sort(s2.substring(i,i+s1.length())))){
+    //             return true;
+    //         }
+    //     }
+    //     return false;
     }
     // answer 2 sorting 
     // public String sort(String s){
@@ -48,7 +52,7 @@ class Solution {
     //        Arrays.sort(arr);
     //     return new String(arr);
     // }
-    // answer 1 recurssion 
+// answer 1 recurssion 
     // public boolean isValid(String s, String permutation, String check) {
     //     if (s.length() == 0) {
     //         return check.contains(permutation);
@@ -65,5 +69,3 @@ class Solution {
     //     return false; // If no permutation matches
     // }
 }
-
-// Title: Permutation in String
