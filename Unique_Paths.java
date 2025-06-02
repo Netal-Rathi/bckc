83ccac1e1ec8aa49669b472d2b41bf6393c5a818
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-02 09:35:26 +0530

    Add Unique Paths

---
+++ Unique_Paths.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    public int checker(int i,int j,int m,int n){
+        if(i==n || j==m){
+            return 0;
+        }
+         if(i==n-1 &&  j==m-1){
+            return 1;
+        }
+
+        int down=checker(i+1,j,m,n);
+        int right=checker(i,j+1,m,n);
+        return down+right;
+
+    }
+    public int uniquePaths(int m, int n) {
+
+            return checker(0,0,m,n);
+        
+    }
+}
