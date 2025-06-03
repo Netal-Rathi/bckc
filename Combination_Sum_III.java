be67982274ec8720fef708516d1feae193ddf04c
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-04 05:25:58 +0530

    Add Combination Sum III

--- Combination_Sum_III.java
+++ Combination_Sum_III.java
@@ -1,25 +1,37 @@
 class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-        check(k, n, 0, ans, 1, list);
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> list=new ArrayList<>();
+        check(k,n,0,ans,1,list);
         return ans;
+        
     }
 
-    public void check(int k, int n, int sum, List<List<Integer>> ans, int i, List<Integer> list) {
-        if (sum > n || list.size() > k) return; 
+    public void check(int k ,int n,int sum ,List<List<Integer>> ans ,int i,List<Integer> list){
+                    // if(i>9){
+                    //     return;
+                    // }
+        if( i==9 ){
+            if(list.size()==k && sum==n){
+           // if(sum==n){
+                ans.add(new ArrayList<>(list));
+                 
+            }
+             return ;   
+        }
+          
 
-        if (sum == n && list.size() == k) {
-            ans.add(new ArrayList<>(list));
-            return;
+            list.add(i);
+            sum+=i;
+            check(k,n,sum,ans,i+1,list);
+            sum-=list.get(list.size()-1);
+            list.remove(list.size()-1);
+
+            check(k,n,sum,ans,i+1,list);
+
+
+
         }
 
-        for (int j = i; j <= 9; j++) {
-            list.add(j);
-            check(k, n, sum + j, ans, j + 1, list); 
-            list.remove(list.size() - 1); 
-        }
+
     }
-}
-
-// Title: Combination Sum III
