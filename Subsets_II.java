c22d430e045dcd95e5343d620b5a68007bf23e17
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-03 10:17:32 +0530

    Add Subsets II

---
+++ Subsets_II.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums); // Important for handling duplicates
+        Set<List<Integer>> ans = new HashSet<>();
+        List<Integer> list = new ArrayList<>();
+        generate(nums, 0, list, ans);
+        return new ArrayList<>(ans);
+    }
+
+    private void generate(int[] nums, int idx, List<Integer> list, Set<List<Integer>> ans) {
+        if (idx == nums.length) {
+            ans.add(new ArrayList<>(list)); // Make a copy before adding
+            return;
+        }
+
+        // Include the current element
+        list.add(nums[idx]);
+        generate(nums, idx + 1, list, ans);
+        list.remove(list.size() - 1); // Backtrack
+
+        // Exclude the current element
+        generate(nums, idx + 1, list, ans);
+    }
+}
