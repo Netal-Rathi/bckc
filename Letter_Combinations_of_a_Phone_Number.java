f22aa1ce94221371055889e6a1d54e18845236a1
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-04 06:15:10 +0530

    Add Letter Combinations of a Phone Number

--- Letter_Combinations_of_a_Phone_Number.java
+++ Letter_Combinations_of_a_Phone_Number.java
@@ -1,40 +1,47 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        if (digits == null || digits.length() == 0) return new ArrayList<>();
+         List<String > ans=new ArrayList<>();
+        if(digits.length()==0){
+            return  ans;
+        }
+        
+        HashMap <Character,String> map=new HashMap<>();
+        
+        map.put('2',"abc");
+        map.put('3',"def");
+        map.put('4',"ghi");
 
-        HashMap<Integer, String> map = new HashMap<>();
-        map.put(2, "abc");
-        map.put(3, "def");
-        map.put(4, "ghi");
-        map.put(5, "jkl");
-        map.put(6, "mno");
-        map.put(7, "pqrs");
-        map.put(8, "tuv");
-        map.put(9, "wxyz");
+        map.put('5',"jkl");
+        map.put('6',"mno");
+        map.put('7',"pqrs");
 
-        List<String> ans = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        check(digits, map, 0, ans, sb);
-        return ans;
+        map.put('8',"tuv");
+        map.put('9',"wxyz");
+      //  map.put(4,"ghi");
+      //List<String > ans=new ArrayList<>();
+      StringBuilder sb=new StringBuilder();
+      check(digits,map,0,ans,sb);
+      return ans;
     }
 
-    public void check(String digits, HashMap<Integer, String> map, int no, List<String> ans, StringBuilder sb) {
-        if (no == digits.length()) {
-            ans.add(sb.toString());
+    public void check(String digits,HashMap<Character,String> map,int no ,List<String> ans ,StringBuilder sb){
+        if(no==digits.length()){
+            ans.add(new StringBuilder(sb).toString());
             return;
+
         }
 
-        int digit = digits.charAt(no) - '0';
-        String letters = map.get(digit);
-
-        if (letters != null) {
-            for (int i = 0; i < letters.length(); i++) {
-                sb.append(letters.charAt(i));
-                check(digits, map, no + 1, ans, sb);
-                sb.deleteCharAt(sb.length() - 1); // backtrack
-            }
+            char ch=digits.charAt(no);
+            String s=map.get(ch);
+        if(s!=null){
+        for(int i=0;i<s.length();i++){
+         //  StringBuilder sb=new StringBuilder();
+         char c =s.charAt(i);
+           // char c=map.get(digits.charAt(no)).charAt(i);
+            check(digits,map,no+1,ans,sb.append(c));
+            sb.deleteCharAt(sb.length()-1);
         }
+        }
+
     }
 }
-
-// Title: Letter Combinations of a Phone Number
