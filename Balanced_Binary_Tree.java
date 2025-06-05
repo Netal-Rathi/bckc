8592efd5beba4b0c2c1e9f50e89e5c95b4346ea4
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-06 03:57:36 +0530

    Add Balanced Binary Tree

--- Balanced_Binary_Tree.java
+++ Balanced_Binary_Tree.java
@@ -32,13 +32,7 @@ class Solution {
             }
 
       int left =  helper(root.left);
-      if(left==-1){
-        return -1;
-      }
         int right=helper(root.right);
-        if(right==-1){
-            return -1;
-        }
         if (Math.abs(left-right)>1){
             return -1;
 
@@ -49,4 +43,3 @@ class Solution {
         
     }
 }
-// Title: Balanced Binary Tree
