80b6011a4ea6ef7b4afe7da350172d9904ba566e
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-05 16:59:02 +0530

    Add Maximum Depth of Binary Tree

---
+++ Maximum_Depth_of_Binary_Tree.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxDepth(TreeNode root) {
+        // if (root == null) return 0;
+        // return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+        if(root==null){
+            return 0;
+        }
+       int left=maxDepth(root.left);
+        int right=maxDepth(root.right);
+        return Math.max(left,right)+1;
+    }
+}
