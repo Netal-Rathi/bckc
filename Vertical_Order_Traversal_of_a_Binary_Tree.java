e0f2f3a01a3a9de0b4c7d79198fa5f3f6531fce6
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-08 05:06:53 +0530

    Add Vertical Order Traversal of a Binary Tree

--- Vertical_Order_Traversal_of_a_Binary_Tree.java
+++ Vertical_Order_Traversal_of_a_Binary_Tree.java
@@ -1,40 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> verticalTraversal(TreeNode root) {
+       TreeMap<Integer,List<Integer>> ans=new TreeMap<>();
+       List<List<Integer>> finalans=new ArrayList<>();
 
-        List<int[]> nodes = new ArrayList<>();
+       check(ans,root,0);
 
-        dfs(root, 0, 0, nodes);  
+        // for(Map.Entry<Integer,List<>Integer> entry:ans.entrySet)
 
-   
-        Collections.sort(nodes, (a, b) -> {
-            if (a[0] != b[0]) return a[0] - b[0];       
-            if (a[1] != b[1]) return a[1] - b[1];       
-            return a[2] - b[2];                         
-        });
 
-        List<List<Integer>> result = new ArrayList<>();
-        int prevCol = Integer.MIN_VALUE;
+       for(List<Integer> list :ans.values() ){
+        Collections.sort(list);
+        finalans.add(list);
+       }
 
-        for (int[] entry : nodes) {
-            int col = entry[0], val = entry[2];
-            if (col != prevCol) {
-                result.add(new ArrayList<>());
-                prevCol = col;
-            }
-            result.get(result.size() - 1).add(val);
+
+return finalans;
+       
+        
+    }
+        public void check(TreeMap<Integer,List<Integer>> ans,TreeNode node,int idx){
+
+                if(node==null){
+                    return;
+                }
+                   //ans.get(idx).add(node.val);
+                 //  map.put(node.val,map.getOrDefault(node.val,idx));
+                 ans.putIfAbsent(idx,new ArrayList<>());
+                 ans.get(idx).add(node.val);
+                check(ans,node.left,idx-1);
+                check(ans,node.right,idx+1);
+
+                
         }
 
-        return result;
-    }
-
-
-    private void dfs(TreeNode node, int row, int col, List<int[]> nodes) {
-        if (node == null) return;
-
-        nodes.add(new int[]{col, row, node.val});
-        dfs(node.left, row + 1, col - 1, nodes);
-        dfs(node.right, row + 1, col + 1, nodes);
-    }
 }
-
-// Title: Vertical Order Traversal of a Binary Tree
