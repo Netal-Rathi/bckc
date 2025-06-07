859a54a3f5d3cd09d38077b26e970dd4677d252e
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-07 15:05:54 +0530

    Add Best Time to Buy and Sell Stock V

--- Best_Time_to_Buy_and_Sell_Stock_V.java
+++ Best_Time_to_Buy_and_Sell_Stock_V.java
@@ -1,19 +1,22 @@
 class Solution {
-    public long maximumProfit(int[] prices, int k) {
+    public int maxProfit(int k, int[] prices) {
         int n = prices.length;
         if (n < 2 || k == 0) return 0;
 
-        long[][] dp = new long[k + 1][n];
+      
+        int[][] dp = new int[k + 1][n];
 
         for (int t = 1; t <= k; t++) {
-            long normalBuy = -prices[0];
-            long shortSell = prices[0];
+        
+            int normalBuy = -prices[0];     
+            int shortSell = prices[0];     
 
             for (int d = 1; d < n; d++) {
-                dp[t][d] = Math.max(dp[t][d - 1],
-                              Math.max(prices[d] + normalBuy,
-                                       shortSell - prices[d]));
+              
+                                    Math.max(prices[d] + normalBuy,   
+                                             shortSell - prices[d])); 
 
+               
                 normalBuy = Math.max(normalBuy, dp[t - 1][d] - prices[d]);
                 shortSell = Math.max(shortSell, dp[t - 1][d] + prices[d]);
             }
@@ -22,5 +25,3 @@ class Solution {
         return dp[k][n - 1];
     }
 }
-
-// Title: Best Time to Buy and Sell Stock V
