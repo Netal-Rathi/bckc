81dca81b4899344089565f04cbdb2ecb06adef4e
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-07 15:09:40 +0530

    Add Best Time to Buy and Sell Stock V

--- Best_Time_to_Buy_and_Sell_Stock_V.java
+++ Best_Time_to_Buy_and_Sell_Stock_V.java
@@ -1,30 +1,24 @@
 class Solution {
     public long maximumProfit(int[] prices, int k) {
         int n = prices.length;
-        if (n == 0 || k == 0) return 0;
+        if (n < 2 || k == 0) return 0;
 
-        long[][] dp = new long[n + 1][2 * k + 1];
+        long[][] dp = new long[k + 1][n];
 
-        for (int i = n - 1; i >= 0; i--) {
-            for (int t = 0; t < 2 * k; t++) {
-                if (t % 2 == 0) {
-            
-                    long buy = -prices[i] + dp[i + 1][t + 1];
-                    long shortSell = prices[i] + dp[i + 1][t + 1];
-                    long skip = dp[i + 1][t];
-                    dp[i][t] = Math.max(skip, Math.max(buy, shortSell));
-                } else {
-                    
-                    long sell = prices[i] + dp[i + 1][t + 1];
-                    long buyBack = -prices[i] + dp[i + 1][t + 1];
-                    long skip = dp[i + 1][t];
-                    dp[i][t] = Math.max(skip, Math.max(sell, buyBack));
-                }
+        for (int t = 1; t <= k; t++) {
+            long normalBuy = -prices[0];
+            long shortSell = prices[0];
+
+            for (int d = 1; d < n; d++) {
+                dp[t][d] = Math.max(dp[t][d - 1],
+                              Math.max(prices[d] + normalBuy,
+                                       shortSell - prices[d]));
+
+                normalBuy = Math.max(normalBuy, dp[t - 1][d] - prices[d]);
+                shortSell = Math.max(shortSell, dp[t - 1][d] + prices[d]);
             }
         }
 
-        return dp[0][0];
+        return dp[k][n - 1];
     }
 }
-
-// Title: Best Time to Buy and Sell Stock V
