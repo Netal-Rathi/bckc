5e0823371de9985c8d0e19a7643d68c4b6701add
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-11 04:23:07 +0530

    Add Construct Binary Tree from Preorder and Inorder Traversal

---
+++ Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int pre=0;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n=preorder.length;
+
+       return  traverse(preorder,inorder,0,n-1);
+        
+    }
+
+    public TreeNode traverse(int[] preorder, int[] inorder,int start,int end){
+        if(start>end){
+            return null;
+        }
+        // if(start<0 || end<0){
+        //     return null;
+        // }
+
+        
+
+        int i=preorder[pre++];
+        TreeNode node=new TreeNode(i);
+
+        int in =find_inorder(inorder,start,end,i);
+
+        node.left=traverse(preorder,inorder,start,in-1);
+  node.right=traverse(preorder,inorder,in+1,end);
+  return node;
+
+
+    }
+
+    public int find_inorder(int[] inorder,int start,int end,int preval){
+        for(int i=start;i<=end;i++){
+            if(inorder[i]==preval){
+                return i;
+            }
+
+        }
+        return -1;
+
+    }
+}
