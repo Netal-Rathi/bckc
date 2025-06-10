2f65b197e0ebe7136d169ec3fa307700349246d8
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-10 14:21:16 +0530

    Add Fizz Buzz

---
+++ Fizz_Buzz.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        int i=1;
+        List<String> lis=new ArrayList<>();
+        while(i<=n){
+        if(i%3==0 && i%5==0){
+            lis.add("FizzBuzz");
+            i++;
+        }else if(i%3==0){
+            lis.add("Fizz");
+            i++;
+        }else if(i%5==0){
+            lis.add("Buzz");
+            i++;
+        }else{
+          
+            lis.add(    String.valueOf(i));
+            i++;
+        }
+        }
+        return lis;
+        
+    }
+}
