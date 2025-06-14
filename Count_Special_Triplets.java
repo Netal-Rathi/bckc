c9d68b2b0688943dd7dfb01c0df72f29af05caf4
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-15 03:51:45 +0530

    Add Count Special Triplets

--- Count_Special_Triplets.java
+++ Count_Special_Triplets.java
@@ -1,46 +1,33 @@
 import java.util.*;
 
 class Solution {
-    int count = 0;
-
     public int specialTriplets(int[] nums) {
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
-        int mod = 1000000007;
+        int n = nums.length;
+        int mod = 1_000_000_007;
+        long count = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (!map.containsKey(nums[i])) {
-                map.put(nums[i], new ArrayList<>());
-            }
-            map.get(nums[i]).add(i);
+        Map<Integer, Integer> suffix = new HashMap<>();
+        Map<Integer, Integer> prefix = new HashMap<>();
+
+       
+        for (int num : nums) {
+            suffix.put(num, suffix.getOrDefault(num, 0) + 1);
         }
 
-        for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
-            List<Integer> list1 = entry.getValue();
+        for (int j = 0; j < n; j++) {
+            int x = nums[j];
+          
+            suffix.put(x, suffix.get(x) - 1);
 
-            if (list1.size() >= 2) {
-                int key = entry.getKey();
-                int halfKey = key / 2;
+            int left = prefix.getOrDefault(2 * x, 0);   
+            int right = suffix.getOrDefault(2 * x, 0);  
 
-                if (map.containsKey(halfKey)) {
-                    List<Integer> list2 = map.get(halfKey);
-                    int l1 = list1.size();
-                    int l2 = list2.size();
+            count = (count + (1L * left * right)) % mod;
 
-                    for (int k = 0; k < l2; k++) {
-                        for (int i = 0; i < l1 - 1; i++) {
-                            for (int j = i + 1; j < l1; j++) {
-                                if (list2.get(k) > list1.get(i) && list2.get(k) < list1.get(j)) {
-                                    count++;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+           
+            prefix.put(x, prefix.getOrDefault(x, 0) + 1);
         }
 
-        return count % mod;
+        return (int) count;
     }
 }
-
-// Title: Count Special Triplets
