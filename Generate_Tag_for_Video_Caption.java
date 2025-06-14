9f15c437957a7c693a79fb3a9bbce9ed02c0810a
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-15 03:02:32 +0530

    Add Generate Tag for Video Caption

--- Generate_Tag_for_Video_Caption.java
+++ Generate_Tag_for_Video_Caption.java
@@ -1,49 +1,39 @@
 class Solution {
     public String generateTag(String caption) {
-        String[] arr = caption.split(" ");
-        StringBuilder s = new StringBuilder();
+        String arr[]= caption.split(" ");
+        StringBuilder s=new StringBuilder();
         s.append('#');
-
-      
-        if (arr.length > 0) {
-            String first = arr[0];
-            if (first.length() > 0) {
-                s.append(Character.toLowerCase(first.charAt(0)));
-                if (first.length() >= 100) {
-                    s.append(first.substring(1, 99));  
-                } else {
-                    s.append(first.substring(1));
+s.append(Character.toLowerCase(arr[0].charAt(0)));
+        if(arr[0].length()>=100){
+        s.append(arr[0].substring(1,99));
+        }else{
+              s.append(arr[0].substring(1));
+        }
+        for( int i=1;i<arr.length;i++){
+            String word=arr[i];
+            if(!word.equalsIgnoreCase("leetcode")  ){
+           s.append(Character.toUpperCase(word.charAt(0)));
+                 if(s.length()==100){
+                    return s.toString();
+                }
+            }else{
+               s.append(Character.toLowerCase(word.charAt(0)));
+                 if(s.length()==100){
+                    return s.toString();
                 }
             }
-        }
-
-        for (int i = 1; i < arr.length; i++) {
-            String word = arr[i];
-            if (word.isEmpty()) continue;
-
-            // Decide first character case
-            if (!word.equalsIgnoreCase("leetcode")) {
-                s.append(Character.toUpperCase(word.charAt(0)));
-            } else {
-                s.append(Character.toLowerCase(word.charAt(0)));
-            }
-
-           
-            if (s.length() >= 100) return s.substring(0, 100);
-
-            
-            for (int j = 1; j < word.length(); j++) {
-                char ch = Character.toLowerCase(word.charAt(j));
-                if (ch >= 'a' && ch <= 'z') {
+            for(int j=1;j<word.length();j++){
+                char ch=Character.toLowerCase(word.charAt(j));
+                if((ch>='a' && ch<='z' )  ){
                     s.append(ch);
                 }
-
-                if (s.length() >= 100) return s.substring(0, 100);
+                if(s.length()==100){
+                    return s.toString();
+                }
             }
         }
-
         return s.toString();
+        
     }
+    
 }
-
-// Title: Generate Tag for Video Caption
