3ee79f4f99920d1d7c9ef44532258698a647ea79
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-15 02:52:01 +0530

    Add Generate Tag for Video Caption

--- Generate_Tag_for_Video_Caption.java
+++ Generate_Tag_for_Video_Caption.java
@@ -3,24 +3,16 @@ class Solution {
         String arr[]= caption.split(" ");
         StringBuilder s=new StringBuilder();
         s.append('#');
-                    if(!arr[0].equalsIgnoreCase("leetcode")  ){
-           s.append(Character.toUpperCase(arr[0].charAt(0)));
-            }else{
-               s.append(Character.toLowerCase(arr[0].charAt(0)));
-            }
-        
-        s.append(arr[0].substring(1));
-        for( int i=1;i<arr.length;i++){
-            String word=arr[i];
+        for(String word: arr){
             if(!word.equalsIgnoreCase("leetcode")  ){
            s.append(Character.toUpperCase(word.charAt(0)));
             }else{
                s.append(Character.toLowerCase(word.charAt(0)));
             }
             for(int j=1;j<word.length();j++){
-                char ch=Character.toLowerCase(word.charAt(j));
-                if((ch>='a' && ch<='z' )  ){
-                    s.append(ch);
+                char a=word.charAt(j);
+                if((a>='a' && a<='z' ) ||(a>='A' && a<='Z') ){
+                    s.append(a);
                 }
                 if(s.length()==100){
                     return s.toString();
@@ -32,4 +24,3 @@ class Solution {
     }
     
 }
-// Title: Generate Tag for Video Caption
