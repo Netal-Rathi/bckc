16a300e6d97614629abacad3196fbcaebfe60048
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-15 12:25:59 +0530

    Add Search Insert Position

---
+++ Search_Insert_Position.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        //start==end==> +1
+        //// if equal ==> middle -1
+        
+
+        int start=0,end=nums.length-1;
+            int  middle=(end+start)/2;
+        while(start<=end){
+            
+      
+            if(target<nums[middle]){
+                end=middle-1;
+
+            }else if(target>nums[middle]){
+
+                    start=middle+1;
+            }else{
+                return middle;
+            }
+             middle=(end+start)/2;
+        }
+        return start;
+    }
+}
