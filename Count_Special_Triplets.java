296088f21842b114ccd4c001da259da8e6581c1c
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-15 09:02:36 +0530

    Add Count Special Triplets

--- Count_Special_Triplets.java
+++ Count_Special_Triplets.java
@@ -1,29 +1,44 @@
+import java.util.*;
+
 class Solution {
+    int count = 0;
+
     public int specialTriplets(int[] nums) {
-       int  count=0;
-        int mod=1000000007;
-        HashMap<Integer,Integer> prefix= new HashMap<>();
-        HashMap<Integer,Integer> suffix=new HashMap<>();
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        int mod = 1000000007;
 
-        int n=nums.length;
-        for(int i=0;i<n;i++){
-            suffix.put(nums[i],suffix.getOrDefault(nums[i],0)+1);
-            
-        }
-
-        for(int j=0;j< n ;j++){
-            int k=nums[j];
-             suffix.put(k,suffix.getOrDefault(k,0)-1);
-            if(suffix.get(k)==0){
-                suffix.remove(k);
+        for (int i = 0; i < nums.length; i++) {
+            if (!map.containsKey(nums[i])) {
+                map.put(nums[i], new ArrayList<>());
             }
-            int left=prefix.getOrDefault(k*2,0);
-            int right=suffix.getOrDefault(k*2,0);
-            count+=left*right;
-
-            prefix.put(k,prefix.getOrDefault(k,0)+1);
+            map.get(nums[i]).add(i);
         }
-        return count%mod;
+
+        for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
+            List<Integer> list1 = entry.getValue();
+
+            if (list1.size() >= 2) {
+                int key = entry.getKey();
+                int halfKey = key / 2;
+
+                if ( key%2==0 && map.containsKey(halfKey)) {
+                    List<Integer> list2 = map.get(halfKey);
+                    int l1 = list1.size();
+                    int l2 = list2.size();
+
+                    for (int k = 0; k < l2; k++) {
+                        for (int i = 0; i < l1 - 1; i++) {
+                            for (int j = i + 1; j < l1; j++) {
+                                if (list2.get(k) > list1.get(i) && list2.get(k) < list1.get(j)) {
+                                    count++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return count % mod;
     }
 }
-// Title: Count Special Triplets
