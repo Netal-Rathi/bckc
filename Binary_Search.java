7430b953d6d50d84051fb0c25ec0358f8225bedf
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-15 09:48:38 +0530

    Add Binary Search

---
+++ Binary_Search.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int start=0,end=nums.length-1;
+        int middle=(start+end)/2;
+            while(start<=end){
+        if(target<nums[middle]){
+            end=middle-1;
+        }else if(target>nums[middle]){
+            start=middle+1;
+        }else{
+            return middle;
+        }
+                middle=(start+end)/2;
+            }
+            return -1;
+        
+    }
+}
