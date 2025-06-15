a8f73ce14a4a24c9243a6c9b77d6bf1217179c9b
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-15 09:24:01 +0530

    Add Count Special Triplets

---
+++ Count_Special_Triplets.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int specialTriplets(int[] nums) {
+      long count=0;
+int mod = 1_000_000_007;
+        HashMap<Integer,Integer> prefix= new HashMap<>();
+        HashMap<Integer,Integer> suffix=new HashMap<>();
+
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            suffix.put(nums[i],suffix.getOrDefault(nums[i],0)+1);
+            
+        }
+
+        for(int j=0;j< n ;j++){
+            int k=nums[j];
+             suffix.put(k,suffix.getOrDefault(k,0)-1);
+            if(suffix.get(k)==0){
+                suffix.remove(k);
+            }
+            int left=prefix.getOrDefault(k*2,0);
+            int right=suffix.getOrDefault(k*2,0);
+          count = (count + (1L * left * right)) % mod;
+
+
+            prefix.put(k,prefix.getOrDefault(k,0)+1);
+        }
+        return (int) count;
+    }
+}
