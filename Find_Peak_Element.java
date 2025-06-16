1b8559733d689c40aafab8d7680bfae72d947112
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-17 04:12:55 +0530

    Add Find Peak Element

---
+++ Find_Peak_Element.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+       // if(num)
+        int n= nums.length-1;
+        int start=0,end=n;
+
+        while(start<=end){
+            if(start!=n && nums[start]>nums[start+1]){
+                return start;
+            }else{
+                start++;
+            }
+
+            if(end!=0 && nums[end]>nums[end-1]){
+                return end;
+            }else{
+                end--;
+            }
+
+        }
+        return 0;
+    }
+}
