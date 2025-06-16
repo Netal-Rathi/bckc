ffae65ccbe7238da1989087e3bd240cccf5a9835
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-17 02:47:46 +0530

    Add Search in Rotated Sorted Array II

---
+++ Search_in_Rotated_Sorted_Array_II.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int start=0,end=nums.length-1;
+        int middle=start + (end-start)/2;
+
+while(start<=end){
+        if(nums[middle]==target){
+            return true;
+        }
+
+         if(nums[start]==nums[middle] && nums[middle]==nums[end]){
+            start++;
+            end--;
+
+         }
+
+      else  if(nums[start]<=nums[middle]){
+            //left part
+            //end=middle-1;
+
+            if(target>=nums[start] && target<=nums[middle]){
+               end=middle-1;
+            }else{
+                start=middle+1;
+
+            }
+        }else{
+            //right part 
+             if(target>=nums[middle] && target<= nums[end]){
+               start=middle+1;
+            }else{
+                end=middle-1;
+
+            }
+        }
+        middle=start + (end-start)/2;
+
+}
+        return false;
+    }
+
+ 
+}
