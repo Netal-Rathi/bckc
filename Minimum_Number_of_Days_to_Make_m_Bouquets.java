d8fae7f4d7cdbceabbe10a585121a1b528724404
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-18 04:41:05 +0530

    Add Minimum Number of Days to Make m Bouquets

--- Minimum_Number_of_Days_to_Make_m_Bouquets.java
+++ Minimum_Number_of_Days_to_Make_m_Bouquets.java
@@ -1,41 +1,48 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
-        if (m * k > bloomDay.length) return -1;
 
-        int left = 1;
-        int right = Arrays.stream(bloomDay).max().getAsInt();
-        int result = -1;
+        if(m*k>bloomDay.length){
+            return -1;
+        }
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        // for(int i=0;i<bloomDay.length;i++){
+        //     map.put(bloomDay[i],map.getOrDefault(bloomDay[i],0)+1);
+        // }
+        int start=1;
+        int end=Arrays.stream(bloomDay).max().getAsInt();
+        int min=end;
+        while(start<end){
+            int middle=start+ (end-start)/2;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (check(bloomDay, m, k, mid)) {
-                result = mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+            if(check(bloomDay,m,k,middle)){
+                min=middle;
+                System.out.println(min);
+                end=middle-1;
+            }else{
+                start=middle +1;
             }
         }
-        return result;
+        return min;
+
+        
     }
 
-    public boolean check(int[] bloomDay, int m, int k, int mid) {
-        int bouquets = 0;
-        int flowers = 0;
+   public boolean check(int[] bloomDay, int m, int k, int mid) {
+    int bouquets = 0;
+    int flowers = 0;
 
-        for (int day : bloomDay) {
-            if (day <= mid) {
-                flowers++;
-                if (flowers == k) {
-                    bouquets++;
-                    flowers = 0;
-                }
-            } else {
+    for (int day : bloomDay) {
+        if (day <= mid) {
+            flowers++;
+            if (flowers == k) {
+                bouquets++;
                 flowers = 0;
             }
+        } else {
+            flowers = 0; // reset since flowers must be adjacent
         }
-        return bouquets >= m;
     }
+    return bouquets >= m;
 }
 
-// Title: Minimum Number of Days to Make m Bouquets
+}
