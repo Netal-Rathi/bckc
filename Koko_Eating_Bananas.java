df21fadde9849b16f0b3dcb3835ec44fef1b2eaf
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-17 07:14:36 +0530

    Add Koko Eating Bananas

--- Koko_Eating_Bananas.java
+++ Koko_Eating_Bananas.java
@@ -1,9 +1,9 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         int left=1;
-        int right=Arrays.stream(piles).max().getAsInt();
+        int right=Integer.MAX_VALUE;
         System.out.println(right);
-       int min=right;
+       int min=0;
 
         while(left<=right){
             int middle=left + (right-left )/2;
@@ -21,12 +21,16 @@ class Solution {
     public Boolean check(int [] piles ,int h ,int mid){
           int count =0;
           for (int i=0;i<piles.length;i++){
-          // count+=Math.ceil(piles[i]/mid);
-          count+=(piles[i]+ mid -1)/mid;
+            if(piles[i]<=mid){
+                count+=1;
+            }else if(piles[i]%mid!=0){
+            count+= piles[i]/mid + 1; 
+            }else{
+                count+=piles[i]/mid;
+            }
 
     }
 System.out.println(count);
     return count<=h ? true : false;
 }
 }
-// Title: Koko Eating Bananas
