fbf5db2a89fb30db9f2f9c511fd3190685e68544
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-18 05:19:56 +0530

    Add Minimum Number of Days to Make m Bouquets

--- Minimum_Number_of_Days_to_Make_m_Bouquets.java
+++ Minimum_Number_of_Days_to_Make_m_Bouquets.java
@@ -1,51 +1,39 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
+        if (m * k > bloomDay.length) return -1;
 
-        if(m*k>bloomDay.length){
-            return -1;
-        }
-        
-        int start=1;
-        int end=Arrays.stream(bloomDay).max().getAsInt();
-        int min=-1;
-        while(start<=end){
-            int middle=start+ (end-start)/2;
+        int left = 1;
+        int right = Arrays.stream(bloomDay).max().getAsInt();
+        int result = -1;
 
-            if(check(bloomDay,m,k,middle)){
-                min=middle;
-                System.out.println(min);
-                end=middle-1;
-            }else{
-                start=middle +1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (check(bloomDay, m, k, mid)) {
+                result = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
-        return min;
-
-        
+        return result;
     }
 
-    public boolean check(int arr[] ,int m ,int k,int mid){
-        int b=0;
-        int flowers=0;
-      //  Arrays.sort(arr);
-       
-        for(int i : arr){
-           if(i<=mid){
-            flowers++;
-            if(flowers==k){
-                b++;
-                flowers=0;
+    public boolean check(int[] bloomDay, int m, int k, int mid) {
+        int bouquets = 0;
+        int flowers = 0;
+
+        for (int day : bloomDay) {
+            if (day <= mid) {
+                flowers++;
+                if (flowers == k) {
+                    bouquets++;
+                    flowers = 0;
+                }
+            } else {
+                flowers = 0;
             }
-           }else{
-            flowers=0;
-           }
-            
-            
         }
-        
-        return b>=m;
-
-
+        return bouquets >= m;
     }
 }
-// Title: Minimum Number of Days to Make m Bouquets
