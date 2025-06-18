56bbf07474e6a479dba6b9613a52d9989d78570c
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-18 05:45:05 +0530

    Add Find the Smallest Divisor Given a Threshold

---
+++ Find_the_Smallest_Divisor_Given_a_Threshold.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int smallestDivisor(int[] nums, int threshold) {
+        int start=1;
+        int end =Arrays.stream(nums).max().getAsInt();
+       int min=end;
+
+        while(start<=end){
+            int middle=start+ (end-start)/2;
+
+            if(check(nums,middle,threshold)){
+                min=middle;
+                end=middle-1;
+            }else{
+                start=middle+1;
+            }
+
+        }
+        return min;
+
+
+        
+    }
+
+    public boolean check(int [] nums,int middle,int thresh){
+        int sum=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            sum+=(nums[i] + middle -1)/middle;
+
+        }
+        return sum<=thresh;
+
+    }
+}
