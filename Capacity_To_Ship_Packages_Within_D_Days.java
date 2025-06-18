3fc791bb3e4066c91887b765a5281002a65eacbf
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-18 06:17:36 +0530

    Add Capacity To Ship Packages Within D Days

--- Capacity_To_Ship_Packages_Within_D_Days.java
+++ Capacity_To_Ship_Packages_Within_D_Days.java
@@ -1,11 +1,14 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int start=1;
-        int end=Integer.MAX_VALUE;
+        
+       
+        int start=Arrays.stream(weights).max().getAsInt();
+        int end=Arrays.stream(weights).sum();
         int min=end;
 
         while(start<=end){
             int middle=start+ (end-start)/2;
+            System.out.println(middle);
             if(check(weights,days,middle)){
                 min=middle;
                 end=middle-1;
@@ -18,24 +21,15 @@ class Solution {
     }
 
     public boolean check(int nums[], int days,int middle ){
-        int count=0;
+        int count=1;
         int sum=0;
 
         for(int i : nums){
-            sum+=i;
-            if(sum==middle){
+            if(sum+ i > middle){
                 count++;
                 sum=0;
-            }else if (sum>middle){
-                count++;
-               
-                sum=i;
-            }else {
-                continue;
             }
-        }
-        if(sum<=middle){
-            count++;
+            sum+=i;
         }
         return count<=days;
 
