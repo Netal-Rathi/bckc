6dfbc2e949b58291fcd688adead99295b5bc8167
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-18 09:35:46 +0530

    Add Kth Missing Positive Number

---
+++ Kth_Missing_Positive_Number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int n = arr.length;
+        int count = 0;
+        int j = 1;
+        int i = 0;
+
+        while (count < k) {
+            if (i < n && arr[i] == j) {
+                i++;
+            } else {
+                count++;
+                if (count == k) return j;
+            }
+            j++;
+        }
+
+        return j;
+    }
+}
