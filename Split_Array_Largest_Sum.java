ef78ecdcfe14f937114adf8f8d5e25138b7c5b0d
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-19 10:15:41 +0530

    Add Split Array Largest Sum

--- Split_Array_Largest_Sum.java
+++ Split_Array_Largest_Sum.java
@@ -1,38 +1,53 @@
 class Solution {
+    int calcsum=0;
     public int splitArray(int[] nums, int k) {
-        int start = Arrays.stream(nums).max().getAsInt();
-        int end = Arrays.stream(nums).sum();
-        int result = end;
 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (canSplit(nums, mid, k)) {
-                result = mid;
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
+        if(k>nums.length){
+            return 0;
         }
-
-        return result;
+    Arrays.sort(nums);
+    
+    int start=nums[0];
+    int n=nums.length;
+    int end=Arrays.stream(nums).sum();
+    int min=end;
+    if(k==1){
+        return end;
     }
 
-    private boolean canSplit(int[] nums, int maxSumAllowed, int k) {
-        int currentSum = 0;
-        int subarrays = 1;
-
-        for (int num : nums) {
-            if (currentSum + num > maxSumAllowed) {
-                subarrays++;
-                currentSum = num;
-                if (subarrays > k) return false;
-            } else {
-                currentSum += num;
-            }
+    while(start<=end){
+        int middle =start + (end-start)/2;
+        int arr[]=calcmax(nums,middle,n,k);
+        if(arr[0]==1){
+            min=Math.min(min,arr[1]);
+            start=middle+1;
+        }else{
+            end=middle-1;
         }
+    }
+return min;
+        
+    }
 
-        return true;
+    public int[] calcmax(int [] nums,int middle,int n,int k){
+        int [] arr=new int[2];
+        int s = n/k + n%k ;
+        s=n-s;
+        int i=n-1;
+        int calcsum=0;
+        while(i>=s && calcsum<middle){
+            calcsum+=nums[i];
+            i--;
+
+        }
+        if(calcsum>=middle){
+            arr[0]=1;
+            arr[1]=calcsum;
+        }else{
+            arr[0]=0;
+            arr[1]=-1;
+        }
+        return arr;
+
     }
 }
-
-// Title: Split Array Largest Sum
