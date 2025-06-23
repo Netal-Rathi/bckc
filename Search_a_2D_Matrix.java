ecaa59de47ece0f8c9eae34b5fe9ada99aff549e
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-23 05:46:42 +0530

    Add Search a 2D Matrix

--- Search_a_2D_Matrix.java
+++ Search_a_2D_Matrix.java
@@ -5,13 +5,15 @@ class Solution {
         int i=0;
 
         while(i<m){
-            if(target<matrix[i][n-1]){
+            if(target<=matrix[i][n-1]){
                 break;
             }else{
                 i++;
             }
         }
-
+if(i==m){
+    return false;
+}
         int start =0;
       int  end=n-1;
 
