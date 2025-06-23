7f37d791c2d6d1a06fd24172864a2997cb5d5348
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-24 03:14:13 +0530

    Add Jump Game II

--- Jump_Game_II.java
+++ Jump_Game_II.java
@@ -1,11 +1,7 @@
 class Solution {
     public int jump(int[] nums) {
-        
         int i=0,count=1;
         int n=nums.length;
-        if(n==1){
-            return 0;
-        }
         while(i<n){
             int j=nums[i];
             int temp=j;
