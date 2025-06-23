01e7fff86d491b97079fb9e980c4ca33127f70e0
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-24 03:15:03 +0530

    Add Jump Game II

--- Jump_Game_II.java
+++ Jump_Game_II.java
@@ -1,32 +1,35 @@
 class Solution {
     public int jump(int[] nums) {
-        int i = 0, count = 0;
-        int n = nums.length;
-        if (n == 1) return 0;
-
-        while (i < n - 1) {
-            int j = nums[i];
-            if (i + j >= n - 1) {
-                count++;
-                break;
-            }
-
-            int maxReach = 0;
-            int k = 1;
-
-            for (int t = 1; t <= j; t++) {
-                if (i + t < n && i + t + nums[i + t] > maxReach) {
-                    maxReach = i + t + nums[i + t];
-                    k = t;
-                }
-            }
-
-            i += k;
-            count++;
+        
+        int i=0,count=1;
+        int n=nums.length;
+        if(n==1){
+            return 0;
         }
+        while(i<n){
+            int j=nums[i];
+            int temp=j;
+             int k=j;
+            if(nums[i+j]==n-1){
+                return count++;
+            }else{
+                
+                int max=nums[i+temp];
+               
+                while(temp>0){
+                    if(nums[i+temp]>max){
+                        k=temp;
+                     //   temp--;
+                    }
+                    temp--;
+                    
+                }
 
+                count++;
+            }
+            i+=k;
+        }
         return count;
+        
     }
 }
-
-// Title: Jump Game II
