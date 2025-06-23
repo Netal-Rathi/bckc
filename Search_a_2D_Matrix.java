8c7cd53659d22fc769dd91db904c26a0bcdd26fd
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-23 05:46:42 +0530

    Add Search a 2D Matrix

---
+++ Search_a_2D_Matrix.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+            int m=matrix.length;
+            int n=matrix[0].length;
+        int i=0;
+
+        while(i<m){
+            if(target<=matrix[i][n-1]){
+                break;
+            }else{
+                i++;
+            }
+        }
+if(i==m){
+    return false;
+}
+        int start =0;
+      int  end=n-1;
+
+        while(start<=end){
+            int middle=start+ (end-start)/2;
+            if(target==matrix[i][middle]){
+                return true;
+            }else if (target>matrix[i][middle]){
+                start=middle+1;
+
+            }else{
+                end=middle-1;
+            }
+
+        }
+        
+        return false;
+    }
+}
