e143bcd4aadd444251180b083a9a559c88bf9d6b
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-29 03:02:28 +0530

    Add Partition String

---
+++ Partition_String_.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<String> partitionString(String s) {
+         Set<String> seen=new HashSet<>();
+         List<String> list=new ArrayList<>();
+        int n=s.length();
+        
+        int i=0;
+        StringBuilder sub=new StringBuilder();
+        while(i<n){
+            sub.append(s.charAt(i));
+            if(!seen.contains(sub.toString())){
+                list.add(sub.toString());
+                 seen.add(sub.toString());
+                i++;
+                sub.setLength(0);
+                
+            }else{
+                i++;
+            }
+        }
+        return list;
+        
+    }
+}
