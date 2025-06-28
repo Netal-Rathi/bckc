7764f99e7437eae15b9aca4b8a8ec505b84b4481
Netal-Rathi <netalrathi2707@gmail.com>
2025-06-28 10:11:41 +0530

    Add 01 Matrix

--- 01_Matrix.java
+++ 01_Matrix.java
@@ -1,44 +1,53 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        Queue<int[]> q=new LinkedList<>();
-        int m=mat.length;
-        int n=mat[0].length;
-
-        //find 0  s and put it in queue (multilevel dfs )
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(mat[i][j]==0){
-                    q.offer(new int[] {i,j});
-                }else{
-                    mat[i][j]=-1;
+        int m = mat.length;
+        int n = mat[0].length;
+        int[][] ans = new int[m][n];
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    ans[i][j] = 0;
+                } else {
+                    ans[i][j] = bfs(mat, m, n, i, j);
                 }
             }
-
         }
+        return ans;
+    }
 
-        // start bfs 
-        int[][] dir = {{0,1}, {1,0}, {-1,0}, {0,-1}};   
-        while (!q.isEmpty()){
-            int [] arr= q.poll();
-            //int x=arr[0];
-            int i=arr[0];
-            int j=arr[1];
-            for(int[] d : dir){
-                int x=i+d[0];//i
-                int y=j + d[1];//j
+    public int bfs(int[][] mat, int m, int n, int p, int qu) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{p, qu});
+        boolean[][] visited = new boolean[m][n];
+        visited[p][qu] = true;
 
-                if(x>=0 && x< m && y >=0 && y<n && mat[x][y]==-1){
-                    mat[x][y]=mat[i][j]+1;
-                    q.offer(new int [] {x,y});
+        int[][] dir = {{0,1}, {1,0}, {-1,0}, {0,-1}};
+        int count = 0;
+
+        while (!q.isEmpty()) {
+            int level = q.size();
+            while (level-- > 0) {
+                int[] arr = q.poll();
+                int x = arr[0];
+                int y = arr[1];
+
+                if (mat[x][y] == 0) {
+                    return count;
                 }
 
+                for (int[] d : dir) {
+                    int i = x + d[0];
+                    int j = y + d[1];
+                    if (i >= 0 && i < m && j >= 0 && j < n && !visited[i][j]) {
+                        visited[i][j] = true;
+                        q.add(new int[]{i, j});
+                    }
+                }
             }
-
-        
-
+            count++;
         }
-        return mat;
-        
+
+        return count;
     }
 }
-// Title: 01 Matrix
