cb6a6e2b561cc06d3a4a85867b923cab16e864fb
Netal-Rathi <netalrathi2707@gmail.com>
2025-07-01 08:40:05 +0530

    Add Surrounded Regions

--- Surrounded_Regions.java
+++ Surrounded_Regions.java
@@ -1,42 +1,48 @@
+import java.util.*;
+
 class Solution {
     public void solve(char[][] board) {
-        // sare o queue mei
-        // maintain boolean check for all os 
-        // check if its not corner/edge value 
-            // if h toh break out of loop 
-        // else check for all if baju mei x yao h toh convert inplace to x 
+        int m = board.length;
+        int n = board[0].length;
+        Queue<int[]> q = new LinkedList<>();
 
-        int m=board.length;
-        int n=board[0].length;
-        Queue<int[]> q=new LinkedList<>();
-        for(int i=0;i<m;i++){
-            for (int j=0;j<n;j++){
-                if(board[i][j]=='O'){
-                    q.offer(new int []{i,j});
+ 
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i == 0 || j == 0 || i == m - 1 || j == n - 1) && board[i][j] == 'O') {
+                    board[i][j] = 'E'; 
+                    q.offer(new int[]{i, j});
                 }
             }
         }
 
-        int dir[][]=new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
+      
+        int[][] dir = new int[][]{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        while (!q.isEmpty()) {
+            int[] cell = q.poll();
+            int x = cell[0];
+            int y = cell[1];
 
+            for (int[] d : dir) {
+                int i = x + d[0];
+                int j = y + d[1];
 
-        while(!q.isEmpty()){
-
-                int [] arr=q.poll();
-                int i=arr[0];
-                int j=arr[1];
-        boolean check;
-        if(i!=m-1 && j!=n-1){
-                for(int[] d: dir){
-                    int x=i+d[0];
-                    int y=j+d[1];
-
-                    if(x>=0 && x<m &&  y>=0 && y<n){
-                        board[x][y]='X';
-                    }
+                if (i >= 0 && i < m && j >= 0 && j < n && board[i][j] == 'O') {
+                    board[i][j] = 'E';       
+                   // q.offer(new int[]{i, j}); 
                 }
+            }
         }
+
+   
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X'; 
+                } else if (board[i][j] == 'E') {
+                    board[i][j] = 'O'; 
+                }
+            }
         }
-      //  return board ;
     }
 }
