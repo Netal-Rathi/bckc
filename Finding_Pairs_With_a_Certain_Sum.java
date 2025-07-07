b14fb689449343c1b71271380897c27800afb393
Netal-Rathi <netalrathi2707@gmail.com>
2025-07-07 08:44:57 +0530

    Add Finding Pairs With a Certain Sum

--- Finding_Pairs_With_a_Certain_Sum.java
+++ Finding_Pairs_With_a_Certain_Sum.java
@@ -1,32 +1,57 @@
 class FindSumPairs {
         int []nums1;
         int []nums2;
+          HashMap<Integer,Integer> map;
     public FindSumPairs(int[] nums1, int[] nums2) {
         this.nums1=nums1;
         this.nums2=nums2;
+        this.map=new HashMap<>();
+           for(int i:nums2){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        
+       
         
     }
     
     public void add(int index, int val) {
-        if(index<nums2.length){
+        // if(index<nums2.length){
+        //     ans-map.get(nums2[index]);
+        // nums2[index]+=val;
+
+        // }
+        int n=nums2[index];
+        map.put(n,map.get(n)-1);
         nums2[index]+=val;
-        }
+        map.put(nums2[index],map.getOrDefault(nums2[index],0)+1);
 
         
     }
     
     public int count(int tot) {
+        // if(check){
+        //     return ans;
+        // }
         int ans=0;
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i:nums1){
-            map.put(tot-i,map.getOrDefault(tot-i,0)+1);
-        }
+      
+      
+        // for(int i:nums1){
+        //     map.put(tot-i,map.getOrDefault(tot-i,0)+1);
+        // }
 
-        for(int i:nums2){
-            if(map.containsKey(i)){
-                ans+=map.get(i);
+        // for(int i:nums2){
+        //     if(map.containsKey(i)){
+        //         ans+=map.get(i);
+        //     }
+        // }
+        
+
+        for(int i:nums1){
+            if(map.containsKey(tot-i)){
+                ans+=map.get(tot-i);
             }
         }
+       
         return ans;
     }
 }
@@ -37,4 +62,3 @@ class FindSumPairs {
  * obj.add(index,val);
  * int param_2 = obj.count(tot);
  */
-// Title: Finding Pairs With a Certain Sum
