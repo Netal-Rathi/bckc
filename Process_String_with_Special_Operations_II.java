d6c488b8d29c41a623e0840933657e27034b7645
Netal-Rathi <netalrathi2707@gmail.com>
2025-07-13 03:10:49 +0530

    Add Process String with Special Operations II

--- Process_String_with_Special_Operations_II.java
+++ Process_String_with_Special_Operations_II.java
@@ -1,25 +1,25 @@
 class Solution {
     public char processStr(String s, long k) {
-        List<Character> sb = new ArrayList<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-
-            if (ch == '#') {
-                if ((long)sb.size() * 2 > k + 1) break; // prevent MLE
-                List<Character> copy = new ArrayList<>(sb);
-                sb.addAll(copy);
-            } else if (ch == '%') {
-                Collections.reverse(sb);
-            } else if (ch == '*') {
-                if (!sb.isEmpty()) sb.remove(sb.size() - 1);
-            } else {
-                sb.add(ch);
+      StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='#'){
+                sb.append(sb);
+                // System.out.println(sb);
+            }else if(s.charAt(i)=='%'){
+              //  StringBuilder sk=new StringBuilder(sb);
+               // sb.append(sk.reverse());
+                sb.reverse();
+            }else if(s.charAt(i)=='*'){
+                if(sb.length()>0){
+                    sb.deleteCharAt(sb.length()-1);
+                }
+            }else{
+                sb.append(s.charAt(i));
             }
-
-            if (sb.size() > k) break;
-        }
-
-        return k < sb.size() ? sb.get((int)k) : '.';
+        }    
+       if(k>=sb.length()){
+           return '.';
+       }
+        return sb.charAt((int)k);
     }
 }
