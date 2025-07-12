e016b502ad8d382f87b78fcb7a064de4bab0b200
Netal-Rathi <netalrathi2707@gmail.com>
2025-07-13 04:10:37 +0530

    Add Process String with Special Operations II

--- Process_String_with_Special_Operations_II.java
+++ Process_String_with_Special_Operations_II.java
@@ -1,13 +1,11 @@
 class Solution {
     public char processStr(String s, long k) {
-        long len=0;
+        int len=0;
         for (char ch:s.toCharArray()){
             if(Character.isLowerCase(ch) ){
                 len++;
             }else if(ch=='*'){
-                if(len>0){
                 len--;
-                }
             }else if(ch=='#'){
                 len+=len;
             }
@@ -39,4 +37,3 @@ class Solution {
         return '.';
     }
 }
-// Title: Process String with Special Operations II
