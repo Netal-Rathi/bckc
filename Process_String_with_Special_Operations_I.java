32c8657ccf8c25894404d3db4865aacf4975a40c
Netal-Rathi <netalrathi2707@gmail.com>
2025-07-15 17:18:06 +0530

    Add Process String with Special Operations I

---
+++ Process_String_with_Special_Operations_I.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String processStr(String s) {
+
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='#'){
+                sb.append(sb);
+                System.out.println(sb);
+            }else if(s.charAt(i)=='%'){
+              //  StringBuilder sk=new StringBuilder(sb);
+               // sb.append(sk.reverse());
+                sb.reverse();
+            }else if(s.charAt(i)=='*'){
+                if(sb.length()>0){
+                    sb.deleteCharAt(sb.length()-1);
+                }
+            }else{
+                sb.append(s.charAt(i));
+            }
+        }    
+        return sb.toString();
+    }
+}
