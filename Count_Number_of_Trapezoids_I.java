b46452865efa272de5ec19cdd7c7c2aedd7f9efe
Netal-Rathi <netalrathi2707@gmail.com>
2025-07-20 03:22:41 +0530

    Add Count Number of Trapezoids I

--- Count_Number_of_Trapezoids_I.java
+++ Count_Number_of_Trapezoids_I.java
@@ -1,47 +1,69 @@
 class Solution {
-    int mod = 1000000007;
-
     public int countTrapezoids(int[][] points) {
-        Map<Integer, Integer> map = new HashMap<>();
-        List<Integer> list = new ArrayList<>();
+        int mod=1000000007;
+         Map<Integer,Integer> map=new HashMap<>();
+      //  List<Integer> toremove=new ArrayList<>();
+        List<Integer> list=new ArrayList<>();
+        //count : 
+        for(int [] arr : points ){
+            map.put(arr[1],map.getOrDefault(arr[1],0)+1);
+        }
 
+          // for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+          //       int s=entry.getValue();
+          //      if(s==1){
+          //         // map.remove(s);
+          //          toremove.add(entry.getKey());
+          //      }
+          // }
+        // replace by factorial : 
+           for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+               
+               int s=entry.getValue();
+               if(s>=2){
+               
+               int dp[] =new int[s];
+               Arrays.fill(dp,-1);
+               map.put(entry.getKey(),factorial(s-1,dp));
+                   list.add(entry.getValue());
+         System.out.println(entry.getKey() + "  "  + entry.getValue());
+               }
+               //    System.out.print(entry,getValue);
+                   
+               
+           }
         
-        for (int[] arr : points) {
-            map.put(arr[1], map.getOrDefault(arr[1], 0) + 1);
-        }
-
-        // Process frequencies
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            int s = entry.getValue();
-
-            if (s == 2) {
-                list.add(1);
-            } else if (s > 2) {
-                int[] dp = new int[s];
-                Arrays.fill(dp, -1);
-                list.add(sumUpToN(s - 1, dp));
+int count=0;
+        // for(Map.Entry<Integer,Integer> i : map.entrySet()){
+        //     for(Map.Entry<Integer,Integer> j : map.entrySet()){
+        //         count+=(i.getValue())%mod *(j.getValue())%mod;
+                
+        //     }
+        // }
+int k=list.size();
+        for(int i=0;i<k-1;i++){
+            for(int j=i+1;j<k;j++){
+                count+=((list.get(i))%mod *(list.get(j))%mod)%mod;
+                
             }
         }
-
+            // count+=entry.getValue();
+          return count%mod;
+        }
        
-        int count = 0;
-        int k = list.size();
-        for (int i = 0; i < k - 1; i++) {
-            for (int j = i + 1; j < k; j++) {
-                count = (int)((count + (1L * list.get(i) * list.get(j)) % mod) % mod);
-            }
+        
+    
+
+
+    public int factorial(int n,int [] dp ){
+        if(n==1 || n==0){
+            return 1;
         }
-
-        return count;
-    }
-
-   
-    public int sumUpToN(int n, int[] dp) {
-        if (n == 0 || n == 1) return n;
-        if (dp[n] != -1) return dp[n];
-        dp[n] = (n + sumUpToN(n - 1, dp)) % mod;
+        if(dp[n]!=-1){
+            return dp[n];
+            
+        }
+        dp[n]=Math.abs(n)+factorial(Math.abs(n-1),dp);
         return dp[n];
     }
 }
-
-// Title: Count Number of Trapezoids I
