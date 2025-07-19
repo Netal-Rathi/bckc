b11d374fd2c0682bcc9b055e0f6a7b428d525629
Netal-Rathi <netalrathi2707@gmail.com>
2025-07-20 03:43:30 +0530

    Add Count Number of Trapezoids I

--- Count_Number_of_Trapezoids_I.java
+++ Count_Number_of_Trapezoids_I.java
@@ -17,27 +17,23 @@ class Solution {
             if (s == 2) {
                 list.add(1);
             } else if (s > 2) {
-                list.add((int)((1L * s * (s - 1) / 2) % mod));
+                // int[] dp = new int[s];
+                // Arrays.fill(dp, -1);
+                // n(n+1)/2
+               //int n = s - 1;
+            list.add((int)((1L * s * (s - 1) / 2) % mod));
 
             }
         }
 
        
-      
+        int count = 0;
         int k = list.size();
         for (int i = 0; i < k - 1; i++) {
             for (int j = i + 1; j < k; j++) {
                 count = (int)((count + (1L * list.get(i) * list.get(j)) % mod) % mod);
             }
         }
-        // int pod=1;
-        // for(int i=0;i<k;i++){
-        //     pod=(int )(1L * pod*list.get(i)) % mod;
-        // }
-        //   int count = pod;
-        // for(int i=0;i<k;i++){
-        //     count =(count + pod/list.get(i))%mod ;
-        // }
 
         return count;
     }
@@ -50,5 +46,3 @@ class Solution {
     //     return dp[n];
     // }
 }
-
-// Title: Count Number of Trapezoids I
